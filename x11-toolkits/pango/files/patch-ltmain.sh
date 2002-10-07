d6ae9af9fa8bcaf6de4c99ad25a5de5fb8b31ec3
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2002-10-07 05:35:26 +0000

    Add pango-1.1.1.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11-toolkits/pango/files/patch-ltmain.sh
@@ -0,0 +1,69 @@
+--- ltmain.sh.orig	Fri Apr 26 05:23:44 2002
++++ ltmain.sh	Wed Jul  3 12:50:12 2002
+@@ -1056,14 +1056,14 @@
+ 	    # These systems don't actually have a C library (as such)
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd*|*-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	  *-*-openbsd*)
++	  *-*-openbsd*|*-*-freebsd*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -1073,8 +1073,16 @@
+ 	continue
+ 	;;
+ 
++      -pthread)
++	compile_command="$compile_command -pthread"
++	finalize_command="$finalize_command -pthread"
++	compiler_flags="$compiler_flags -pthread"
++	continue
++	;;
++
+       -module)
+ 	module=yes
++	build_old_libs=no
+ 	continue
+ 	;;
+ 
+@@ -2399,6 +2407,9 @@
+ 	    # problems, so we reset it completely
+ 	    verstring=""
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    verstring="0.0"
+ 	    ;;
+@@ -2502,6 +2513,9 @@
+ 	  *-*-openbsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test "$build_libtool_need_lc" = "yes"; then
+@@ -4286,10 +4300,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
