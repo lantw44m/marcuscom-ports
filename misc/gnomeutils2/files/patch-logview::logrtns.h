98a87a8cf45bf63dcae1f9fc6959a3d4f24b7e59
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2002-10-10 03:43:42 +0000

    Add gnomeutils2.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@40 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ misc/gnomeutils2/files/patch-logview::logrtns.h
@@ -0,0 +1,15 @@
+
+$FreeBSD: ports/misc/gnomeutils2/files/patch-logview::logrtns.h,v 1.1 2001/11/23 16:24:43 sobomax Exp $
+
+--- logview/logrtns.h	2001/11/23 16:04:00	1.1
++++ logview/logrtns.h	2001/11/23 16:04:14
+@@ -24,7 +24,9 @@
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
++#ifdef HAVE_MALLOC_H
+ #include <malloc.h>
++#endif
+ #include <string.h>
+ #include "logview.h"
+ 
