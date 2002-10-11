9408811f5ce27b53e40babbbbbb1dc3972d6cd05
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2002-10-11 06:39:49 +0000

    Add evolution-devel.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@58 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
@@ -0,0 +1,14 @@
+--- my-evolution/e-summary-preferences.c.orig	Thu Sep 19 00:43:57 2002
++++ my-evolution/e-summary-preferences.c	Thu Sep 19 00:43:58 2002
+@@ -574,9 +574,11 @@
+ 	{"http://www.bsdtoday.com/backend/bt.rdf", "BSD Today", FALSE},
+ 	{"http://beyond2000.com/b2k.rdf", "Beyond 2000", FALSE},
+   	{"http://www.cnn.com/cnn.rss", "CNN", FALSE},
++	{"http://daily.daemonnews.org/ddn.rdf.php3", "Daily Daemon News"},
+         {"http://www.debianplanet.org/debianplanet/backend.php", "Debian Planet", FALSE},
+ 	{"http://www.dictionary.com/wordoftheday/wotd.rss", N_("Dictionary.com Word of the Day"), FALSE},
+ 	{"http://www.dvdreview.com/rss/newschannel.rss", "DVD Review", FALSE},
++	{"http://www.freebsd.org/news/news.rdf", "FreeBSD Newsflash"},
+ 	{"http://freshmeat.net/backend/fm.rdf", "Freshmeat", FALSE},
+ 	{"http://news.gnome.org/gnome-news/rdf", "GNotices", FALSE},
+ 	{"http://headlines.internet.com/internetnews/prod-news/news.rss", "Internet.com", FALSE},
