6d50931112c4b638a7e0bf9b8c683439d60d0023
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2002-10-15 22:14:04 +0000

    Update to 1.1.2.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@89 df743ca5-7f9a-e211-a948-0013205c9059

--- mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
+++ mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
@@ -1,11 +1,10 @@
---- my-evolution/e-summary-preferences.c.orig	Thu Sep 19 00:43:57 2002
-+++ my-evolution/e-summary-preferences.c	Thu Sep 19 00:43:58 2002
-@@ -574,9 +574,11 @@
+--- my-evolution/e-summary-preferences.c.orig	Thu Sep 26 15:23:36 2002
++++ my-evolution/e-summary-preferences.c	Tue Oct 15 17:22:04 2002
+@@ -581,8 +581,10 @@
+ 	{"http://barrapunto.com/gnome.rdf", "Barrapunto GNOME", FALSE,},
  	{"http://www.bsdtoday.com/backend/bt.rdf", "BSD Today", FALSE},
  	{"http://beyond2000.com/b2k.rdf", "Beyond 2000", FALSE},
-   	{"http://www.cnn.com/cnn.rss", "CNN", FALSE},
 +	{"http://daily.daemonnews.org/ddn.rdf.php3", "Daily Daemon News"},
-         {"http://www.debianplanet.org/debianplanet/backend.php", "Debian Planet", FALSE},
  	{"http://www.dictionary.com/wordoftheday/wotd.rss", N_("Dictionary.com Word of the Day"), FALSE},
  	{"http://www.dvdreview.com/rss/newschannel.rss", "DVD Review", FALSE},
 +	{"http://www.freebsd.org/news/news.rdf", "FreeBSD Newsflash"},
