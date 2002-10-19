fb67261ec5ff4105931726bf126052c1c1dfc416
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2002-10-19 19:42:59 +0000

    Add bonobo-activation.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@98 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ devel/bonobo-activation/files/patch-server::activation-server-main.c
@@ -0,0 +1,14 @@
+
+$FreeBSD: ports/devel/bonobo-activation/files/patch-server::activation-server-main.c,v 1.2 2002/05/28 16:08:17 marcus Exp $
+
+--- server/activation-server-main.c.orig	Tue May 14 04:51:55 2002
++++ server/activation-server-main.c	Mon May 27 13:36:25 2002
+@@ -127,7 +127,7 @@
+                         g_string_append (gnome_od_source_dir,
+                                          gnome_dirs[i]);
+                         g_string_append (gnome_od_source_dir,
+-                                         "/lib/bonobo/servers:");
++                                         "/libdata/bonobo/servers:");
+                 }
+                 g_strfreev (gnome_dirs);
+                 g_string_append_c (real_od_source_dir, ':');
