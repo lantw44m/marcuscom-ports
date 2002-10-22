223f24a2d0283d1af4d81e981e8777fdb5e1c32f
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2002-10-22 21:53:50 +0000

    Update to 0.9.2.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@102 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11-toolkits/vte/files/patch-src_keymap.c
@@ -0,0 +1,10 @@
+--- src/keymap.c.orig	Tue Oct 22 17:42:20 2002
++++ src/keymap.c	Tue Oct 22 17:42:33 2002
+@@ -18,6 +18,7 @@
+ 
+ #ident "$Id: patch-src_keymap.c,v 1.1 2002-10-22 21:53:50 marcus Exp $"
+ #include "../config.h"
++#include <sys/types.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <glib.h>
