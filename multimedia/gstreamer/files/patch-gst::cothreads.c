c28a561a66352cad2b4608b4e0d9e2fcfb2480af
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-01-14 05:37:09 +0000

    Fix build on 5.x.
    Reported by:	Mezz bsdforums.org <reigncracks@hotmail.com>
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@387 df743ca5-7f9a-e211-a948-0013205c9059

--- multimedia/gstreamer/files/patch-gst::cothreads.c
+++ multimedia/gstreamer/files/patch-gst::cothreads.c
@@ -1,6 +1,15 @@
---- gst/cothreads.c.orig	Wed Nov 13 22:59:21 2002
-+++ gst/cothreads.c	Wed Nov 13 22:59:38 2002
-@@ -41,11 +41,11 @@
+--- gst/cothreads.c.orig	Tue Dec 31 23:28:32 2002
++++ gst/cothreads.c	Mon Jan 13 19:31:54 2003
+@@ -37,16 +37,20 @@
+ #include "gstlog.h"
+ #include "gstutils.h"
+ 
++#ifdef HAVE_MAKECONTEXT
++#include <ucontext.h>
++#endif
++
+ /* older glibc's have MAP_ANON instead of MAP_ANONYMOUS */
+ #ifndef MAP_ANONYMOUS
  #define MAP_ANONYMOUS MAP_ANON
  #endif
  
@@ -13,4 +22,4 @@
 +#define COTHREAD_MAXTHREADS 8
  #define COTHREAD_STACKSIZE (STACK_SIZE/COTHREAD_MAXTHREADS)
  
- static void 	cothread_destroy 	(cothread_state *thread);
+ static void 	cothread_destroy 	(cothread_state *cothread);
