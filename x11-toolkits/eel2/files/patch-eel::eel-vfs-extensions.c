1d073ce4dff059e5bfb69910e1809f03e53feba9
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-04-12 05:48:16 +0000

    Add eel2.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@590 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11-toolkits/eel2/files/patch-eel::eel-vfs-extensions.c
@@ -0,0 +1,15 @@
+
+$FreeBSD: ports/x11-toolkits/eel2/files/patch-eel::eel-vfs-extensions.c,v 1.3 2003/02/07 18:42:19 marcus Exp $
+
+--- eel/eel-vfs-extensions.c	2001/12/20 13:41:21	1.1
++++ eel/eel-vfs-extensions.c	2001/12/20 13:47:36
+@@ -116,7 +116,8 @@
+ 
+ 	/* Return the file. */
+ 	*file_size = total_bytes_read;
+-	*file_contents = g_realloc (buffer, total_bytes_read);
++	*file_contents = g_realloc (buffer, total_bytes_read + 1);
++	(*file_contents)[total_bytes_read] = '\0';
+ 	return GNOME_VFS_OK;
+ }
+ 
