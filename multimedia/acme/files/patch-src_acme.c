69c17b7fa3358118b2509768934b9ced52afa434
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-05-25 22:05:24 +0000

    * Update to 2.0.4 * Add a patch to use gcalctool over the now non-existent gnome-calculator [1]
    Submitted by:	Franz Klammer <klammer@webonaut.com>
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@811 df743ca5-7f9a-e211-a948-0013205c9059

--- multimedia/acme/files/patch-src_acme.c
+++ multimedia/acme/files/patch-src_acme.c
@@ -1,15 +1,24 @@
---- src/acme.c.orig	Sun Jan 19 06:43:45 2003
-+++ src/acme.c	Mon Jan 27 18:09:38 2003
-@@ -201,10 +201,10 @@
+--- src/acme.c.orig	Tue Mar 11 20:48:13 2003
++++ src/acme.c	Sun May 25 11:44:56 2003
+@@ -198,10 +198,10 @@
  	if ((soundfile == NULL) || (strcmp (soundfile, "") == 0)) 
  		return;
  
 -	if (g_file_test ("/usr/bin/esdplay",
-+	if (g_file_test ("%%LOCALBASE%%/bin/esdplay",
++	if (g_file_test ("/usr/local/bin/esdplay",
  			(G_FILE_TEST_EXISTS | G_FILE_TEST_IS_EXECUTABLE)))
  	{
 -		command = g_strdup_printf ("/usr/bin/esdplay %s",
-+		command = g_strdup_printf ("%%LOCALBASE%%/bin/esdplay %s",
++		command = g_strdup_printf ("/usr/local/bin/esdplay %s",
  				soundfile);
  	} else if (g_file_test ("/usr/bin/play",
  			(G_FILE_TEST_EXISTS | G_FILE_TEST_IS_EXECUTABLE)))
+@@ -1120,7 +1120,7 @@
+ 		do_www_action (acme, "http://www.gnomedesktop.org");
+ 		break;
+ 	case CALCULATOR_KEY:
+-		execute ("gnome-calculator", FALSE);
++		execute ("gcalctool", FALSE);
+ 		break;
+ 	case RECORD_KEY:
+ 		execute ("gnome-sound-recorder", FALSE);
