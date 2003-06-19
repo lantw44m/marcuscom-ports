40775abe75ae07d0eec90ae4e0f6e334fd51f9eb
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-06-19 18:26:35 +0000

    Chase atk's shared lib version.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@907 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -213,7 +213,7 @@ glib20_LIB_DEPENDS=	glib-2.0.200:${PORTSDIR}/devel/glib20
 glib20_DETECT=		${LOCALBASE}/libdata/pkgconfig/glib-2.0.pc
 glib20_USE_GNOME_IMPL=gnometarget pkgconfig
 
-atk_LIB_DEPENDS=	atk-1.0.301:${PORTSDIR}/devel/atk
+atk_LIB_DEPENDS=	atk-1.0.304:${PORTSDIR}/devel/atk
 atk_DETECT=		${LOCALBASE}/libdata/pkgconfig/atk.pc
 atk_USE_GNOME_IMPL=	glib20
 
