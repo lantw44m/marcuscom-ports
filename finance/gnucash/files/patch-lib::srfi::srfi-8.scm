ea63b7c72c612479caf224b5f52850b46254c2dc
adamw <adamw@df743ca5-7f9a-e211-a948-0013205c9059>
2003-09-21 20:21:45 +0000

    Add gnucash-1.8.7.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1235 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ finance/gnucash/files/patch-lib::srfi::srfi-8.scm
@@ -0,0 +1,15 @@
+
+$FreeBSD: ports/finance/gnucash/files/patch-lib::srfi::srfi-8.scm,v 1.2 2002/08/18 07:40:20 sobomax Exp $
+
+--- lib/srfi/srfi-8.scm.orig	Tue Jun 25 23:53:35 2002
++++ lib/srfi/srfi-8.scm	Sat Aug 17 22:34:33 2002
+@@ -22,7 +22,8 @@
+ (cond
+  ((or (string=? "1.3" (version))
+       (string=? "1.3.4" (version))
+-      (string=? "1.4" (substring (version) 0 3)))
++      (string=? "1.4" (substring (version) 0 3))
++      (string=? "1.4.1" (version)))
+   
+   (use-modules (ice-9 slib))
+   (require 'macro-by-example)
