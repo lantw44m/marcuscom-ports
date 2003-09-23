4fbb708a465f269834e6059b325cc338dd55e4ea
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-09-23 04:50:30 +0000

    Add gdm2, and update to 2.4.4.2.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1245 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11/gdm2/files/patch-daemon_gdm.c
@@ -0,0 +1,11 @@
+--- daemon/gdm.c.orig	Wed Jun 25 17:04:49 2003
++++ daemon/gdm.c	Sat Jun 28 18:17:44 2003
+@@ -38,7 +38,7 @@
+ #include <locale.h>
+ 
+ /* This should be moved to auth.c I suppose */
+-#include <X11/Xauth.h>
++/*#include <X11/Xauth.h>*/
+ 
+ #include <vicious.h>
+ 
