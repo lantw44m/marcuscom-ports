15df654d982b38236856d520e497c13c9aff8ec8
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-11-14 18:00:05 +0000

    Update to 2.5.1.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1326 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11/gnome-panel/files/patch-applets_clock_clock.c
@@ -0,0 +1,27 @@
+
+$FreeBSD: ports/x11/gnomepanel/files/patch-applets_clock_clock.c,v 1.1 2003/09/18 06:49:26 marcus Exp $
+
+--- applets/clock/clock.c	2002/05/14 14:08:16	1.1
++++ applets/clock/clock.c	2002/05/14 14:12:56
+@@ -14,7 +14,9 @@
+ #include <dirent.h>
+ #include <string.h>
+ #include <time.h>
++#ifdef HAVE_LANGINFO_H
+ #include <langinfo.h>
++#endif
+ 
+ #include <panel-applet.h>
+ #include <panel-applet-gconf.h>
+@@ -684,7 +686,11 @@
+ 	cd->hourformat = panel_applet_gconf_get_int (applet, KEY_HOUR_FORMAT, &error);
+ 	if (error || (cd->hourformat != 12 && cd->hourformat != 24)) {
+ 		/* if value is not valid, set it according to locale */
++#ifdef HAVE_LANGINFO_H
+ 		const char *am = nl_langinfo (AM_STR);
++#else
++		const char *am = "AM";
++#endif
+ 		cd->hourformat = (am[0] == '\0') ? 24 : 12;
+ 
+ 		if (error)
