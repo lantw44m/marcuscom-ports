fed3b4596bf8c0f4562474c65c6108c78bb67769
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-11-16 21:58:34 +0000

    Allow the enhanced mixer_info to work on -CURRENT where it's supported.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1337 df743ca5-7f9a-e211-a948-0013205c9059

--- multimedia/gstreamer-plugins/files/patch-sys_oss_gstossmixer.c
+++ multimedia/gstreamer-plugins/files/patch-sys_oss_gstossmixer.c
@@ -1,21 +1,26 @@
---- sys/oss/gstossmixer.c.orig	Sat Nov 15 02:02:25 2003
-+++ sys/oss/gstossmixer.c	Sat Nov 15 02:10:18 2003
-@@ -399,7 +399,6 @@
+--- sys/oss/gstossmixer.c.orig	Sun Nov 16 13:43:53 2003
++++ sys/oss/gstossmixer.c	Sun Nov 16 16:57:37 2003
+@@ -399,7 +399,9 @@
    gint i, devmask;
    const GList *pads = gst_element_get_pad_list (GST_ELEMENT (oss));
    GstPadDirection dir = GST_PAD_UNKNOWN;
--  struct mixer_info minfo;
++#ifdef SOUND_MIXER_INFO
+   struct mixer_info minfo;
++#endif
  
    g_return_if_fail (oss->mixer_fd == -1);
  
-@@ -428,9 +427,7 @@
+@@ -428,9 +430,13 @@
    }
  
    /* get name */
--  if (ioctl (oss->mixer_fd, SOUND_MIXER_INFO, &minfo) == 0) {
--    oss->device_name = g_strdup (minfo.name);
--  }
++#ifdef SOUND_MIXER_INFO
+   if (ioctl (oss->mixer_fd, SOUND_MIXER_INFO, &minfo) == 0) {
+     oss->device_name = g_strdup (minfo.name);
+   }
++#else
 +  oss->device_name = g_strdup ("FreeBSD Mixer");
++#endif
  
    /* build track list */
    for (i = 0; i < SOUND_MIXER_NRDEVICES; i++) {
