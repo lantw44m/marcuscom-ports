5ecdf1d42a4fbbb7c2de5595256beb36edb46931
adamw <adamw@df743ca5-7f9a-e211-a948-0013205c9059>
2003-12-05 15:35:27 +0000

    Track atk shared library version.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1394 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -221,7 +221,7 @@ glib20_LIB_DEPENDS=	glib-2.0.300:${PORTSDIR}/devel/glib20
 glib20_DETECT=		${LOCALBASE}/libdata/pkgconfig/glib-2.0.pc
 glib20_USE_GNOME_IMPL=gnometarget pkgconfig
 
-atk_LIB_DEPENDS=	atk-1.0.500:${PORTSDIR}/devel/atk
+atk_LIB_DEPENDS=	atk-1.0.501:${PORTSDIR}/devel/atk
 atk_DETECT=		${LOCALBASE}/libdata/pkgconfig/atk.pc
 atk_USE_GNOME_IMPL=	glib20
 
