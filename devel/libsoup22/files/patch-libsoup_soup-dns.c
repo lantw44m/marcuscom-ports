125eec50c0a27f12ed7a0d97ca36c1718300f6d1
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2003-12-29 23:02:48 +0000

    Update to 2.1.3.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1555 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/libsoup22/files/patch-libsoup_soup-dns.c
+++ devel/libsoup22/files/patch-libsoup_soup-dns.c
@@ -1,10 +1,15 @@
---- libsoup/soup-dns.c.orig	Tue Dec  9 03:27:29 2003
-+++ libsoup/soup-dns.c	Tue Dec  9 03:27:52 2003
-@@ -17,6 +17,7 @@
+--- libsoup/soup-dns.c.orig	Wed Dec 17 14:49:13 2003
++++ libsoup/soup-dns.c	Mon Dec 29 17:59:52 2003
+@@ -17,10 +17,11 @@
  #include <unistd.h>
  #include <sys/select.h>
  #include <sys/types.h>
 +#include <sys/uio.h>
  #include <sys/wait.h>
  
- #include <arpa/inet.h>
+-#include <arpa/inet.h>
+ #include <netinet/in.h>
++#include <arpa/inet.h>
+ 
+ #include "soup-dns.h"
+ #include "soup-misc.h"
