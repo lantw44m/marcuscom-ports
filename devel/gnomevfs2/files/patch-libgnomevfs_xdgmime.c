82e2c722bf4dd583198c3f45a638372e94136e01
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-01-13 06:57:04 +0000

    Use a simple assignment to NUL out the last byte of a string instead of memset.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1641 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnomevfs2/files/patch-libgnomevfs_xdgmime.c
+++ devel/gnomevfs2/files/patch-libgnomevfs_xdgmime.c
@@ -1,5 +1,5 @@
---- libgnomevfs/xdgmime.c.orig	Tue Jan 13 01:20:05 2004
-+++ libgnomevfs/xdgmime.c	Tue Jan 13 01:20:35 2004
+--- libgnomevfs/xdgmime.c.orig	Wed Jan  7 13:44:02 2004
++++ libgnomevfs/xdgmime.c	Tue Jan 13 01:55:41 2004
 @@ -97,7 +97,7 @@
  
        xdg_data_dirs = getenv ("XDG_DATA_DIRS");
@@ -9,13 +9,14 @@
  
        ptr = xdg_data_dirs;
  
-@@ -121,7 +121,8 @@
+@@ -121,8 +121,9 @@
  	    len = end_ptr - ptr;
  	  else
  	    len = end_ptr - ptr + 1;
 -	  dir = malloc (len);
 +	  dir = malloc (len + 1);
-+	  memset (dir, 0, len + 1);
  	  strncpy (dir, ptr, len);
++	  dir[len] = '\0';
  	  _xdg_mime_init_from_directory (dir);
  	  free (dir);
+ 
