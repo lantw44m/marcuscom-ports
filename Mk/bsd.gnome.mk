e75fadcdac3090ba48e44b7f224849bdee49951b
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-01-20 00:34:26 +0000

    Catch up with the new intltool component from the ports tree.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1688 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -26,7 +26,7 @@ Gnome_Include_MAINTAINER=	gnome@FreeBSD.org
 
 # non-version specific components
 _USE_GNOME_ALL=	gnomehack lthack gnomeprefix gnomehier esound gnomemimedata \
-		gnometarget pkgconfig intlhack
+		gnometarget pkgconfig intltool intlhack
 
 # GNOME 1 components
 _USE_GNOME_ALL+=libghttp glib12 gtk12 libxml gdkpixbuf imlib orbit \
@@ -234,7 +234,7 @@ pango_USE_GNOME_IMPL=	glib20
 
 gtk20_LIB_DEPENDS=	gtk-x11-2.0.301:${PORTSDIR}/x11-toolkits/gtk20
 gtk20_DETECT=		${X11BASE}/libdata/pkgconfig/gtk+-x11-2.0.pc
-gtk20_USE_GNOME_IMPL=	atk pango
+gtk20_USE_GNOME_IMPL=	intltool atk pango
 
 linc_LIB_DEPENDS=	linc.1:${PORTSDIR}/net/linc
 linc_DETECT=		${LOCALBASE}/libdata/pkgconfig/linc.pc
@@ -398,10 +398,13 @@ gstreamerplugins_LIB_DEPENDS=	gstplay-0.7.2:${PORTSDIR}/multimedia/gstreamer-plu
 gstreamerplugins_DETECT=	${X11BASE}/libdata/pkgconfig/gstreamer-play-0.7.pc
 gstreamerplugins_USE_GNOME_IMPL=	gconf2
 
-intlhack_BUILD_DEPENDS=	intltool-extract:${PORTSDIR}/textproc/intltool
+intltool_DETECT=		${LOCALBASE}/bin/intltool-extract
+intltool_BUILD_DEPENDS=	${intltool_DETECT}:${PORTSDIR}/textproc/intltool
+
 intlhack_PRE_PATCH=		${FIND} ${WRKSRC} -name "intltool-merge.in" | ${XARGS} ${REINPLACE_CMD} -e \
 				's|mkdir $$lang or|mkdir $$lang, 0777 or| ; \
 				 s|^push @INC, "/.*|push @INC, "${LOCALBASE}/share/intltool";|'
+intlhack_USE_GNOME_IMPL=intltool
 
 gtkhtml3_LIB_DEPENDS=	gtkhtml-3.1.5:${PORTSDIR}/www/gtkhtml3
 gtkhtml3_DETECT=		${X11BASE}/libdata/pkgconfig/libgtkhtml-3.1.pc
