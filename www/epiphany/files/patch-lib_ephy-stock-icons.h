199ff07b2bacd5fb3184fa175cf8f73089a0ee8d
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-02-01 22:19:30 +0000

    Fix the last broken Ephy icon.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1747 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ www/epiphany/files/patch-lib_ephy-stock-icons.h
@@ -0,0 +1,11 @@
+--- lib/ephy-stock-icons.h.orig	Sun Feb  1 17:21:00 2004
++++ lib/ephy-stock-icons.h	Sun Feb  1 17:21:30 2004
+@@ -33,7 +33,7 @@
+ #define STOCK_NEW_TAB		   "stock_new-tab"
+ #define STOCK_FULLSCREEN	   "stock_fullscreen"
+ #define STOCK_VIEW_SOURCE	   "stock_view-html-source"
+-#define STOCK_SEND_MAIL		   "stock_mail_send"
++#define STOCK_SEND_MAIL		   "stock_mail-send"
+ #define STOCK_ADD_BOOKMARK	   "stock_add-bookmark"
+ #define STOCK_PRINT_SETUP	   "stock_print-setup"
+ 
