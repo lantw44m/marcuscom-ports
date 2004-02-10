c35a57ab37118b0255ccbd085b5c194dd94e644d
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-02-10 01:28:58 +0000

    Update to 0.0.7.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1806 df743ca5-7f9a-e211-a948-0013205c9059

--- databases/evolution-data-server/files/patch-ltmain.sh
+++ databases/evolution-data-server/files/patch-ltmain.sh
@@ -1,15 +1,15 @@
---- ltmain.sh.orig	Sat Dec  6 14:47:45 2003
-+++ ltmain.sh	Tue Dec  9 12:35:43 2003
-@@ -1280,7 +1280,7 @@
+--- ltmain.sh.orig	Sat Feb  8 09:47:44 2003
++++ ltmain.sh	Mon Feb  9 20:07:47 2004
+@@ -1075,7 +1075,7 @@
  	  esac
- 	elif test "X$arg" = "X-lc_r"; then
- 	 case $host in
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
 -	 *-*-openbsd* | *-*-freebsd*)
 +	 *-*-openbsd* | *-*-freebsd4*)
- 	   # Do not include libc_r directly, use -pthread flag.
- 	   continue
- 	   ;;
-@@ -1290,8 +1290,16 @@
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -1085,8 +1085,16 @@
  	continue
  	;;
  
@@ -26,17 +26,17 @@
  	continue
  	;;
  
-@@ -3000,6 +3008,9 @@
- 	    # problems, so we reset it completely
- 	    verstring=
+@@ -2503,6 +2511,9 @@
+ 	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
  	    ;;
 +	  *-*-freebsd*)
 +	    # FreeBSD doesn't need this...
 +	    ;;
  	  *)
- 	    verstring="0.0"
- 	    ;;
-@@ -5428,10 +5439,12 @@
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4330,10 +4341,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
