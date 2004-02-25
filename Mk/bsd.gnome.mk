96692b97218b5894a9a3e1f7fbb23399385a3d35
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-02-25 05:51:19 +0000

    Chase the gtk20 shared lib version...again.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1892 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -233,7 +233,7 @@ pango_LIB_DEPENDS=	pango-1.0.300:${PORTSDIR}/x11-toolkits/pango
 pango_DETECT=		${X11BASE}/libdata/pkgconfig/pango.pc
 pango_USE_GNOME_IMPL=	glib20
 
-gtk20_LIB_DEPENDS=	gtk-x11-2.0.303:${PORTSDIR}/x11-toolkits/gtk20
+gtk20_LIB_DEPENDS=	gtk-x11-2.0.304:${PORTSDIR}/x11-toolkits/gtk20
 gtk20_DETECT=		${X11BASE}/libdata/pkgconfig/gtk+-x11-2.0.pc
 gtk20_USE_GNOME_IMPL=	intltool atk pango
 
