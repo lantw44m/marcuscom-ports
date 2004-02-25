a00f58724f1e9beafc2e1b01b2666199edb127ec
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-02-25 08:27:53 +0000

    Update to 2.5.8.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1910 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-fm/nautilus/files/patch-src_file-manager_fm-directory-view.c
+++ x11-fm/nautilus/files/patch-src_file-manager_fm-directory-view.c
@@ -1,6 +1,6 @@
---- src/file-manager/fm-directory-view.c.orig	Thu Oct  2 23:30:40 2003
-+++ src/file-manager/fm-directory-view.c	Thu Oct  2 23:52:04 2003
-@@ -4314,19 +4314,25 @@
+--- src/file-manager/fm-directory-view.c.orig	Tue Feb 24 08:17:24 2004
++++ src/file-manager/fm-directory-view.c	Wed Feb 25 02:43:55 2004
+@@ -4476,19 +4476,25 @@
  
  	nautilus_bonobo_set_hidden (view->details->ui, 
  				    FM_DIRECTORY_VIEW_MENU_PATH_SCRIPTS, 
@@ -29,16 +29,3 @@
  	nautilus_bonobo_set_hidden (view->details->ui, 
  				    FM_DIRECTORY_VIEW_POPUP_PATH_BACKGROUND_SCRIPTS_SEPARATOR, 
  				    !any_scripts);
-@@ -5149,10 +5149,11 @@
- 			GList *selection,
- 			gint selection_count)
- {
--	selection_count = g_list_length (selection);
- 	gboolean can_paste_files_into;
- 	gboolean selection_is_read_only;
- 	gboolean is_read_only;
-+
-+	selection_count = g_list_length (selection);
- 
- 	selection_is_read_only = selection_count == 1
- 		&& !nautilus_file_can_write (NAUTILUS_FILE (selection->data));
