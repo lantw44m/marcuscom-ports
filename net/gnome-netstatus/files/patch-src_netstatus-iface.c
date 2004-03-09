00f484dfff13efa56d97230824d719c291074e4f
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-03-09 03:26:35 +0000

    Update to 2.5.91.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@1984 df743ca5-7f9a-e211-a948-0013205c9059

--- net/gnome-netstatus/files/patch-src_netstatus-iface.c
+++ net/gnome-netstatus/files/patch-src_netstatus-iface.c
@@ -1,12 +1,15 @@
---- src/netstatus-iface.c.orig	Tue Jan 20 10:00:20 2004
-+++ src/netstatus-iface.c	Wed Feb  4 15:39:09 2004
-@@ -32,9 +32,12 @@
+--- src/netstatus-iface.c.orig	Wed Mar  3 08:07:35 2004
++++ src/netstatus-iface.c	Mon Mar  8 22:18:03 2004
+@@ -32,12 +32,15 @@
  
  #include <libgnome/gnome-i18n.h>
  
 +#include <sys/types.h>
 +#include <sys/socket.h>
  #include <sys/ioctl.h>
+ #ifdef HAVE_SYS_SOCKIO_H
+ #include <sys/sockio.h>
+ #endif /* HAVE_SYS_SOCKIO_H */
  #include <net/if.h>
  #include <net/if_arp.h>
 +#include <netinet/in.h>
