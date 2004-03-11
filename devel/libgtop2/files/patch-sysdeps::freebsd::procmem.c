c0dc5330922e65dd62dcfdcec3a961030946f392
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-03-11 21:35:58 +0000

    Update to 2.5.2.  There were a lot of consolidated patches, so this really needs to be tested out.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2033 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/libgtop2/files/patch-sysdeps::freebsd::procmem.c
+++ devel/libgtop2/files/patch-sysdeps::freebsd::procmem.c
@@ -1,6 +1,6 @@
---- sysdeps/freebsd/procmem.c.orig	Tue Oct 21 18:54:24 2003
-+++ sysdeps/freebsd/procmem.c	Sun Feb 15 15:43:11 2004
-@@ -117,7 +117,7 @@
+--- sysdeps/freebsd/procmem.c.orig	Wed Mar 10 21:23:52 2004
++++ sysdeps/freebsd/procmem.c	Thu Mar 11 15:46:05 2004
+@@ -125,7 +125,7 @@
  #else
  	struct vm_object object;
  #endif
@@ -9,32 +9,16 @@
  	int count;
  
  	glibtop_init_p (server, (1L << GLIBTOP_SYSDEPS_PROC_MEM), 0);
-@@ -136,16 +136,28 @@
- 		glibtop_warn_io_r (server, "kvm_getprocs (%d)", pid);
- 		return;
- 	}
-+#if __FreeBSD_version >= 500013
+@@ -160,15 +160,12 @@
+ 
+ #define        PROC_VMSPACE   kp_proc.p_vmspace
  
 -	if (kvm_read (server->machine.kd,
--		      (unsigned long) pinfo [0].kp_proc.p_limit,
+-		      (unsigned long) pinfo [0].PROC_VMSPACE,
 -		      (char *) &plimit, sizeof (plimit)) != sizeof (plimit)) {
 -		glibtop_warn_io_r (server, "kvm_read (plimit)");
-+#define		XXX_P_VMSPACE	ki_vmspace
-+	buf->rss_rlim = pinfo [0].ki_rssize;
-+
-+	buf->vsize = (u_int64_t) pinfo [0].ki_size;
-+	buf->size = (u_int64_t) pagetok
-+	    	(pinfo [0].ki_tsize + pinfo [0].ki_dsize + pinfo[0].ki_ssize)
-+			<< LOG1024;
-+	buf->resident = buf->rss = (u_int64_t) pagetok
-+	    	(pinfo [0].ki_rssize) << LOG1024;
-+
-+#else
-+
-+#define		XXX_P_VMSPACE	kp_proc.p_vmspace
-+
 +	if (getrlimit (RLIMIT_RSS, &rlimit) < 0) {
-+	    	glibtop_warn_io_r (server, "getrlimit");
++		glibtop_warn_io_r (server, "getrlimit");
  		return;
  	}
  
@@ -44,17 +28,3 @@
  	
  	vms = &pinfo [0].kp_eproc.e_vm;
  
-@@ -154,11 +166,12 @@
- 	
- 	buf->resident = buf->rss = (guint64) pagetok
- 		(vms->vm_rssize) << LOG1024;
-+#endif
- 
- 	/* Now we get the shared memory. */
- 
- 	if (kvm_read (server->machine.kd,
--		      (unsigned long) pinfo [0].kp_proc.p_vmspace,
-+		      (unsigned long) pinfo [0].XXX_P_VMSPACE,
- 		      (char *) &vmspace, sizeof (vmspace)) != sizeof (vmspace)) {
- 		glibtop_warn_io_r (server, "kvm_read (vmspace)");
- 		return;
