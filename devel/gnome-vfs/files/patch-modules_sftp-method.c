7e47faafdc9818f2c96133146e087c995b4346e5
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-04-04 16:59:17 +0000

    Get password authentication working for sftp on recent 5.X systems at least. Since 4.X lacks the UNIX98 terminal functions, it is unlikely that password auth will ever work correctly on those systems.  However, pure password-less publickey sftp does work.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2251 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnome-vfs/files/patch-modules_sftp-method.c
+++ devel/gnome-vfs/files/patch-modules_sftp-method.c
@@ -1,14 +1,5 @@
 --- modules/sftp-method.c.orig	Sat Apr  3 23:58:06 2004
 +++ modules/sftp-method.c	Sun Apr  4 00:00:38 2004
-@@ -72,7 +72,7 @@
- size_t default_req_len = 32768;
- guint max_req = 16;
- 
--#ifdef HAVE_GRANTPT
-+#if defined(HAVE_GRANTPT) && !defined(__FreeBSD__)
- /* We only use this on systems with unix98 ptys */
- #define USE_PTY 1
- #endif
 @@ -174,6 +174,15 @@
  /* Inspired by atomicio() from OpenSSH */
  
