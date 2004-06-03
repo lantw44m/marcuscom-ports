c78db757f81d6eca619788e19496d64062e91df9
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-06-03 19:31:35 +0000

    Use the patch from Evo CVS now that they have fixed the Tasks crash bug.
    Obtained from:	GNOME CVS
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2357 df743ca5-7f9a-e211-a948-0013205c9059

--- mail/evolution/files/patch-calendar_gui_e-tasks.c
+++ mail/evolution/files/patch-calendar_gui_e-tasks.c
@@ -1,11 +1,32 @@
---- calendar/gui/e-tasks.c.orig	Wed May 19 11:07:08 2004
-+++ calendar/gui/e-tasks.c	Wed Jun  2 14:45:05 2004
-@@ -558,6 +558,8 @@
+===================================================================
+RCS file: /cvs/gnome/evolution/calendar/gui/e-tasks.c,v
+retrieving revision 1.95
+retrieving revision 1.96
+diff -u -r1.95 -r1.96
+--- evolution/calendar/gui/e-tasks.c	2004/06/02 09:33:36	1.95
++++ evolution/calendar/gui/e-tasks.c	2004/06/03 17:54:24	1.96
+@@ -679,15 +679,18 @@
+ {
  	ETasksPrivate *priv;
  	va_list args;
- 	char sz[2048];
+-	char sz[2048];
+-	
+-	va_start (args, message);
+-	vsnprintf (sz, sizeof sz, message, args);
+-	va_end (args);
++	char sz[2048], *msg_string = NULL;
 +
-+	g_return_if_fail (message != NULL);
++	if (message) {
++		va_start (args, message);
++		vsnprintf (sz, sizeof sz, message, args);
++		va_end (args);
++		msg_string = sz;
++	}
+ 
+ 	priv = tasks->priv;
  	
- 	va_start (args, message);
- 	vsnprintf (sz, sizeof sz, message, args);
+-	e_calendar_table_set_status_message (E_CALENDAR_TABLE (priv->tasks_view), sz);
++	e_calendar_table_set_status_message (E_CALENDAR_TABLE (priv->tasks_view), msg_string);
+ }
+ 
+ /* Callback from the calendar client when an error occurs in the backend */
