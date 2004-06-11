a56aee7edf9fa546b49edfdba8c68336060d4f14
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-06-11 05:20:07 +0000

    Tab-align some macros values, and add a comment about changing the pre-patch target to something else.  We can't use pre-patch since it conflicts with bsd.gnome.mk.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2391 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.mozilla.mk
+++ Mk/bsd.mozilla.mk
@@ -4,7 +4,7 @@
 # $FreeBSD$
 #	$NetBSD: $
 #	
-# MarcusCOM: $Id: bsd.mozilla.mk,v 1.1 2004-06-11 01:10:40 adamw Exp $
+# MarcusCOM: $Id: bsd.mozilla.mk,v 1.2 2004-06-11 05:20:07 marcus Exp $
 #
 # 4 column tabs prevent hair loss and tooth decay!
 
@@ -25,23 +25,23 @@ Mozilla_Pre_Include=			bsd.mozilla.mk
 # MOZILLA_INCLUDE_DIR:  for use in --with-mozilla-includes="${MOZILLA_INCLUDE_DIR}"
 # MOZILLA_LIB_DIR:      for use in --with-mozilla-libs="${MOZILLA_LIB_DIR}"
 
-MOZ_DEVEL_SNAPSHOT_VER=		1.7rc2
-MOZ_STABLE_SNAPSHOT_VER=	1.6
-MOZ_GTK1_DEVEL_SNAPSHOT_VER=trunk
-MOZ_GTK1_STABLE_SNAPSHOT_VER=trunk
+MOZ_DEVEL_SNAPSHOT_VER=			1.7rc2
+MOZ_STABLE_SNAPSHOT_VER=		1.6
+MOZ_GTK1_DEVEL_SNAPSHOT_VER=	trunk
+MOZ_GTK1_STABLE_SNAPSHOT_VER=	trunk
 
 .if defined(USE_MOZILLA_GTK1)
 
 .if ${WITH_MOZILLA}=="mozilla-devel" || ${WITH_MOZILLA}=="mozilla-devel-gtk1" || \
 	exists(${X11BASE}/bin/mozilla-devel)
-BUILD_DEPENDS+=	${X11BASE}/lib/mozilla-devel-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
-RUN_DEPENDS+=	${X11BASE}/lib/mozilla-devel-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
+BUILD_DEPENDS+=			${X11BASE}/lib/mozilla-devel-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
+RUN_DEPENDS+=			${X11BASE}/lib/mozilla-devel-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
 MOZILLA_SNAPSHOT_VER=	${MOZ_GTK1_DEVEL_SNAPSHOT_VER}
 MOZILLA_INCLUDE_DIR=	${X11BASE}/include/mozilla-devel-gtk1
 MOZILLA_LIB_DIR=		${X11BASE}/lib/mozilla-devel-gtk1
 .else
-BUILD_DEPENDS+=	${X11BASE}/lib/mozilla-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
-RUN_DEPENDS+=	${X11BASE}/lib/mozilla-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
+BUILD_DEPENDS+=			${X11BASE}/lib/mozilla-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
+RUN_DEPENDS+=			${X11BASE}/lib/mozilla-gtk1/components/libgtkembedmoz.so:${PORTSDIR}/www/mozilla-devel-gtk1
 MOZILLA_SNAPSHOT_VER=	${MOZ_GTK1_STABLE_SNAPSHOT_VER}
 MOZILLA_INCLUDE_DIR=	${X11BASE}/include/mozilla-gtk1
 MOZILLA_LIB_DIR=		${X11BASE}/lib/mozilla-gtk1
@@ -51,11 +51,12 @@ MOZILLA_LIB_DIR=		${X11BASE}/lib/mozilla-gtk1
 
 .if ${WITH_MOZILLA}=="mozilla-devel" || ${WITH_MOZILLA}=="mozilla-devel-gtk2" || \
 	exists(${X11BASE}/bin/mozilla-devel)
-BUILD_DEPENDS+=	${X11BASE}/lib/mozilla-devel/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla-devel
-RUN_DEPENDS+=	${X11BASE}/lib/mozilla-devel/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla-devel
+BUILD_DEPENDS+=			${X11BASE}/lib/mozilla-devel/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla-devel
+RUN_DEPENDS+=			${X11BASE}/lib/mozilla-devel/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla-devel
 MOZILLA_SNAPSHOT_VER=	${MOZ_DEVEL_SNAPSHOT_VER}
 MOZILLA_INCLUDE_DIR=	${X11BASE}/include/mozilla-devel
 MOZILLA_LIB_DIR=		${X11BASE}/lib/mozilla-devel
+# XXX We can't use pre-patch here, since it conflicts with bsd.gnome.mk
 pre-patch:
 	@${REINPLACE_CMD} -e "s|mozilla-gtkmozembed|mozilla-gtkmozembed-devel|g; \
 		s|mozilla-xpcom|mozilla-xpcom-devel|g; \
@@ -63,8 +64,8 @@ pre-patch:
 		s|mozilla-nss|mozilla-nss-devel|g; \
 		s|mozilla-plugin|mozilla-plugin-devel|g" ${WRKSRC}/configure
 .else
-BUILD_DEPENDS+=	${X11BASE}/lib/mozilla/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla
-RUN_DEPENDS+=	${X11BASE}/lib/mozilla/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla
+BUILD_DEPENDS+=			${X11BASE}/lib/mozilla/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla
+RUN_DEPENDS+=			${X11BASE}/lib/mozilla/components/libwidget_gtk2.so:${PORTSDIR}/www/mozilla
 MOZILLA_SNAPSHOT_VER=	${MOZ_STABLE_SNAPSHOT_VER}
 MOZILLA_INCLUDE_DIR=	${X11BASE}/include/mozilla
 MOZILLA_LIB_DIR=		${X11BASE}/lib/mozilla
