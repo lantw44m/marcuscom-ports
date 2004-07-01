5fa79f6a35f23b161343e858c681e723a0bc516d
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-07-01 07:21:41 +0000

    We'll need this patch to fix the 4.X build with howl once the howl include thing gets sorted out.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2430 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-dns-sd.c
+++ devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-dns-sd.c
@@ -1,5 +1,5 @@
---- libgnomevfs/gnome-vfs-dns-sd.c.orig	Fri Jun 18 13:20:41 2004
-+++ libgnomevfs/gnome-vfs-dns-sd.c	Fri Jun 18 13:21:20 2004
+--- libgnomevfs/gnome-vfs-dns-sd.c.orig	Fri Jun 18 05:20:48 2004
++++ libgnomevfs/gnome-vfs-dns-sd.c	Thu Jul  1 02:39:34 2004
 @@ -24,6 +24,9 @@
  #include <config.h>
  
@@ -10,3 +10,13 @@
  #include <resolv.h>
  #include <string.h>
  #include "gnome-vfs-dns-sd.h"
+@@ -684,8 +687,8 @@
+ 	    gpointer     callback_data)
+ {
+ 	sw_discovery session;
+-	session = callback_data;
+ 	sw_salt salt;
++	session = callback_data;
+ 
+ 	if (sw_discovery_salt (session, &salt) == SW_OKAY) {
+ 		sw_salt_lock (salt);
