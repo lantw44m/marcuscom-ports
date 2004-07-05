ba1139e36237156134c21358acc11c6df526d6b1
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-07-05 20:48:14 +0000

    Update to 2.7.3.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2436 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-dns-sd.c
+++ devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-dns-sd.c
@@ -1,16 +1,6 @@
---- libgnomevfs/gnome-vfs-dns-sd.c.orig	Fri Jun 18 05:20:48 2004
-+++ libgnomevfs/gnome-vfs-dns-sd.c	Thu Jul  1 02:39:34 2004
-@@ -24,6 +24,9 @@
- #include <config.h>
- 
- #include <sys/time.h>
-+#include <sys/types.h>
-+#include <netinet/in.h>
-+#include <arpa/nameser.h>
- #include <resolv.h>
- #include <string.h>
- #include "gnome-vfs-dns-sd.h"
-@@ -684,8 +687,8 @@
+--- libgnomevfs/gnome-vfs-dns-sd.c.orig	Mon Jul  5 16:39:19 2004
++++ libgnomevfs/gnome-vfs-dns-sd.c	Mon Jul  5 16:39:33 2004
+@@ -694,8 +694,8 @@
  	    gpointer     callback_data)
  {
  	sw_discovery session;
