df7c2096309352fa1b691e023610f84bc0ccb077
adamw <adamw@df743ca5-7f9a-e211-a948-0013205c9059>
2004-07-15 01:03:07 +0000

    Update to 0.9.6.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2477 df743ca5-7f9a-e211-a948-0013205c9059

--- accessibility/gnopernicus/files/patch-srcore_srbrl.c
+++ accessibility/gnopernicus/files/patch-srcore_srbrl.c
@@ -1,38 +1,39 @@
---- srcore/srbrl.c.orig	Sun May 30 03:10:22 2004
-+++ srcore/srbrl.c	Sun May 30 03:11:59 2004
-@@ -724,6 +724,11 @@
+--- srcore/srbrl.c.orig	Wed Jul 14 20:51:31 2004
++++ srcore/srbrl.c	Wed Jul 14 20:55:57 2004
+@@ -723,6 +723,12 @@
+ static void
  src_braille_load_values (SRCBraille *braille)
  {
-     gint default_int_val;
 +    gint default_pos_sensor;
 +    gint default_opt_sensor;
 +    gchar *default_braille_transaltion_table;
 +    gchar *default_braille_style;
 +    gchar *default_braille_cursor_style;
-     
++
      sru_assert (braille);
      
-@@ -751,7 +756,7 @@
+     if (!braille->device)
+@@ -749,7 +755,7 @@
  	}					  
      }    
      
 -    gchar *default_braille_transaltion_table = DEFAULT_BRAILLE_TRANSLATION;
-+    default_braille_transaltion_table = DEFAULT_BRAILLE_TRANSLATION;
++    *default_braille_transaltion_table = DEFAULT_BRAILLE_TRANSLATION;
      GET_BRAILLE_CONFIG_DATA_WITH_DEFAULT (BRAILLE_TRANSLATION, CFGT_STRING, 
  					  &braille->translation_table, 
  					  default_braille_transaltion_table);		
-@@ -763,22 +768,22 @@
+@@ -761,22 +767,22 @@
      }
  	
      
 -    gchar *default_braille_style = DEFAULT_BRAILLE_STYLE;
-+    default_braille_style = DEFAULT_BRAILLE_STYLE;
++    *default_braille_style = DEFAULT_BRAILLE_STYLE;
      GET_BRAILLE_CONFIG_DATA_WITH_DEFAULT (BRAILLE_STYLE, CFGT_STRING, 
  					  &braille->style, 
  					  default_braille_style);
  					  
 -    gchar *default_braille_cursor_style = DEFAULT_BRAILLE_CURSOR_STYLE;
-+    default_braille_cursor_style = DEFAULT_BRAILLE_CURSOR_STYLE;
++    *default_braille_cursor_style = DEFAULT_BRAILLE_CURSOR_STYLE;
      GET_BRAILLE_CONFIG_DATA_WITH_DEFAULT (BRAILLE_CURSOR_STYLE, CFGT_STRING, 
  					  &braille->cursor_style, 
  					  default_braille_cursor_style);
