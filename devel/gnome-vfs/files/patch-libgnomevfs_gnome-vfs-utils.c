82b47af81d1849795aaa2b7b0d2dcea64798c78c
adamw <adamw@df743ca5-7f9a-e211-a948-0013205c9059>
2004-07-31 22:00:42 +0000

    Update to 2.7.90.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2577 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-utils.c
+++ devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-utils.c
@@ -1,5 +1,5 @@
---- libgnomevfs/gnome-vfs-utils.c.orig	Fri Apr 16 06:43:05 2004
-+++ libgnomevfs/gnome-vfs-utils.c	Mon Apr 19 15:41:47 2004
+--- libgnomevfs/gnome-vfs-utils.c.orig	Sat Jul 31 17:54:38 2004
++++ libgnomevfs/gnome-vfs-utils.c	Sat Jul 31 17:56:05 2004
 @@ -797,7 +797,9 @@
  	
  #if HAVE_STATVFS
@@ -10,16 +10,16 @@
  #if STATFS_ARGS == 2
  	statfs_result = statfs (unescaped_path, &statfs_buffer);   
  #elif STATFS_ARGS == 4
-@@ -838,7 +840,7 @@
- 	}
+@@ -839,7 +841,7 @@
+ #else 	
+ 	g_free (unescaped_path);
  #endif
- 
 -	block_size = statfs_buffer.f_bsize; 
 +	block_size = statfs_buffer.statfs_bsize; 
  	free_blocks = statfs_buffer.f_bavail;
  
  	*size = block_size * free_blocks;
-@@ -866,7 +868,7 @@
+@@ -867,7 +869,7 @@
  	paths = g_strsplit (gnome_var, ":", 0); 
  
  	for (temp_paths = paths; *temp_paths != NULL; temp_paths++) {
--- devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-resolve.c
+++ devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-resolve.c
@@ -1,13 +1,13 @@
---- libgnomevfs/gnome-vfs-resolve.c.orig	Tue Jul 20 14:39:26 2004
-+++ libgnomevfs/gnome-vfs-resolve.c	Thu Jul 22 16:38:35 2004
-@@ -133,8 +133,10 @@
- 	switch (error) {
- 
- 	case EAI_NONAME: return GNOME_VFS_ERROR_HOST_NOT_FOUND;
+--- libgnomevfs/gnome-vfs-resolve.c.orig	Sat Jul 31 17:52:23 2004
++++ libgnomevfs/gnome-vfs-resolve.c	Sat Jul 31 17:52:45 2004
+@@ -171,8 +171,10 @@
+ 	   switch (error) {
+ 			 
+ 	   case EAI_NONAME: return GNOME_VFS_ERROR_HOST_NOT_FOUND;
 +#if defined(EAI_ADDRFAMILY) && defined(EAI_NODATA)
- 	case EAI_ADDRFAMILY:		
- 	case EAI_NODATA: return GNOME_VFS_ERROR_HOST_HAS_NO_ADDRESS;
+ 	   case EAI_ADDRFAMILY:		
+ 	   case EAI_NODATA: return GNOME_VFS_ERROR_HOST_HAS_NO_ADDRESS;
 +#endif
- 	case EAI_SYSTEM: return gnome_vfs_result_from_errno ();
- 	case EAI_FAIL:
- 	case EAI_AGAIN: return GNOME_VFS_ERROR_NAMESERVER;
+ 	   case EAI_SYSTEM: return gnome_vfs_result_from_errno ();
+ 	   case EAI_FAIL:
+ 	   case EAI_AGAIN: return GNOME_VFS_ERROR_NAMESERVER;
