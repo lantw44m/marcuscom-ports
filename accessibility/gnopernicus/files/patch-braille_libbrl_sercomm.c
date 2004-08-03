9e38652358f403adc7bf64d71108e00bda6b6963
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-08-03 18:50:27 +0000

    Fix build on 4.X, and use a fix from gnopernicus CVS to fix the previous build issue.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2602 df743ca5-7f9a-e211-a948-0013205c9059

--- accessibility/gnopernicus/files/patch-braille_libbrl_sercomm.c
+++ accessibility/gnopernicus/files/patch-braille_libbrl_sercomm.c
@@ -1,14 +1,11 @@
---- braille/libbrl/sercomm.c.orig	Tue Aug  3 11:12:04 2004
-+++ braille/libbrl/sercomm.c	Tue Aug  3 11:17:36 2004
-@@ -370,10 +370,9 @@
+--- braille/libbrl/sercomm.c.orig	Mon Aug  2 13:13:53 2004
++++ braille/libbrl/sercomm.c	Tue Aug  3 14:49:40 2004
+@@ -370,7 +370,7 @@
  static gboolean 
  brl_ser_glib_cb (GIOChannel   *source, 
  		 GIOCondition condition, 
 -		 gpointer     data)
-+		 guchar       *data)
++		 gpointer     unused)
  {			
      gint n, i;
--    guchar data[256];
- 	
-     if (!glib_poll) 
- 	return FALSE;
+     guchar data[256];
