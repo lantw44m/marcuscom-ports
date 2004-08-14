18b72d5de7bae644b951a2a277b631a5fb4a104c
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2004-08-14 02:58:54 +0000

    -Update to 2.1.5.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2646 df743ca5-7f9a-e211-a948-0013205c9059

--- editors/abiword-plugins/files/patch-tools::aiksaurus::xp::AiksaurusABI.cpp
+++ editors/abiword-plugins/files/patch-tools::aiksaurus::xp::AiksaurusABI.cpp
@@ -1,6 +1,6 @@
---- tools/aiksaurus/xp/AiksaurusABI.cpp.orig	Sun Jun 15 07:09:42 2003
-+++ tools/aiksaurus/xp/AiksaurusABI.cpp	Fri Jul 23 12:19:18 2004
-@@ -310,13 +310,13 @@
+--- tools/aiksaurus/xp/AiksaurusABI.cpp.orig	Fri Aug 13 21:09:34 2004
++++ tools/aiksaurus/xp/AiksaurusABI.cpp	Fri Aug 13 21:10:33 2004
+@@ -310,11 +310,11 @@
      pView->extSelTo(FV_DOCPOS_EOW_SELECT);  
  	
      // Now we will figure out what word to look up when we open our dialog.
@@ -9,12 +9,8 @@
      if (!pView->isSelectionEmpty())
      {
          // We need to get the Ascii version of the current word.
--        search = AiksaurusABI_ucsToAscii(
--					 pView->getSelectionText()
--        );
-+        UT_UCS4Char *ucs4ST;
-+        pView->getSelectionText(*&ucs4ST);
-+        search = AiksaurusABI_ucsToAscii(ucs4ST);
-     }
- 
-     // Now we will run the thesaurus dialog and get a response.
+-        UT_UCS4Char * ucs4ST;
++	 UT_UCS4Char * ucs4ST;
+ 	 pView->getSelectionText(*&ucs4ST);
+ 	 search = AiksaurusABI_ucsToAscii(
+ 					 ucs4ST
