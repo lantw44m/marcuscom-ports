a071214799c33d1f21d8f42a1825f7d082911bb1
adamw <adamw@df743ca5-7f9a-e211-a948-0013205c9059>
2004-08-16 15:24:41 +0000

    Update to 2.7.91.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2669 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-utils.c
+++ devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-utils.c
@@ -1,25 +1,6 @@
---- libgnomevfs/gnome-vfs-utils.c.orig	Sat Jul 31 17:54:38 2004
-+++ libgnomevfs/gnome-vfs-utils.c	Sat Jul 31 17:56:05 2004
-@@ -797,7 +797,9 @@
- 	
- #if HAVE_STATVFS
- 	statfs_result = statvfs (unescaped_path, &statfs_buffer);
-+#define statfs_bsize f_frsize
- #else
-+#define statfs_bsize f_bsize
- #if STATFS_ARGS == 2
- 	statfs_result = statfs (unescaped_path, &statfs_buffer);   
- #elif STATFS_ARGS == 4
-@@ -839,7 +841,7 @@
- #else 	
- 	g_free (unescaped_path);
- #endif
--	block_size = statfs_buffer.f_bsize; 
-+	block_size = statfs_buffer.statfs_bsize; 
- 	free_blocks = statfs_buffer.f_bavail;
- 
- 	*size = block_size * free_blocks;
-@@ -867,7 +869,7 @@
+--- libgnomevfs/gnome-vfs-utils.c.orig	Mon Aug 16 11:11:09 2004
++++ libgnomevfs/gnome-vfs-utils.c	Mon Aug 16 11:11:16 2004
+@@ -871,7 +871,7 @@
  	paths = g_strsplit (gnome_var, ":", 0); 
  
  	for (temp_paths = paths; *temp_paths != NULL; temp_paths++) {
