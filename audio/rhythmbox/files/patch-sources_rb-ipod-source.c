42955dfe06a38fd27ef295ce6d8bf11b5c738599
adamw <adamw@df743ca5-7f9a-e211-a948-0013205c9059>
2004-09-19 21:06:20 +0000

    The new disc number code causes rb to crash when reading metadata from itunes.db, so comment it out until a proper fix is committed.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2873 df743ca5-7f9a-e211-a948-0013205c9059

--- audio/rhythmbox/files/patch-sources_rb-ipod-source.c
+++ audio/rhythmbox/files/patch-sources_rb-ipod-source.c
@@ -1,11 +1,18 @@
---- sources/rb-ipod-source.c.orig	Sun Sep 19 16:50:39 2004
-+++ sources/rb-ipod-source.c	Sun Sep 19 16:51:22 2004
-@@ -238,7 +238,7 @@
+--- sources/rb-ipod-source.c.orig	Sun Sep 19 17:05:37 2004
++++ sources/rb-ipod-source.c	Sun Sep 19 17:05:49 2004
+@@ -234,6 +234,7 @@
+ 		}
+ 
+ 		/* Set disc number */
++		/*
+ 		if (song->cd_nr != 0) {
  			GValue value = {0, };
  			g_value_init (&value, G_TYPE_ULONG);
- 			g_value_set_ulong (&value, song->cd_nr);
--			rhythmdb_entry_set (RHYTHMDB (db), entry, 
-+			rhythmdb_entry_set (RHYTHMDB (ctxt->db), entry, 
- 					    RHYTHMDB_PROP_DISC_NUMBER, 
+@@ -243,6 +244,7 @@
  					    &value);
  			g_value_unset (&value);
+ 		}
++		*/
+ 		
+ 		/* Set bitrate */
+ 		if (song->bitrate != 0) {
