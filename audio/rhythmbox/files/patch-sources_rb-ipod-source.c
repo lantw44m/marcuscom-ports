0f318b8a182a86b269dc251c36cf1b389943f897
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-09-20 00:00:02 +0000

    Blindly attempt to fix the recent iPod crash.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@2874 df743ca5-7f9a-e211-a948-0013205c9059

--- audio/rhythmbox/files/patch-sources_rb-ipod-source.c
+++ audio/rhythmbox/files/patch-sources_rb-ipod-source.c
@@ -1,18 +1,11 @@
---- sources/rb-ipod-source.c.orig	Sun Sep 19 17:05:37 2004
-+++ sources/rb-ipod-source.c	Sun Sep 19 17:05:49 2004
-@@ -234,6 +234,7 @@
- 		}
- 
- 		/* Set disc number */
-+		/*
- 		if (song->cd_nr != 0) {
+--- sources/rb-ipod-source.c.orig	Thu Sep 16 18:03:09 2004
++++ sources/rb-ipod-source.c	Sun Sep 19 20:00:17 2004
+@@ -238,7 +238,7 @@
  			GValue value = {0, };
  			g_value_init (&value, G_TYPE_ULONG);
-@@ -243,6 +244,7 @@
+ 			g_value_set_ulong (&value, song->cd_nr);
+-			rhythmdb_entry_set (RHYTHMDB (db), entry, 
++			entry_set_locked (RHYTHMDB (ctxt->db), entry, 
+ 					    RHYTHMDB_PROP_DISC_NUMBER, 
  					    &value);
  			g_value_unset (&value);
- 		}
-+		*/
- 		
- 		/* Set bitrate */
- 		if (song->bitrate != 0) {
