0470fe8a4adaccb216c0ce4901e0d726951220c8
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-11-23 22:34:31 +0000

    Reset $FreeBSD$ tags.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3108 df743ca5-7f9a-e211-a948-0013205c9059

--- mail/evolution/files/patch-camel::camel-store.h
+++ mail/evolution/files/patch-camel::camel-store.h
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/mail/evolution/files/patch-camel::camel-store.h,v 1.3 2004/11/07 22:24:18 marcus Exp $
+$FreeBSD$
 
 --- camel/camel-store.h	2001/09/04 16:45:47	1.1
 +++ camel/camel-store.h	2001/09/04 16:46:32
