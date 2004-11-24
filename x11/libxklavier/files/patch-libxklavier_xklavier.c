689de8d75b6102b7b7e211854f45675d6e1babfc
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-11-24 00:57:05 +0000

    Fix build on 5.X and later.
    Reported by:	mezz
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3114 df743ca5-7f9a-e211-a948-0013205c9059

--- x11/libxklavier/files/patch-libxklavier_xklavier.c
+++ x11/libxklavier/files/patch-libxklavier_xklavier.c
@@ -1,5 +1,14 @@
---- libxklavier/xklavier.c.orig	Tue Nov 23 18:32:29 2004
-+++ libxklavier/xklavier.c	Tue Nov 23 18:32:48 2004
+--- libxklavier/xklavier.c.orig	Tue Nov  2 16:58:48 2004
++++ libxklavier/xklavier.c	Tue Nov 23 19:56:29 2004
+@@ -536,7 +536,7 @@
+                             int level, const char format[], va_list args )
+ {
+   time_t now = time( NULL );
+-  fprintf( stdout, "[%08ld,%03d,%s:%s/] \t", now, level, file, function );
++  fprintf( stdout, "[%08ld,%03d,%s:%s/] \t", (long) now, level, file, function );
+   vfprintf( stdout, format, args );
+ }
+ 
 @@ -700,9 +700,10 @@
  {
    if ( xklVTable == NULL )
