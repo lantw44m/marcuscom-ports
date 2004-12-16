ff19727f6483a5cfb523702c37086b0d67aaaa2b
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-12-16 22:58:23 +0000

    Update to 2.6.0.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3256 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-toolkits/gtk20/files/patch-gtk_gtkiconcache.c
+++ x11-toolkits/gtk20/files/patch-gtk_gtkiconcache.c
@@ -1,16 +1,16 @@
---- gtk/gtkiconcache.c.orig	Mon Nov 22 21:39:32 2004
-+++ gtk/gtkiconcache.c	Mon Nov 22 21:39:53 2004
-@@ -20,6 +20,8 @@
- #include <config.h>
+--- gtk/gtkiconcache.c.orig	Sun Dec 12 16:09:13 2004
++++ gtk/gtkiconcache.c	Thu Dec 16 17:09:23 2004
+@@ -21,6 +21,8 @@
  #include "gtkdebug.h"
  #include "gtkiconcache.h"
+ #include <glib/gstdio.h>
 +#include <sys/types.h>
 +#include <sys/stat.h>
  
  #ifdef HAVE_MMAP
  #include <sys/mman.h>
-@@ -27,8 +29,6 @@
- #ifdef G_OS_WIN32
+@@ -29,8 +31,6 @@
+ #include <windows.h>
  #include <io.h>
  #endif
 -#include <sys/types.h>
