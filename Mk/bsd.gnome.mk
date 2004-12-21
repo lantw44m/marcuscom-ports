06b5f94460721d812c7fa5677900142ac7771958
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2004-12-21 00:25:30 +0000

    Sync with FreeBSD ports by add two more hacks of mime.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3274 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -71,6 +71,8 @@ gnomehack_PRE_PATCH=	${FIND} ${WRKSRC} -name "Makefile.in*" | ${XARGS} ${REINPLA
 				 s|[(]datadir[)]/gnome/|(datadir)/|g ; \
 				 s|[(]datadir[)]/mime/|(prefix)/share/mime/|g ; \
 				 s|[(]datadir[)]/mime"|(prefix)/share/mime"|g ; \
+				 s|[(]datadir[)]/mime;|(prefix)/share/mime;|g ; \
+				 s|[(]datadir[)]/mime$$|(prefix)/share/mime|g ; \
 				 s|[(]libdir[)]/pkgconfig|(prefix)/libdata/pkgconfig|g ; \
 				 s|[$$][(]localstatedir[)]/scrollkeeper|${SCROLLKEEPER_DIR}|g ; \
 				 s|[(]datadir[)]/icons/hicolor|(prefix)/share/icons/hicolor|g ; \
