9a4ff43500dca6b93366639c0e5bbbf873a4b71d
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2004-12-26 18:42:43 +0000

    Fix a patch by correct the path.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3331 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-themes/gtk-engines2/files/patch-engines_crux_src_crux-pixmaps.c
+++ x11-themes/gtk-engines2/files/patch-engines_crux_src_crux-pixmaps.c
@@ -1,5 +1,5 @@
---- engines/crux/crux-pixmaps.c.orig	Thu Dec 23 16:13:44 2004
-+++ engines/crux/crux-pixmaps.c	Thu Dec 23 16:14:09 2004
+--- engines/crux/src/crux-pixmaps.c.orig	Thu Dec 23 16:13:44 2004
++++ engines/crux/src/crux-pixmaps.c	Thu Dec 23 16:14:09 2004
 @@ -329,8 +329,9 @@
  static GdkPixbuf *
  load_image (const char *file)
