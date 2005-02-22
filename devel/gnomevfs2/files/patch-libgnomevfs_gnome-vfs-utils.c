a7eeacca61584e34b5d230a08c8fee1443914696
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-02-22 23:06:30 +0000

    Revert a local patch that crept into my last commit.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3696 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-utils.c
+++ devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-utils.c
@@ -1,6 +1,6 @@
---- libgnomevfs/gnome-vfs-utils.c.orig	Thu Feb 10 05:00:21 2005
-+++ libgnomevfs/gnome-vfs-utils.c	Mon Feb 21 16:32:20 2005
-@@ -880,7 +880,7 @@ gnome_vfs_icon_path_from_filename (const
+--- libgnomevfs/gnome-vfs-utils.c.orig	Mon Aug 16 11:11:09 2004
++++ libgnomevfs/gnome-vfs-utils.c	Mon Aug 16 11:11:16 2004
+@@ -871,7 +871,7 @@
  	paths = g_strsplit (gnome_var, ":", 0); 
  
  	for (temp_paths = paths; *temp_paths != NULL; temp_paths++) {
@@ -9,22 +9,3 @@
  		if (g_file_test (full_filename, G_FILE_TEST_EXISTS)) {
  			g_strfreev (paths);
  			return full_filename;
-@@ -1083,6 +1083,9 @@ gnome_vfs_read_entire_file (const char *
- 		total_bytes_read += bytes_read;
- 	} while (result == GNOME_VFS_OK);
- 
-+	buffer = g_realloc (buffer, total_bytes_read + 1);
-+	buffer[total_bytes_read] = '\0';
-+
- 	/* Close the file. */
- 	result = gnome_vfs_close (handle);
- 	if (result != GNOME_VFS_OK) {
-@@ -1092,7 +1095,7 @@ gnome_vfs_read_entire_file (const char *
- 
- 	/* Return the file. */
- 	*file_size = total_bytes_read;
--	*file_contents = g_realloc (buffer, total_bytes_read);
-+	*file_contents = g_realloc (buffer, total_bytes_read + 1);
- 	return GNOME_VFS_OK;
- }
- 
