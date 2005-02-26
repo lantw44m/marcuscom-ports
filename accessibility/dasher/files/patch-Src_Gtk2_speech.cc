9481caeac4bc2b613539c3964b4fe3d284da3aae
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-02-26 06:01:48 +0000

    Be more aggressive when checking if we need to free voices.  This fixes a crash on quit.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3708 df743ca5-7f9a-e211-a948-0013205c9059

--- accessibility/dasher/files/patch-Src_Gtk2_speech.cc
+++ accessibility/dasher/files/patch-Src_Gtk2_speech.cc
@@ -1,11 +1,11 @@
---- Src/Gtk2/speech.cc.orig	Wed Feb 23 02:30:15 2005
-+++ Src/Gtk2/speech.cc	Wed Feb 23 02:30:44 2005
+--- Src/Gtk2/speech.cc.orig	Fri Sep 17 06:53:53 2004
++++ Src/Gtk2/speech.cc	Sat Feb 26 01:00:12 2005
 @@ -78,7 +78,8 @@
  void teardown_speech() {
  
    bonobo_object_release_unref (speaker, NULL);
 -  CORBA_free (voices);
-+  if (voices != NULL && voices->_length != 0)
++  if (voices != NULL && !BONOBO_EX (&ev) && voices->_length != 0)
 +    CORBA_free (voices);
    CORBA_exception_free (&ev);
  
