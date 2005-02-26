f42bfc1a2b8eb3d4d4aac908bc335f168abeee60
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-02-26 05:52:31 +0000

    Add support for CPU, Network, and Disk applet load monitors.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3707 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/libgtop2/files/patch-sysdeps_common_fsusage.c
+++ devel/libgtop2/files/patch-sysdeps_common_fsusage.c
@@ -1,6 +1,22 @@
---- sysdeps/common/fsusage.c.orig	Sun Oct 17 00:51:26 2004
-+++ sysdeps/common/fsusage.c	Sun Oct 17 00:52:07 2004
-@@ -253,8 +253,9 @@
+--- sysdeps/common/fsusage.c.orig	Thu Dec 23 16:56:10 2004
++++ sysdeps/common/fsusage.c	Fri Feb 25 21:55:06 2005
+@@ -134,6 +134,15 @@
+ #define _glibtop_get_fsusage_read_write(S, B, P) \
+         _glibtop_linux_get_fsusage_read_write(S, B, P)
+ 
++#elif defined(__FreeBSD__)
++void G_GNUC_INTERNAL
++_glibtop_freebsd_get_fsusage_read_write(glibtop *server,
++					glibtop_fsusage *buf,
++					const char *path);
++
++#define _glibtop_get_fsusage_read_write(S, B, P) \
++	_glibtop_freebsd_get_fsusage_read_write(S, B, P)
++
+ #else /* default fallback */
+ #warning glibtop_get_fsusage .read .write are not implemented.
+ static inline void
+@@ -254,8 +263,9 @@
    if (statvfs (path, &fsd) < 0)
      return;
  
--- devel/libgtop2/files/patch-sysdeps_freebsd_netlist.c
+++ devel/libgtop2/files/patch-sysdeps_freebsd_netlist.c
@@ -1,6 +1,17 @@
---- sysdeps/freebsd/netlist.c.orig	Wed Feb 23 01:20:33 2005
-+++ sysdeps/freebsd/netlist.c	Wed Feb 23 01:20:44 2005
-@@ -50,6 +50,7 @@
+--- sysdeps/freebsd/netlist.c.orig	Sun Dec  5 07:01:43 2004
++++ sysdeps/freebsd/netlist.c	Sat Feb 26 00:13:28 2005
+@@ -43,6 +43,10 @@
+ 	struct if_nameindex *ifstart, *ifs;
+ 	GPtrArray *devices;
+ 
++	glibtop_init_s (&server, GLIBTOP_SYSDEPS_NETLIST, 0);
++
++	memset (buf, 0, sizeof (glibtop_netlist));
++
+ 	ifs = ifstart = if_nameindex();
+ 
+ 	devices = g_ptr_array_new();
+@@ -50,6 +54,7 @@
  	while(ifs && ifs->if_name) {
  		g_ptr_array_add(devices, g_strdup(ifs->if_name));
  		buf->number++;
