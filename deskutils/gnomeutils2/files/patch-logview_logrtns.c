a2f4affeafa084a3f8804b83c1e0b9b7c6d0826a
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-03-01 18:02:37 +0000

    This hunk is no longer needed with the new gnome-vfs.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3777 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/gnomeutils2/files/patch-logview_logrtns.c
+++ deskutils/gnomeutils2/files/patch-logview_logrtns.c
@@ -1,16 +1,5 @@
 --- logview/logrtns.c.orig	Sun Feb 20 13:03:24 2005
 +++ logview/logrtns.c	Sun Feb 20 13:27:46 2005
-@@ -217,6 +217,10 @@ OpenLogFile (char *filename)
- 	   return NULL;
-    }
- 
-+   /* gnome_vfs_read_entire_file() does not return a NUL-terminated buffer. */
-+   buffer = g_realloc (buffer, size+1);
-+   buffer[size] = '\0';
-+
-    /* Alloc memory for log structure */
-    tlog = g_new0 (Log, 1);
-    if (tlog == NULL) {
 @@ -230,6 +234,7 @@ OpenLogFile (char *filename)
  	   g_free (filename);
  
