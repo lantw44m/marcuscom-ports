c2db4786270c16749adc69b4ab0aea7d6fd1179e
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-03-03 09:16:02 +0000

    Update to 0.1.6.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3798 df743ca5-7f9a-e211-a948-0013205c9059

--- graphics/evince/files/patch-ps_ps-document.c
+++ graphics/evince/files/patch-ps_ps-document.c
@@ -1,21 +1,19 @@
---- ps/ps-document.c.orig	Sat Feb 26 17:36:03 2005
-+++ ps/ps-document.c	Sat Feb 26 17:38:26 2005
-@@ -487,12 +487,12 @@
-   GdkColor white = { 0, 0xFFFF, 0xFFFF, 0xFFFF };   /* pixel, r, g, b */
+--- ps/ps-document.c.orig	Mon Feb 28 16:53:42 2005
++++ ps/ps-document.c	Thu Mar  3 04:09:09 2005
+@@ -548,11 +548,10 @@
    GdkColormap *colormap;
+   gboolean size_changed;
  
 -  LOG ("Setup the page");
 -
  #ifdef HAVE_LOCALE_H
    char *savelocale;
  #endif
- 
 +  LOG ("Setup the page");
-+
-   if (gs->pstarget == NULL)
-     return;
  
-@@ -720,8 +720,6 @@
+   size_changed = compute_size (gs);
+ 
+@@ -783,8 +782,6 @@
    int std_out[2];               /* pipe from interp stdout */
    int std_err[2];               /* pipe from interp stderr */
  
@@ -24,7 +22,7 @@
  #define NUM_ARGS    100
  #define NUM_GS_ARGS (NUM_ARGS - 20)
  #define NUM_ALPHA_ARGS 10
-@@ -729,6 +727,8 @@
+@@ -792,6 +789,8 @@
    char *argv[NUM_ARGS], *dir, *gv_env;
    char **gs_args, **alpha_args = NULL;
    int argc = 0, i;
