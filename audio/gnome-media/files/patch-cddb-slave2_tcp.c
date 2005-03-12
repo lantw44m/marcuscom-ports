d050d3248e141bda25252a3e56308c822b1ddca7
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-03-12 08:33:04 +0000

    Correct some botched patches.  We don't need to patch inetaddr.c, and be sure to pass the correct length to connect().
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@3929 df743ca5-7f9a-e211-a948-0013205c9059

--- audio/gnome-media/files/patch-cddb-slave2_tcp.c
+++ audio/gnome-media/files/patch-cddb-slave2_tcp.c
@@ -1,30 +1,20 @@
---- cddb-slave2/tcp.c.orig	Sat Mar 12 02:32:52 2005
-+++ cddb-slave2/tcp.c	Sat Mar 12 02:39:55 2005
-@@ -171,6 +171,9 @@ gnet_tcp_socket_new_async (const GInetAd
-   struct sockaddr_in6* 	sa_in6;
-   struct sockaddr_in6 	sa6;
-   int ifindex;
-+#define SS_LEN ss_len
-+#else
-+#define SS_LEN sa_len
- #endif
-   struct sockaddr_in* 	sa_in;
-   struct sockaddr	sa;
-@@ -265,7 +268,7 @@ gnet_tcp_socket_new_async (const GInetAd
+--- cddb-slave2/tcp.c.orig	Mon Sep 20 14:52:50 2004
++++ cddb-slave2/tcp.c	Sat Mar 12 03:30:52 2005
+@@ -265,7 +265,7 @@ gnet_tcp_socket_new_async (const GInetAd
  		continue;
  
  	      sa_in6->sin6_scope_id = ifindex;
 -	      con = connect (s->sockfd, (struct sockaddr *)&sa6, sizeof(s->sa));
-+	      con = connect (s->sockfd, (struct sockaddr *)&sa6, s->sa.SS_LEN);
++	      con = connect (s->sockfd, (struct sockaddr *)&sa6, sizeof(sa6));
  	      if (con == 0 || errno == EINPROGRESS)
  				break;
  
-@@ -283,7 +286,7 @@ gnet_tcp_socket_new_async (const GInetAd
+@@ -283,7 +283,7 @@ gnet_tcp_socket_new_async (const GInetAd
        memcpy(&sa, &addr->sa, sizeof(sa));
        sa_in = (struct sockaddr_in *) &sa;
        sa_in->sin_family = AF_INET;
 -      con = connect(s->sockfd, &sa, sizeof(s->sa));
-+      con = connect(s->sockfd, &sa, s->sa.SS_LEN);
++      con = connect(s->sockfd, &sa, sizeof(sa));
      }
  
    /* Connect (but non-blocking!) */
