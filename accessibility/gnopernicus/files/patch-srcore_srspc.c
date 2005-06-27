a197fcef70d877979b5b585634c5a68565450c67
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2005-06-27 08:29:51 +0000

    - Back out of last commit, its not needed.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4124 df743ca5-7f9a-e211-a948-0013205c9059

--- accessibility/gnopernicus/files/patch-srcore_srspc.c
+++ accessibility/gnopernicus/files/patch-srcore_srspc.c
@@ -1,6 +1,6 @@
---- srcore/srspc.c.orig	Mon Jun 27 20:06:42 2005
-+++ srcore/srspc.c	Mon Jun 27 20:06:49 2005
-@@ -1535,8 +1535,8 @@
+--- srcore/srspc.c.orig	Sun May  8 01:10:29 2005
++++ srcore/srspc.c	Sun May  8 01:10:37 2005
+@@ -1531,8 +1531,8 @@ src_timeout_start_speech (gpointer data)
  static void
  src_restart_speech ()
  {
@@ -8,22 +8,5 @@
      gboolean use_speech = FALSE;
 +    g_printerr ("\nRestarting speech.\n");
      SET_SRCORE_CONFIG_DATA (SRCORE_SPEECH_ACTIVE, CFGT_BOOL, &use_speech);
-     speech_timeout_id = g_timeout_add (SRC_SPEECH_RESTART_TIMEOUT, src_timeout_start_speech, NULL);
- }
-@@ -1636,6 +1636,8 @@
- void
- src_speech_terminate ()
- {
-+    gboolean use_speech = TRUE;
-+
-     sru_assert (src_speech_initialized);
- 
-     src_speech_shutup ();
-@@ -1653,7 +1655,6 @@
-     g_ptr_array_add (src_created_voices, NULL);
-     g_strfreev ((gchar**)g_ptr_array_free (src_created_voices, TRUE));
-     
--    gboolean use_speech = TRUE;
-     if (speech_timeout_id == 0)
-         SET_SRCORE_CONFIG_DATA (SRCORE_SPEECH_ACTIVE, CFGT_BOOL, &use_speech);
+     g_timeout_add (SRC_SPEECH_RESTART_TIMEOUT, src_timeout_start_speech, NULL);
  }
