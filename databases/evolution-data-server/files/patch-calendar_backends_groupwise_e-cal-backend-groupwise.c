98dced94f4f5d89c4f33728eef1a18531ad05a6f
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-07-29 21:50:54 +0000

    Update to 1.3.6.1.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4355 df743ca5-7f9a-e211-a948-0013205c9059

--- databases/evolution-data-server/files/patch-calendar_backends_groupwise_e-cal-backend-groupwise.c
+++ databases/evolution-data-server/files/patch-calendar_backends_groupwise_e-cal-backend-groupwise.c
@@ -1,6 +1,6 @@
---- calendar/backends/groupwise/e-cal-backend-groupwise.c.orig	Wed Jul 13 02:21:58 2005
-+++ calendar/backends/groupwise/e-cal-backend-groupwise.c	Wed Jul 13 02:22:22 2005
-@@ -365,11 +365,12 @@ get_deltas (gpointer handle)
+--- calendar/backends/groupwise/e-cal-backend-groupwise.c.orig	Mon Jul 25 21:47:58 2005
++++ calendar/backends/groupwise/e-cal-backend-groupwise.c	Fri Jul 29 17:26:01 2005
+@@ -312,12 +312,13 @@ get_deltas (gpointer handle)
  
  	for (; item_list != NULL; item_list = g_list_next(item_list)) {
  		EGwItem *item = NULL;
@@ -9,8 +9,18 @@
  		char *cache_comp_str = NULL;
  		const char *uid, *rid = NULL;
  		int r_key;
-+
-+		item = E_GW_ITEM(item_list->data);
  
++		item = E_GW_ITEM(item_list->data);
++
  		modified_comp = e_gw_item_to_cal_component (item, cbgw);
  		if (!modified_comp) {
+ 			g_message ("Invalid component returned in update");
+@@ -1048,7 +1049,7 @@ e_cal_backend_groupwise_get_static_capab
+ 				  CAL_STATIC_CAPABILITY_DELEGATE_TO_MANY "," \
+ 				  CAL_STATIC_CAPABILITY_NO_ORGANIZER "," \
+ 				  CAL_STATIC_CAPABILITY_RECURRENCES_NO_MASTER "," \
+-				  CAL_STATIC_CAPABILITY_HAS_UNACCEPTED_MEETING "," \ 
++				  CAL_STATIC_CAPABILITY_HAS_UNACCEPTED_MEETING "," \
+ 				  CAL_STATIC_CAPABILITY_SAVE_SCHEDULES);
+ 
+ 	return GNOME_Evolution_Calendar_Success;
