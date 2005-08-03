1545a8728eccc30ca1d6c373b62280a16f3a8630
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-03 05:16:32 +0000

    Update to 0.6.0 and fix the libsvg-cairo plist.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4377 df743ca5-7f9a-e211-a948-0013205c9059

--- graphics/py-cairo/files/patch-cairo_pycairo-font.c
+++ graphics/py-cairo/files/patch-cairo_pycairo-font.c
@@ -1,31 +1,19 @@
---- cairo/pycairo-font.c.orig	Fri Jul 22 21:34:39 2005
-+++ cairo/pycairo-font.c	Fri Jul 22 21:35:38 2005
-@@ -183,6 +183,7 @@
- {
+--- cairo/pycairo-font.c.orig	Mon Aug  1 05:31:41 2005
++++ cairo/pycairo-font.c	Wed Aug  3 01:09:38 2005
+@@ -196,6 +196,7 @@ scaled_font_new (PyTypeObject *type, PyO
      PycairoFontFace *ff;
+     PycairoFontOptions *fo;
      PycairoMatrix *mx1, *mx2;
 +    PyObject *o;
  
-     if (!PyArg_ParseTuple(args, "O!O!O!:ScaledFont.__init__",
+     if (!PyArg_ParseTuple(args, "O!O!O!O!:ScaledFont.__new__",
  			  &PycairoFontFace_Type, &ff,
-@@ -190,7 +191,7 @@
- 			  &PycairoMatrix_Type, &mx2))
+@@ -204,7 +205,7 @@ scaled_font_new (PyTypeObject *type, PyO
+ 			  &PycairoFontOptions_Type, &fo))
  	return NULL;
  
 -    PyObject *o = type->tp_alloc(type, 0);
 +    o = type->tp_alloc(type, 0);
      if (o) {
  	cairo_scaled_font_t *scaled_font = cairo_scaled_font_create 
- 	    (ff->font_face, &mx1->matrix, &mx2->matrix);
-@@ -211,8 +210,9 @@
- scaled_font_extents (PycairoScaledFont *o)
- {
-     cairo_font_extents_t e;
--    cairo_status_t status = cairo_scaled_font_extents (o->scaled_font, &e);
--    if (Pycairo_Check_Status (status))
-+
-+    cairo_scaled_font_extents (o->scaled_font, &e);
-+    if (Pycairo_Check_Status (cairo_scaled_font_status(o->scaled_font)))
- 	return NULL;
-     return Py_BuildValue ("(ddddd)", e.ascent, e.descent, e.height, 
- 			  e.max_x_advance, e.max_y_advance);
+ 	    (ff->font_face, &mx1->matrix, &mx2->matrix, fo->font_options);
