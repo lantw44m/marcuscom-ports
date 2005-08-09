0160d6cc5ad9c7882867610b9fea59aa8f1d4951
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-09 23:46:44 +0000

    Make sure audio CDs are reported as audio CDs.
    Reported by:	kwm and ankon on #freebsd-gnome
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4459 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/nautilus-cd-burner/files/patch-nautilus-burn-drive.c
+++ sysutils/nautilus-cd-burner/files/patch-nautilus-burn-drive.c
@@ -1,5 +1,5 @@
---- nautilus-burn-drive.c.orig	Thu Jun  9 14:32:26 2005
-+++ nautilus-burn-drive.c	Thu Jun  9 14:36:28 2005
+--- nautilus-burn-drive.c.orig	Thu Aug  4 16:23:24 2005
++++ nautilus-burn-drive.c	Tue Aug  9 19:42:32 2005
 @@ -48,6 +48,13 @@
  #include <sys/cdio.h>
  #include <sys/cdrio.h>
@@ -14,3 +14,12 @@
  #endif /* __FreeBSD__ */
  
  #ifdef HAVE_SYS_CDIO_H
+@@ -484,7 +491,7 @@ get_disc_type (const char *dev_path)
+ 		entry.track++) {
+ 	    	entry.address_format = CD_LBA_FORMAT;
+ 		if (ioctl (fd, CDIOREADTOCENTRY, &entry) == 0) {
+-		    	if (entry.entry.control & CDROM_DATA_TRACK) {
++		    	if (!(entry.entry.control & CDROM_DATA_TRACK)) {
+ 			    	type = CDS_AUDIO;
+ 				break;
+ 			}
