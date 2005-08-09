fc61e518ff9477c2c8de6b9fd898ae5b0b57f86c
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-09 00:52:00 +0000

    Update to 1.3.7.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4428 df743ca5-7f9a-e211-a948-0013205c9059

--- databases/evolution-data-server/files/patch-camel_providers_groupwise_camel-groupwise-folder.c
+++ databases/evolution-data-server/files/patch-camel_providers_groupwise_camel-groupwise-folder.c
@@ -1,14 +1,32 @@
---- camel/providers/groupwise/camel-groupwise-folder.c.orig	Wed Jul 13 02:14:43 2005
-+++ camel/providers/groupwise/camel-groupwise-folder.c	Wed Jul 13 02:15:18 2005
-@@ -947,9 +947,10 @@ gw_update_summary ( CamelFolder *folder,
+--- camel/providers/groupwise/camel-groupwise-folder.c.orig	Mon Aug  8 05:12:44 2005
++++ camel/providers/groupwise/camel-groupwise-folder.c	Mon Aug  8 20:42:33 2005
+@@ -929,10 +929,11 @@ gw_update_cache ( CamelFolder *folder, G
  		char *temp_date = NULL;
  		const char *id;
  		GSList *recp_list = NULL;
 -		status_flags = 0;
  		CamelStream *cache_stream;
  		CamelMimeMessage *mail_msg = NULL;
-+
-+		status_flags = 0;
  
++		status_flags = 0;
++
  		id = e_gw_item_get_id (temp_item);
  
+ 		cache_stream  = camel_data_cache_get (gw_folder->cache, "cache", id, ex);
+@@ -1609,6 +1610,7 @@ groupwise_transfer_messages_to (CamelFol
+ 			const char *old_uid = (const char*)uids->pdata[index];
+ 			char **tmp;
+ 			guint32 temp_flags = 0;
++			CamelGroupwiseMessageInfo *dest_info;
+ 			CamelGroupwiseMessageInfo *src_info = (CamelGroupwiseMessageInfo *)camel_folder_summary_uid (source->summary, (const char*)uids->pdata[index]);
+ 			
+ 			/* we don't want to blindly copy the info: reset some flags not suitable for destination*/
+@@ -1617,7 +1619,7 @@ groupwise_transfer_messages_to (CamelFol
+ 			else if (!strcmp(destination->full_name, JUNK_FOLDER))
+ 				camel_folder_set_message_flags (source, old_uid, CAMEL_MESSAGE_JUNK, CAMEL_GW_MESSAGE_JUNK);
+ 
+-			CamelGroupwiseMessageInfo *dest_info = (CamelGroupwiseMessageInfo *)camel_message_info_clone((CamelMessageInfo *)src_info);
++			dest_info = (CamelGroupwiseMessageInfo *)camel_message_info_clone((CamelMessageInfo *)src_info);
+ 			tmp = g_strsplit (old_uid, ":", -1);
+ 			dest_info->info.uid = g_strdup_printf ("%s:%s",tmp[0], dest_container_id);
+ 			temp_flags = camel_folder_get_message_flags (source, (const char *)uids->pdata[index]);
