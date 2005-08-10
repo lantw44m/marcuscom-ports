3639138a93e9b777c6e2ce08601f169d91dcc4cc
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-10 19:57:20 +0000

    Update to 2.11.91.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4466 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/gnome-control-center/files/patch-capplets_about-me_gnome-about-me-password.c
+++ sysutils/gnome-control-center/files/patch-capplets_about-me_gnome-about-me-password.c
@@ -1,9 +1,10 @@
---- capplets/about-me/gnome-about-me-password.c.orig	Tue Jul  5 14:14:11 2005
-+++ capplets/about-me/gnome-about-me-password.c	Tue Jul  5 14:14:50 2005
-@@ -35,7 +35,12 @@
+--- capplets/about-me/gnome-about-me-password.c.orig	Wed Aug 10 05:45:08 2005
++++ capplets/about-me/gnome-about-me-password.c	Wed Aug 10 15:44:57 2005
+@@ -35,7 +35,13 @@
  #include <sys/wait.h>
  #include <sys/poll.h>
  #include <termios.h>
++#include <signal.h>
 +#ifndef __FreeBSD__
  #include <pty.h>
 +#else
