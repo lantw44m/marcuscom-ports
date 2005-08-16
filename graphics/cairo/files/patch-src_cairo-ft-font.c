b213eec8b81f1a92d0c25d656d293e3621659e81
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-16 18:49:01 +0000

    Include stdlib.h for random().
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4516 df743ca5-7f9a-e211-a948-0013205c9059

--- graphics/cairo/files/patch-src_cairo-ft-font.c
+++ graphics/cairo/files/patch-src_cairo-ft-font.c
@@ -1,17 +1,18 @@
---- src/cairo-ft-font.c.orig	Tue Aug 16 14:33:55 2005
-+++ src/cairo-ft-font.c	Tue Aug 16 14:43:47 2005
-@@ -44,6 +44,10 @@
+--- src/cairo-ft-font.c.orig	Tue Aug 16 14:46:46 2005
++++ src/cairo-ft-font.c	Tue Aug 16 14:46:59 2005
+@@ -44,6 +44,11 @@
  #include <fontconfig/fontconfig.h>
  #include <fontconfig/fcfreetype.h>
  
 +#include <sys/types.h>
++#include <stdlib.h>
 +#include <time.h>
 +#include <unistd.h>
 +
  #include <ft2build.h>
  #include FT_FREETYPE_H
  #include FT_OUTLINE_H
-@@ -257,7 +261,12 @@ _cairo_ft_unscaled_font_init_key (cairo_
+@@ -257,7 +262,12 @@ _cairo_ft_unscaled_font_init_key (cairo_
      key->id = id;
  
      /* 1607 is just an arbitrary prime. */
