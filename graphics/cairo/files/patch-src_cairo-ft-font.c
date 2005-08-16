da1b2366cec1ea098aa31f11e18e3b7370978e46
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-16 18:47:05 +0000

    Instead of not hashing a NULL filename at all, create a [weak] random number for the hash.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4515 df743ca5-7f9a-e211-a948-0013205c9059

--- graphics/cairo/files/patch-src_cairo-ft-font.c
+++ graphics/cairo/files/patch-src_cairo-ft-font.c
@@ -1,13 +1,27 @@
---- src/cairo-ft-font.c.orig	Tue Aug 16 14:31:13 2005
-+++ src/cairo-ft-font.c	Tue Aug 16 14:31:57 2005
-@@ -296,9 +296,8 @@ _cairo_ft_unscaled_font_init (cairo_ft_u
- 	filename_copy = strdup (filename);
- 	if (filename_copy == NULL)
- 	    return CAIRO_STATUS_NO_MEMORY;
-+        _cairo_ft_unscaled_font_init_key (unscaled, filename_copy, id);
-     }
--
--    _cairo_ft_unscaled_font_init_key (unscaled, filename_copy, id);
+--- src/cairo-ft-font.c.orig	Tue Aug 16 14:33:55 2005
++++ src/cairo-ft-font.c	Tue Aug 16 14:43:47 2005
+@@ -44,6 +44,10 @@
+ #include <fontconfig/fontconfig.h>
+ #include <fontconfig/fcfreetype.h>
  
-     if (face) {
- 	unscaled->from_face = 1;
++#include <sys/types.h>
++#include <time.h>
++#include <unistd.h>
++
+ #include <ft2build.h>
+ #include FT_FREETYPE_H
+ #include FT_OUTLINE_H
+@@ -257,7 +261,12 @@ _cairo_ft_unscaled_font_init_key (cairo_
+     key->id = id;
+ 
+     /* 1607 is just an arbitrary prime. */
+-    hash = _cairo_hash_string (filename);
++    if (filename != NULL) {
++        hash = _cairo_hash_string (filename);
++    } else {
++	srandom(time(NULL) * getpid());
++	hash = random();
++    }
+     hash += ((unsigned long) id) * 1607;
+ 	
+     key->base.hash_entry.hash = hash;
