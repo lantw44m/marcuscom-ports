590565a1d7ebff3daea2fa539d3b2db0d171825e
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-23 17:50:27 +0000

    Update to 1.3.8.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4596 df743ca5-7f9a-e211-a948-0013205c9059

--- databases/evolution-data-server/files/patch-camel_providers_groupwise_camel-groupwise-utils.c
+++ databases/evolution-data-server/files/patch-camel_providers_groupwise_camel-groupwise-utils.c
@@ -1,14 +1,13 @@
---- camel/providers/groupwise/camel-groupwise-utils.c.orig	Wed Jul 13 02:16:06 2005
-+++ camel/providers/groupwise/camel-groupwise-utils.c	Wed Jul 13 02:16:19 2005
-@@ -383,9 +383,10 @@ camel_groupwise_util_item_from_message (
- 				attachment->contentType = g_strdup_printf ("%s/%s", type->type, type->subtype) ;
- 				if (!g_ascii_strncasecmp (attachment->contentType, RFC_822, strlen (RFC_822))) {
- 					char *temp_id = NULL, *id = NULL;
-+					int len;
- 					//id = (char *)camel_mime_message_get_message_id ((CamelMimeMessage *)dw);
- 					temp_id = (char *)camel_medium_get_header (CAMEL_MEDIUM ((CamelMimeMessage *)dw), "Message-Id");
--					int len = strlen (temp_id);
-+					len = strlen (temp_id);
+--- camel/providers/groupwise/camel-groupwise-utils.c.orig	Fri Aug 19 08:56:58 2005
++++ camel/providers/groupwise/camel-groupwise-utils.c	Tue Aug 23 13:14:06 2005
+@@ -333,8 +333,9 @@ send_as_attachment (EGwConnection *cnc, 
+ 	attachment->name = g_strdup (filename ? filename : "");
+ 	if (!g_ascii_strncasecmp (attachment->contentType, RFC_822, strlen (RFC_822))) {
+ 		char *temp_id = NULL, *id = NULL;
++		int len;
+ 		temp_id = (char *)camel_medium_get_header (CAMEL_MEDIUM ((CamelMimeMessage *)dw), "Message-Id");
+-		int len = strlen (temp_id);
++		len = strlen (temp_id);
  
- 					id = (char *)g_malloc0 (len-1);
- 					id = memcpy(id, temp_id+2, len-3);
+ 		id = (char *)g_malloc0 (len-1);
+ 		id = memcpy(id, temp_id+2, len-3);
