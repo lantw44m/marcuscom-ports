b56fb2a10a1020e4a11b3bc5c763ab3bd80b4c55
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-08-25 16:28:44 +0000

    Fix screen resolution initialization issues that were causing vte to lockup. See http://bugzilla.gnome.org/show_bug.cgi?id=314452 for more details.
    Obtained from:	Above bug
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4626 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11-toolkits/gtk20/files/patch-gdk_gdkscreen.c
@@ -0,0 +1,50 @@
+--- gdk/gdkscreen.c.orig	Thu Aug 25 12:09:18 2005
++++ gdk/gdkscreen.c	Thu Aug 25 12:25:31 2005
+@@ -53,14 +53,14 @@ gdk_screen_get_type (void)
+       static const GTypeInfo object_info =
+ 	{
+ 	  sizeof (GdkScreenClass),
+-	  (GBaseInitFunc) gdk_screen_init,
++	  (GBaseInitFunc) NULL,
+ 	  (GBaseFinalizeFunc) NULL,
+ 	  (GClassInitFunc) gdk_screen_class_init,
+ 	  NULL,			/* class_finalize */
+ 	  NULL,			/* class_data */
+ 	  sizeof (GdkScreen),
+ 	  0,			/* n_preallocs */
+-	  (GInstanceInitFunc) NULL,
++	  (GInstanceInitFunc) gdk_screen_init_instance,
+ 	};
+       
+       object_type = g_type_register_static (G_TYPE_OBJECT,
+@@ -101,9 +101,10 @@ gdk_screen_class_init (GdkScreenClass *k
+ }
+ 
+ static void
+-gdk_screen_init (GdkScreen *screen)
++gdk_screen_init_instance (GTypeInstance *instance,
++			  gpointer g_class)
+ {
+-    screen->resolution = -1;
++  GDK_SCREEN (instance)->resolution = -1.;
+ }
+ 
+ static void
+@@ -407,7 +408,7 @@ gdk_screen_set_resolution_libgtk_only (G
+     if (dpi >= 0)
+ 	screen->resolution = dpi;
+     else
+-	screen->resolution = -1;
++	screen->resolution = -1.;
+ }
+ 
+ /**
+@@ -423,7 +424,7 @@ gdk_screen_set_resolution_libgtk_only (G
+ gdouble
+ gdk_screen_get_resolution_libgtk_only (GdkScreen *screen)
+ {
+-    g_return_val_if_fail (GDK_IS_SCREEN (screen), -1);
++    g_return_val_if_fail (GDK_IS_SCREEN (screen), -1.);
+ 
+     return screen->resolution;
+ }
