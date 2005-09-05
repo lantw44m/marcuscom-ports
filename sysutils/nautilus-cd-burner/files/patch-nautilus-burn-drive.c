928e7206a8b3e1aed4d3e4b0e799131f1bfdf576
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-09-05 19:11:36 +0000

    Update to 2.12.0.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4719 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/nautilus-cd-burner/files/patch-nautilus-burn-drive.c
+++ sysutils/nautilus-cd-burner/files/patch-nautilus-burn-drive.c
@@ -1,5 +1,5 @@
---- nautilus-burn-drive.c.orig	Mon Aug 22 18:58:32 2005
-+++ nautilus-burn-drive.c	Mon Aug 22 20:20:06 2005
+--- nautilus-burn-drive.c.orig	Tue Aug 23 11:55:56 2005
++++ nautilus-burn-drive.c	Mon Sep  5 15:04:22 2005
 @@ -471,7 +471,6 @@ get_disc_type (const char *dev_path)
  	struct cd_toc_entry              entry;
  	int                              i;
@@ -8,12 +8,3 @@
  #ifndef CDROM_DATA_TRACK
  #define CDROM_DATA_TRACK 4
  #endif
-@@ -481,7 +480,7 @@ get_disc_type (const char *dev_path)
- 	    	return -1;
- 	}
- 
--	if (ioctl (fd, CDIOREADTOCHEADER, &header) == 0) {
-+	if (ioctl (fd, CDIOREADTOCHEADER, &header) != 0) {
- 		close (fd);
- 	    	return -1;
- 	}
