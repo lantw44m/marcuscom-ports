93928fd8d7ad42e2f26ee8856dfaf950bc03283b
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2005-09-05 15:24:42 +0000

    Update to 2.12.0.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4708 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-fm/nautilus/files/patch-libnautilus-private_nautilus-file-operations.c
+++ x11-fm/nautilus/files/patch-libnautilus-private_nautilus-file-operations.c
@@ -1,9 +1,9 @@
---- libnautilus-private/nautilus-file-operations.c.orig	Sun Jul 31 13:44:00 2005
-+++ libnautilus-private/nautilus-file-operations.c	Sun Jul 31 14:23:47 2005
-@@ -2453,12 +2453,21 @@ nautilus_file_operations_new_file (GtkWi
- 				   NautilusNewFileCallback done_callback,
+--- libnautilus-private/nautilus-file-operations.c.orig	Thu Aug 25 10:28:10 2005
++++ libnautilus-private/nautilus-file-operations.c	Mon Sep  5 17:09:55 2005
+@@ -2481,12 +2481,22 @@
  				   gpointer data)
  {
+ 	struct NewFileData *new_data;
 -	char source_file_str[] = "/tmp/nautilus-sourceXXXXXX";
 +	char *source_file_str;
  	char *source_file_uri;
@@ -19,13 +19,14 @@
 +	}
 +
 +	source_file_str = g_build_filename (gnome_vfs_uri_get_path (parent_uri),
-+                                            "nautilus-sourceXXXXXX", NULL);
++						"nautilus-sourceXXXXXX", NULL);
++
  	fd = mkstemp (source_file_str);
  	if (fd == -1) {
  		(*done_callback) (NULL, data);
-@@ -2486,6 +2495,8 @@ nautilus_file_operations_new_file (GtkWi
- 							 done_callback,
- 							 data);
+@@ -2518,6 +2528,8 @@
+ 							 new_file_from_temp_callback,
+ 							 new_data);
  
 +	gnome_vfs_uri_unref (parent_uri);
 +	g_free (source_file_str);
