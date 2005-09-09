739be8a581fc21302566d5f94b0560f9e97df6bf
bland <bland@df743ca5-7f9a-e211-a948-0013205c9059>
2005-09-09 03:42:56 +0000

    Plug memory leak introduced by de-C99-fication.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4805 df743ca5-7f9a-e211-a948-0013205c9059

--- graphics/librsvg2/files/patch-rsvg-shapes.c
+++ graphics/librsvg2/files/patch-rsvg-shapes.c
@@ -1,17 +1,18 @@
---- rsvg-shapes.c.orig	Thu Aug 25 22:32:03 2005
-+++ rsvg-shapes.c	Thu Aug 25 22:32:13 2005
-@@ -145,12 +145,12 @@ _rsvg_node_poly_draw(RsvgNode * self, Rs
+--- rsvg-shapes.c.orig	Tue Jul 19 23:47:27 2005
++++ rsvg-shapes.c	Fri Sep  9 12:34:17 2005
+@@ -145,12 +145,13 @@
  					 int dominate)
  {
  	RsvgNodePoly * poly = (RsvgNodePoly *)self;
 +	gsize i;
-+	GString * d = g_string_new ("");
++	GString * d;
  	/* represent as a "moveto, lineto*, close" path */  
  	if (poly->pointlist_len < 2)
  		return;
  
 -	gsize i;
 -	GString * d = g_string_new ("");
++	d = g_string_new ("");
  	g_string_append_printf (d, "M %f %f ", poly->pointlist[0], poly->pointlist[1] );
  	
  	for (i = 2; i < poly->pointlist_len; i += 2)
