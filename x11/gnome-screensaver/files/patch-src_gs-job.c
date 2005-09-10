4d99dad2557a0628b8faaa1cb461700350e221d2
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-09-10 00:25:02 +0000

    Complete xscreensaver support, and bump PORTREVISION again.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4810 df743ca5-7f9a-e211-a948-0013205c9059

--- x11/gnome-screensaver/files/patch-src_gs-job.c
+++ x11/gnome-screensaver/files/patch-src_gs-job.c
@@ -1,5 +1,5 @@
---- src/gs-job.c.orig	Wed Jun  8 23:57:35 2005
-+++ src/gs-job.c	Thu Jun  9 00:00:29 2005
+--- src/gs-job.c.orig	Wed Sep  7 09:23:59 2005
++++ src/gs-job.c	Fri Sep  9 20:21:31 2005
 @@ -29,7 +29,8 @@
  #include <sys/wait.h>
  #include <errno.h>
@@ -9,4 +9,16 @@
 +#include <sys/time.h>
  #include <sys/resource.h>
  #endif
+ 
+@@ -655,9 +656,10 @@ gs_job_set_widget  (GSJob     *job,
+ static const char *known_locations [] = {
+         SAVERDIR,
+         LIBEXECDIR "/xscreensaver",
+-        "/usr/X11R6/lib/xscreensaver",
++        "%%X11BASE%%/lib/xscreensaver",
+         "/usr/libexec/xscreensaver",
+         "/usr/lib/xscreensaver",
++	"%%X11BASE%%/bin/xscreensaver-hacks",
+         NULL
+ };
  
