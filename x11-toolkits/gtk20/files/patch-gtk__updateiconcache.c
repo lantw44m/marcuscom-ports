221ff8bc5fea431c215e67d64fb6a43629f8ae03
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-09-13 18:37:47 +0000

    Reverse the order of the use-after-free patch so it applies.
    Submitted by:	marcusg on #freebsd-gnome
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4836 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-toolkits/gtk20/files/patch-gtk__updateiconcache.c
+++ x11-toolkits/gtk20/files/patch-gtk__updateiconcache.c
@@ -1,13 +1,13 @@
---- gtk/updateiconcache.c	Tue Sep 13 22:59:51 2005
-+++ gtk/updateiconcache.c.orig	Tue Sep 13 22:58:33 2005
+--- gtk/updateiconcache.c.orig	Tue Sep 13 22:59:51 2005
++++ gtk/updateiconcache.c	Tue Sep 13 22:58:33 2005
 @@ -144,9 +144,9 @@
    if (image->flags == HAS_ICON_FILE)
      {
        g_free (key);
-+      g_free (image);
+-      g_free (image);
        g_free (image->attach_points);
        g_strfreev (image->display_names);
--      g_free (image);
++      g_free (image);
  
        return TRUE;
      }
