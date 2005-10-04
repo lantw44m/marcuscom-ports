96d54cc6d7254ae7e48e532f53d316e202b966fe
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2005-10-04 17:39:31 +0000

    Update to 2.12.1.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4915 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/gnomeutils2/files/patch-logview_logview.c
+++ deskutils/gnomeutils2/files/patch-logview_logview.c
@@ -1,22 +1,6 @@
---- logview/logview.c.orig	Mon Jul 11 17:31:04 2005
-+++ logview/logview.c	Mon Jul 11 17:31:50 2005
-@@ -815,6 +815,7 @@ FileSelectResponse (GtkWidget * chooser,
- {
-    char *f;
-    LogviewWindow *logview = data;
-+   Log *tl;
- 
-    gtk_widget_hide (GTK_WIDGET (chooser));
-    if (response != GTK_RESPONSE_OK)
-@@ -834,7 +835,6 @@ FileSelectResponse (GtkWidget * chooser,
- 		   }
- 	   }
- 
--	   Log *tl;
- 	   if ((tl = OpenLogFile (f, TRUE)) != NULL)
- 	     logview_add_log (logview, tl);
-    }
-@@ -930,7 +930,7 @@ open_databases (void)
+--- logview/logview.c.orig	Tue Oct  4 12:31:47 2005
++++ logview/logview.c	Tue Oct  4 12:32:04 2005
+@@ -930,7 +930,7 @@
  
  	if ( ! found) {
  		g_snprintf (full_name, sizeof (full_name),
@@ -25,7 +9,7 @@
  		if (access (full_name, R_OK) == 0) {
  			found = TRUE;
  			g_free (cfg->regexp_db_path);
-@@ -952,7 +952,7 @@ open_databases (void)
+@@ -952,7 +952,7 @@
  
  	if ( ! found) {
  		g_snprintf (full_name, sizeof (full_name),
@@ -34,7 +18,7 @@
  		if (access (full_name, R_OK) == 0) {
  			found = TRUE;
  			g_free (cfg->descript_db_path);
-@@ -983,7 +983,7 @@ open_databases (void)
+@@ -983,7 +983,7 @@
  
  	if ( ! found) {
  		g_snprintf (full_name, sizeof (full_name),
