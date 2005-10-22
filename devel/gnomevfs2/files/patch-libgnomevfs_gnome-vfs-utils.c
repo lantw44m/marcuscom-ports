8614b0515a091e2214f9559e3aa7e01d4c056bb1
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2005-10-22 06:16:43 +0000

    Fix a crash that can occur when trying to resolve nested symlinks.  See http://bugzilla.gnome.org/show_bug.cgi?id=318457 for more details.
    Obtained from:	GNOME CVS
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4963 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-utils.c
+++ devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-utils.c
@@ -1,5 +1,5 @@
---- libgnomevfs/gnome-vfs-utils.c.orig	Wed Jun  8 03:19:54 2005
-+++ libgnomevfs/gnome-vfs-utils.c	Tue Sep 20 01:39:33 2005
+--- libgnomevfs/gnome-vfs-utils.c.orig	Thu Sep 15 07:02:53 2005
++++ libgnomevfs/gnome-vfs-utils.c	Sat Oct 22 02:12:32 2005
 @@ -839,6 +839,7 @@ gnome_vfs_icon_path_from_filename (const
  	for (temp_paths = paths; *temp_paths != NULL; temp_paths++) {
  		full_filename = g_build_filename (*temp_paths,
@@ -8,3 +8,19 @@
  						  "pixmaps",
  						  relative_filename,
  						  NULL);
+@@ -1991,9 +1992,14 @@ _gnome_vfs_uri_resolve_all_symlinks_uri 
+ 			resolved_uri = gnome_vfs_uri_resolve_relative (new_uri,
+ 								       info->symlink_name);
+ 			if (*p != 0) {
++				gnome_vfs_uri_unref (new_uri);
++
++				new_uri = gnome_vfs_uri_append_string (resolved_uri, p);
++
+ 				gnome_vfs_uri_unref (uri);
+-				uri = gnome_vfs_uri_append_string (resolved_uri, p);
+ 				gnome_vfs_uri_unref (resolved_uri);
++
++				uri = gnome_vfs_uri_ref (new_uri);
+ 			} else {
+ 				gnome_vfs_uri_unref (uri);
+ 				uri = resolved_uri;
