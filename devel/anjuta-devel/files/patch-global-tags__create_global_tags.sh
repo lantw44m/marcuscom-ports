9154c309dcf968fadfde4bbea7e375be1e89dafa
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2005-10-27 18:55:07 +0000

    - Update to 2.0.1, now it can build w/ new gdi or/and gnomebuild. - Change from patch-global-tags__create_global_tags.sh to sed to kill a hard   core path of bash. /usr/local -> ${LOCALBASE} - Fix the plist.
    There is a weird issue with render or whatever what is a better word. It has
    some weird ghost, it will showing something move by itself and doesn't display
    correct (render?). Oh well, better to have it runs than can't build.
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@4976 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/anjuta-devel/files/patch-global-tags__create_global_tags.sh
+++
@@ -1,8 +0,0 @@
---- global-tags/create_global_tags.sh.orig	Sun May 22 21:18:32 2005
-+++ global-tags/create_global_tags.sh	Sun May 22 21:21:14 2005
-@@ -1,4 +1,4 @@
--#!/bin/sh
-+#!/usr/local/bin/bash
- 
- function anjuta_tags () {
- 
