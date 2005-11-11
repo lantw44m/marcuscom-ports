3519652ba26fe5688e9981c3413ba4b86734869d
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2005-11-11 23:25:36 +0000

    - Add >/dev/null to MTREE_CMD line to be like bsd.port.mk
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@5110 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -3,7 +3,7 @@
 #
 # $FreeBSD$
 #	$NetBSD: $
-#     $MCom: ports/Mk/bsd.gnome.mk,v 1.337 2005/11/11 11:18:38 ahze Exp $
+#     $MCom: ports/Mk/bsd.gnome.mk,v 1.338 2005/11/11 11:24:38 ahze Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -721,7 +721,7 @@ post-install: gnome-post-install
 
 gnome-pre-su-install:
 .if ${_USE_GNOME:Mgnomeprefix}!="" && !defined(NO_MTREE)
-	@${MTREE_CMD} ${MTREE_ARGS:S/${MTREE_FILE}/${GNOME_MTREE}/} ${PREFIX}/
+	@${MTREE_CMD} ${MTREE_ARGS:S/${MTREE_FILE}/${GNOME_MTREE}/} ${PREFIX}/ >/dev/null
 .endif
 .if defined(GCONF_SCHEMAS)
 	@${MKDIR} ${PREFIX}/etc/gconf/gconf.xml.defaults/
