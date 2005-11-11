d3abf004ac39df100c9cbac19e2d976833df94b1
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2005-11-11 04:11:03 +0000

    - Add seamonkey!
    This is the current release of the Seamonkey open source web browser. It should
    be fully compliant with all W3C standards, including HTML, CSS, XML, XSL,
    JavaScript, MathML, SSL encryption, SVG and RDF.  Also supports Java with
    the use of the FreeBSD native Java plug-in.
    
    This is the latest bleeding edge release.  Beware that you'll probably find
    bugs here, so if you value stability, try www/mozilla.
    
    WWW: http://www.mozilla.org/projects/seamonkey/
    
    TODO:
    	- sync PIS scripts
    	- Use system nss/nspr
    	- Update patch-browser_app_mozilla.in ... (blah)
    
    Repocopied by:	marcus
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@5102 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ www/seamonkey/files/patch-build_unix_run-mozilla.sh
@@ -0,0 +1,21 @@
+--- build/unix/run-mozilla.sh.orig	Mon Oct 17 13:21:17 2005
++++ build/unix/run-mozilla.sh	Thu Nov 10 22:01:41 2005
+@@ -333,7 +333,7 @@
+ ## LD_LIBRARY_PATH because $ORIGIN doesn't work on a symbolic link.
+ if [ `uname -s` != "SunOS" -o -h "$MOZ_PROGRAM" ]
+ then
+-	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
+ fi 
+ 
+ if [ -n "$LD_LIBRARYN32_PATH" ]
+@@ -422,6 +422,9 @@
+ #
+ export MOZILLA_FIVE_HOME LD_LIBRARY_PATH
+ export SHLIB_PATH LIBPATH LIBRARY_PATH ADDON_PATH DYLD_LIBRARY_PATH
++
++MOZ_PLUGIN_PATH=%%PREFIX%%/lib/browser_plugins
++export MOZ_PLUGIN_PATH
+ 
+ if [ $moz_debug -eq 1 ]
+ then
