38d4d9a4cbb4068804836f0ecc1e32754b6f4a0c
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2005-11-13 06:30:01 +0000

    - Take adamw's advice about using *_GECKO
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@5119 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.mozilla.mk
+++ Mk/bsd.mozilla.mk
@@ -2,7 +2,7 @@
 # ex:ts=4
 #
 # $FreeBSD$
-#    $MCom: ports/Mk/bsd.mozilla.mk,v 1.7 2005/11/12 07:55:19 ahze Exp $
+#    $MCom: ports/Mk/bsd.mozilla.mk,v 1.9 2005/11/13 05:35:10 ahze Exp $
 #
 # 4 column tabs prevent hair loss and tooth decay!
 
@@ -15,28 +15,28 @@ Mozilla_Pre_Include=			bsd.mozilla.mk
 
 # Ports can use the following:
 #
-# WANT_MOZILLA=	yes
+# WANT_GECKO=	yes
 #  include bsd.mozilla.mk
 #
-# USE_MOZILLA= mozilla firefox mozilla-devel
+# USE_GECKO= mozilla firefox mozilla-devel
 #  Lists gecko's the port supports. The first entry will
-#  be the default gecko to use unless WITH_MOZILLA is defined
+#  be the default gecko to use unless WITH_GECKO is defined
 #  then bsd.mozilla.mk will test if the listed entries in 
-#  WITH_MOZILLA and match ones in USE_MOZILLA, if true then the
-#  first match found in WITH_MOZILLA will be used. bsd.mozilla.mk
+#  WITH_GECKO and match ones in USE_GECKO, if true then the
+#  first match found in WITH_GECKO will be used. bsd.mozilla.mk
 #  will set MOZILLA to the gecko it will be using. 
 #
-#  The use of USE_MOZILLA= yes will default to firefox unless
-#  WITH_MOZILLA is defined and this means your port supports
+#  The use of USE_GECKO= yes will default to firefox unless
+#  WITH_GECKO is defined and this means your port supports
 #  every gecko listed in bsd.mozilla.mk
 #
-# .if ${MOZILLA}=="seamonkey"
-# CONFIGURE_ARGS+= --with-mozilla=${MOZILLA}
+# .if ${GECKO}=="seamonkey"
+# CONFIGURE_ARGS+= --with-mozilla=${GECKO}
 # .endif
 #
 # End users can use the following:
 #
-# WITH_MOZILLA= mozilla firefox seamonkey
+# WITH_GECKO= mozilla firefox seamonkey
 #
 
 _GECKO_ALL= 	firefox firefox-devel mozilla mozilla-devel nvu \
@@ -54,37 +54,37 @@ ${gecko}_PLIST?=	${X11BASE}/lib/${gecko}/libgtkembedmoz.so
 .endfor
 
 # Figure out which mozilla to use
-# Weed out bad options in USE_MOZILLA
-.for badgecko in ${USE_MOZILLA}
+# Weed out bad options in USE_GECKO
+.for badgecko in ${USE_GECKO}
 . if ${_GECKO_ALL:M${badgecko}}!=""
-GOOD_USE_MOZILLA+=	${badgecko}
+GOOD_USE_GECKO+=	${badgecko}
 . endif
 .endfor
 
 # Figure out which mozilla to use and weed out the bad ones
-.if defined(WITH_MOZILLA) && defined(GOOD_USE_MOZILLA)
-. for badgecko in ${WITH_MOZILLA}
-.  if ${GOOD_USE_MOZILLA:M${badgecko}}!=""
-GOOD_WITH_MOZILLA+=	${badgecko}
+.if defined(WITH_GECKO) && defined(GOOD_USE_GECKO)
+. for badgecko in ${WITH_GECKO}
+.  if ${GOOD_USE_GECKO:M${badgecko}}!=""
+GOOD_WITH_GECKO+=	${badgecko}
 .  endif
 . endfor
-. if defined(GOOD_WITH_MOZILLA)
-MOZILLA=	#### CHANGE ME
+. if defined(GOOD_WITH_GECKO)
+GECKO=	#### CHANGE ME
 . endif
 .endif
 
-.if !defined(MOZILLA) && defined(GOOD_USE_MOZILLA)
-MOZILLA=	#### CHANGE ME
+.if !defined(GECKO) && defined(GOOD_USE_GECKO)
+GECKO=	#### CHANGE ME
 .endif
 
-.if defined(MOZILLA) && ${_GECKO_ALL:M${MOZILLA}}!=""
-. if ${_NEW_GCC_GECKO:M${MOZILLA}!=""
+.if defined(GECKO) && ${_GECKO_ALL:M${GECKO}}!=""
+. if ${_NEW_GCC_GECKO:M${GECKO}!=""
 USE_GCC?=	3.4+
 . endif
-BUILD_DEPENDS+=	${MOZILLA_${PLIST}}:${MOZILLA_${DEPENDS}}
-RUN_DEPENDS+=	${MOZILLA_${PLIST}}:${MOZILLA_${DEPENDS}}
+BUILD_DEPENDS+=	${GECKO_${PLIST}}:${GECKO_${DEPENDS}}
+RUN_DEPENDS+=	${GECKO_${PLIST}}:${GECKO_${DEPENDS}}
 .else
-BROKEN="Bad use of USE_MOZILLA"
+BROKEN="Bad use of USE_GECKO"
 .endif
 
 .endif # end all
