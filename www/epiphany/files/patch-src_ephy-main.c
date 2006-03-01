57a019d6b4296ec8f760d4086b70f016e170c9a8
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-03-01 21:41:33 +0000

    Fix a crash that occurs when passing a URL to epiphany on the command line. See http://bugzilla.gnome.org/show_bug.cgi?id=333051 for more details.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@5820 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ www/epiphany/files/patch-src_ephy-main.c
@@ -0,0 +1,19 @@
+--- ephy-main.c.orig	Wed Mar  1 16:34:13 2006
++++ ephy-main.c	Wed Mar  1 16:34:15 2006
+@@ -569,14 +569,13 @@ main (int argc,
+ 
+ 		for (i = 0; arguments[i] != NULL; ++i)
+ 		{
+-			char *uri, *path = NULL;
++			char *uri, *path = NULL, rpath[PATH_MAX];
+ 
+-			path = realpath (arguments[i], NULL);
++			path = realpath (arguments[i], rpath);
+ 			if (path != NULL)
+ 			{
+ 				uri = g_locale_to_utf8 (path, -1, 
+ 							NULL, NULL, &error);
+-				free (path);
+ 			}
+ 			else
+ 			{
