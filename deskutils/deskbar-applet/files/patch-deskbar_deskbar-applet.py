9c5bf7452ec79961460f5f13df6a9f9b4e640f0c
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-03-12 22:28:42 +0000

    Update to 2.14.0.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@5879 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/deskbar-applet/files/patch-deskbar_deskbar-applet.py
+++ deskutils/deskbar-applet/files/patch-deskbar_deskbar-applet.py
@@ -1,13 +1,15 @@
---- deskbar/deskbar-applet.py.orig	Sat Nov 19 14:15:58 2005
-+++ deskbar/deskbar-applet.py	Thu Dec  8 22:23:00 2005
-@@ -34,8 +34,8 @@
- gettext.bindtextdomain('deskbar-applet', abspath(join(deskbar.defs.DATA_DIR, "locale")))
+--- deskbar/deskbar-applet.py.orig	Sat Mar 11 18:05:44 2006
++++ deskbar/deskbar-applet.py	Sun Mar 12 17:19:57 2006
+@@ -41,9 +41,9 @@ gettext.bindtextdomain('deskbar-applet',
+ gettext.bind_textdomain_codeset('deskbar-applet','UTF-8')
  gettext.textdomain('deskbar-applet')
  
 -locale.bindtextdomain('deskbar-applet', abspath(join(deskbar.defs.DATA_DIR, "locale")))
+-locale.bind_textdomain_codeset('deskbar-applet','UTF-8')
 -locale.textdomain('deskbar-applet')
 +#locale.bindtextdomain('deskbar-applet', abspath(join(deskbar.defs.DATA_DIR, "locale")))
++#locale.bind_textdomain_codeset('deskbar-applet','UTF-8')
 +#locale.textdomain('deskbar-applet')
  
- def applet_factory(applet, iid):
- 	print 'Starting Deskbar instance:', applet, iid
+ import gtkexcepthook
+ 
