1513fe1ea82ca979f54d1d3a47d21f91563d727b
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-04-20 14:00:04 +0000

    Update to 2.14.1.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6094 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-address.c
+++ devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-address.c
@@ -1,5 +1,5 @@
---- libgnomevfs/gnome-vfs-address.c.orig	Tue Dec 13 21:41:32 2005
-+++ libgnomevfs/gnome-vfs-address.c	Wed Dec 14 00:15:32 2005
+--- libgnomevfs/gnome-vfs-address.c.orig	Wed Apr 12 10:16:28 2006
++++ libgnomevfs/gnome-vfs-address.c	Thu Apr 20 09:52:12 2006
 @@ -32,6 +32,7 @@
  #include <sys/types.h>
  
@@ -8,14 +8,3 @@
  #include <sys/select.h>
  #include <netinet/in.h>
  #include <arpa/inet.h>
-@@ -388,8 +389,8 @@ static gboolean
- v6_v6_equal (const struct sockaddr_in6 *a,
- 	     const struct sockaddr_in6 *b)
- {
--	return IN6_ARE_ADDR_EQUAL (a->sin6_addr.s6_addr,
--				   b->sin6_addr.s6_addr);
-+	return IN6_ARE_ADDR_EQUAL (&a->sin6_addr,
-+				   &b->sin6_addr);
- }
- 
- static gboolean
