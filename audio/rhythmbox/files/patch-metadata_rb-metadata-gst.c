31ca390e4cd2fcd65d91c80d4f92545cb8f2936c
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-06-03 04:21:56 +0000

    The error cannot be NULL, so instead set the error message to a single space.
    Obtained from:	Rhythmbox CVS
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6362 df743ca5-7f9a-e211-a948-0013205c9059

--- audio/rhythmbox/files/patch-metadata_rb-metadata-gst.c
+++ audio/rhythmbox/files/patch-metadata_rb-metadata-gst.c
@@ -1,15 +1,11 @@
---- metadata/rb-metadata-gst.c.orig	Fri Jun  2 15:42:35 2006
-+++ metadata/rb-metadata-gst.c	Fri Jun  2 16:15:13 2006
-@@ -1065,10 +1065,8 @@ rb_metadata_load (RBMetaData *md,
- 				     "%s", msg);
- 			g_free (msg);
- 		} else {
--			g_set_error (error,
--				     RB_METADATA_ERROR,
--				     RB_METADATA_ERROR_NOT_AUDIO_IGNORE,
+--- metadata/rb-metadata-gst.c.orig	Sat Jun  3 00:21:25 2006
++++ metadata/rb-metadata-gst.c	Sat Jun  3 00:21:34 2006
+@@ -1068,7 +1068,7 @@ rb_metadata_load (RBMetaData *md,
+ 			g_set_error (error,
+ 				     RB_METADATA_ERROR,
+ 				     RB_METADATA_ERROR_NOT_AUDIO_IGNORE,
 -				     NULL);
-+			if (error != NULL)
-+				*error = NULL;
++				     " ");
  		}
  	} else {
  		/* yay, it worked */
