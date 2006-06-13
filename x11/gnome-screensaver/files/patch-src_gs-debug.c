6596625ca933b0091a020ad9f13c85048cf97f2e
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-06-13 19:28:55 +0000

    Fix a crash in gnome-screensaver-dialog that prevented unlocking.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6437 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11/gnome-screensaver/files/patch-src_gs-debug.c
@@ -0,0 +1,19 @@
+--- gs-debug.c.orig	Tue Jun 13 15:18:14 2006
++++ gs-debug.c	Tue Jun 13 15:21:32 2006
+@@ -132,9 +132,13 @@ _gs_profile_log (const char *func,
+         char   *str;
+         char   *formatted;
+ 
+-        va_start (args, format);
+-        formatted = g_strdup_vprintf (format, args);
+-        va_end (args);
++	if (format == NULL)
++		formatted = g_strdup ("");
++	else {
++        	va_start (args, format);
++        	formatted = g_strdup_vprintf (format, args);
++        	va_end (args);
++	}
+ 
+         if (func != NULL) {
+                 str = g_strdup_printf ("MARK: %s %s: %s %s", g_get_prgname(), func, note ? note : "", formatted);
