bc035d06c116b6cc2d1f4b51b50ddee90ea49699
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-06-14 02:52:43 +0000

    Add another icon hack.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6439 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -3,7 +3,7 @@
 #
 # $FreeBSD$
 #	$NetBSD: $
-#     $MCom: ports/Mk/bsd.gnome.mk,v 1.388 2006/05/29 05:34:30 mezz Exp $
+#     $MCom: ports/Mk/bsd.gnome.mk,v 1.389 2006/06/13 18:37:10 marcus Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -83,6 +83,7 @@ gnomehack_PRE_PATCH=	${FIND} ${WRKSRC} -name "Makefile.in*" -type f | ${XARGS} $
 				 s|[(]libdir[)]/pkgconfig|(prefix)/libdata/pkgconfig|g ; \
 				 s|[$$][(]localstatedir[)]/scrollkeeper|${SCROLLKEEPER_DIR}|g ; \
 				 s|[(]datadir[)]/icons/hicolor|(prefix)/share/icons/hicolor|g ; \
+				 s|{datadir}/icons/hicolor|{prefix}/share/icons/hicolor|g ; \
 				 s|[(]libdir[)]/bonobo/servers|(prefix)/libdata/bonobo/servers|g' ; \
 			${FIND} ${WRKSRC} -name "configure" -type f | ${XARGS} ${REINPLACE_CMD} -e \
 				's|-lpthread|${PTHREAD_LIBS}|g ; \
