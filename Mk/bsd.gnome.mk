7ad2bc2e88b9d641a0bbc0fbd3d741c46f7cd203
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-07-05 18:07:35 +0000

    Sync with the ports tree.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6510 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -3,7 +3,7 @@
 #
 # $FreeBSD$
 #	$NetBSD: $
-#     $MCom: ports/Mk/bsd.gnome.mk,v 1.389 2006/06/13 18:37:10 marcus Exp $
+#     $MCom: ports/Mk/bsd.gnome.mk,v 1.390 2006/06/14 02:52:43 marcus Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -621,18 +621,18 @@ ${component}_USE_GNOME_IMPL+=${${subcomponent}_USE_GNOME_IMPL}
 # Then use already expanded USE_GNOME_IMPL to expand USE_GNOME.
 # Also, check to see if each component has a desktop requirement.  If it does,
 # and if the user's chosen desktop is not of the same version, mark the
-# port as broken.
+# port as IGNORE.
 . for component in ${USE_GNOME}
 .      if defined(GNOME_DESKTOP_VERSION) && \
 	defined(${component}_GNOME_DESKTOP_VERSION)
 .         if ${GNOME_DESKTOP_VERSION}!=${${component}_GNOME_DESKTOP_VERSION}
-BROKEN=	${PORTNAME} wants to use the GNOME
-BROKEN+=${${component}_GNOME_DESKTOP_VERSION} desktop, but you wish to use
-BROKEN+=the GNOME ${GNOME_DESKTOP_VERSION} desktop
+IGNORE=	cannot install: ${PORTNAME} wants to use the GNOME
+IGNORE+=${${component}_GNOME_DESKTOP_VERSION} desktop, but you wish to use
+IGNORE+=the GNOME ${GNOME_DESKTOP_VERSION} desktop
 .         endif
 .      endif
 .  if ${_USE_GNOME_ALL:M${component}}==""
-BROKEN=	Unknown component ${component}
+IGNORE=	cannot install: Unknown component ${component}
 .  endif
 _USE_GNOME+=	${${component}_USE_GNOME_IMPL} ${component}
 . endfor
@@ -655,7 +655,7 @@ ltverhack_PRE_PATCH=	${CP} -pf ${LTMAIN} ${WRKDIR}/gnome-ltmain.sh && \
 						done;
 .else
 .  if ${USE_GNOME:Mltverhack}!=""
-BROKEN=	${PORTNAME} uses the ltverhack GNOME component but does not use libtool
+BROKEN=	cannot install: ${PORTNAME} uses the ltverhack GNOME component but does not use libtool
 .  endif
 .endif
 
