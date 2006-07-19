fbef24b936c9210f39ea9ef1a687f6631d7605f4
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2006-07-19 19:56:20 +0000

    - Update bluecurve-themes through gtk-long.. for LOCALBASE
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6663 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11-themes/gnome-icons/bsd.gnome-icons.mk
@@ -0,0 +1,40 @@
+# New ports collection makefile for:	Gnome iconset
+# Date created:				29 Feb 2004
+# Whom:					Tom McLaughlin <tmclaugh@sdf.lonestar.org>
+#
+# $FreeBSD$
+#
+
+# Port logic gratuitously stolen from x11-themes/kde-icons-noia by
+# lioux@.
+
+PKGNAMEPREFIX=	gnome-icons-
+
+NO_BUILD=	yes
+
+REASON=		Themes may contain artwork not done by the author. \
+		Keep FreeBSD safe if theme author violated copyrights.
+
+PLIST=		${WRKDIR}/plist
+
+WANT_GNOME=	yes
+INSTALLS_ICONS=	yes
+
+pre-install:
+	@${RM} -rf ${PLIST}
+
+	@cd ${WRKDIR} && ${FIND} * ! -type d | ${SORT} >> ${PLIST}; \
+	${SED} -i "" -e "s:^:share/icons/:" ${PLIST}; \
+	${FIND} * -type d ! -empty | ${SORT} -r | \
+		${SED} -e "s:^:@dirrm share/icons/:" | \
+		${GREP} / >> ${PLIST}; \
+	${ECHO} "@unexec /bin/rmdir %D/share/icons 2> /dev/null || \
+		/usr/bin/true" >> ${PLIST}
+
+do-install:
+	cd ${WRKDIR} && ${FIND} * -type d ! -empty \
+		-exec ${MKDIR} -m 0755 \
+		${PREFIX}/share/icons/"{}" \;
+	cd ${WRKDIR} && ${FIND} * ! -type d ! -name 'plist' \
+		-exec ${INSTALL_DATA} ${WRKDIR}/"{}" \
+		${PREFIX}/share/icons/"{}" \;
