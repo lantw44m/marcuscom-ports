6fd4afa2f1a534545f82ce94be37a51b5e2173ac
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2006-07-25 02:20:26 +0000

    - Update to 2.15.90
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6767 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gnome-vfs/files/patch-modules_file-method-acl.c
+++ devel/gnome-vfs/files/patch-modules_file-method-acl.c
@@ -1,11 +1,11 @@
---- modules/file-method-acl.c.orig	Tue Jun 13 16:04:46 2006
-+++ modules/file-method-acl.c	Tue Jul 11 09:36:57 2006
+--- modules/file-method-acl.c.orig	Mon Jul 24 18:47:12 2006
++++ modules/file-method-acl.c	Mon Jul 24 22:01:34 2006
 @@ -45,7 +45,7 @@
  #endif
  
  #ifdef HAVE_POSIX_ACL
 -# include <acl/libacl.h>
 +# include <sys/acl.h>
- #define HAVE_ACL
+ #define HAVE_ACL 1
  #endif
  
