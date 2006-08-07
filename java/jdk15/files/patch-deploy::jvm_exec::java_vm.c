3131debe925e4389bbdc70573ac73d836b45ecfc
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2006-08-07 19:20:01 +0000

    Forgot to run 'cvs add' to add these files.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@6961 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ java/jdk15/files/patch-deploy::jvm_exec::java_vm.c
@@ -0,0 +1,13 @@
+$FreeBSD: ports/java/jdk15/files/patch-deploy::jvm_exec::java_vm.c,v 1.1 2006/07/18 23:23:02 glewis Exp $
+
+--- ../../deploy/src/plugin/solaris/jvm_exec/java_vm.c.orig	Thu Jul  6 17:10:12 2006
++++ ../../deploy/src/plugin/solaris/jvm_exec/java_vm.c	Thu Jul  6 17:10:12 2006
+@@ -157,7 +157,7 @@
+     libjvm = dlopen(jvmpath, RTLD_NOW + RTLD_GLOBAL);
+   
+     if (libjvm == NULL) {
+-        snprintf(jvmpath, sizeof jvmpath, "%s/lib/"LIBARCH"/%s", java_home, libname);
++        snprintf(jvmpath, sizeof jvmpath, "%s/lib/"LIBARCH"/server/%s", java_home, libname);
+         libjvm = dlopen(jvmpath, RTLD_NOW + RTLD_GLOBAL);
+     }
+ 
---
+++ java/jdk15/files/patch-deploy::nscore::JavaVM5.cpp
@@ -0,0 +1,16 @@
+$FreeBSD: ports/java/jdk15/files/patch-deploy::nscore::JavaVM5.cpp,v 1.1 2006/07/18 23:23:02 glewis Exp $
+
+--- ../../deploy/src/plugin/solaris/nscore/JavaVM5.cpp.orig	Thu Jun 15 18:08:03 2006
++++ ../../deploy/src/plugin/solaris/nscore/JavaVM5.cpp	Thu Jul  6 17:10:37 2006
+@@ -1289,7 +1289,11 @@
+   //          jre/lib/<arch>/classic/libjvm.so
+   // (to permit hotspot later).  So this path is only needed for 1.2
+ 
++#if defined(_ALLBSD_SOURCE) && defined(__amd64__)
++      sprintf(buff, "LD_LIBRARY_PATH=%s/lib/%s/server:%s/lib/%s",
++#else
+       sprintf(buff, "LD_LIBRARY_PATH=%s/lib/%s/client:%s/lib/%s",
++#endif
+               jre, LIBARCH,                // libjvm
+               jre, LIBARCH);               // libjava, libawt & friends
+ 
