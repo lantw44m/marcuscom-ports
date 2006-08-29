fc91a98cc1d6a69ab3df64eb9e971380a1e39397
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-08-29 14:57:02 +0000

    Fix a runaway mapping-daemon process that is triggered by bad EOF checking on file descriptors.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@7168 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/nautilus-cd-burner/files/patch-src_mapping-protocol.c
+++ sysutils/nautilus-cd-burner/files/patch-src_mapping-protocol.c
@@ -1,5 +1,5 @@
 --- src/mapping-protocol.c.orig	Sun Aug 20 08:47:20 2006
-+++ src/mapping-protocol.c	Tue Aug 22 01:28:24 2006
++++ src/mapping-protocol.c	Tue Aug 29 10:53:50 2006
 @@ -26,6 +26,8 @@
  
  #include "config.h"
@@ -9,3 +9,17 @@
  #include <stdio.h>
  #include <unistd.h>
  #include <string.h>
+@@ -1188,6 +1190,13 @@ mapping_protocol_channel_fill_read_buffe
+ 	}
+ 
+ 	status = g_io_channel_read_chars (channel->iochannel, data, count, &length, NULL);
++
++	if (status == G_IO_STATUS_EOF) {
++		channel->read_watch_id = 0;
++		if (channel->message_handler != NULL) {
++			channel->message_handler (channel, NULL, channel->user_data);
++		}
++	}
+ 
+ 	if (status != G_IO_STATUS_NORMAL) {
+ 		return;
