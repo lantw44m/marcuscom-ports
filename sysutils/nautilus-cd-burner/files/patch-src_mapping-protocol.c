a408932bc0fc6cc05147639a52594f954917618f
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-08-29 20:43:26 +0000

    Try a different, safer fix for the runaway mapping-daemon problem.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@7169 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/nautilus-cd-burner/files/patch-src_mapping-protocol.c
+++ sysutils/nautilus-cd-burner/files/patch-src_mapping-protocol.c
@@ -1,5 +1,5 @@
 --- src/mapping-protocol.c.orig	Sun Aug 20 08:47:20 2006
-+++ src/mapping-protocol.c	Tue Aug 29 10:53:50 2006
++++ src/mapping-protocol.c	Tue Aug 29 16:39:11 2006
 @@ -26,6 +26,8 @@
  
  #include "config.h"
@@ -9,17 +9,30 @@
  #include <stdio.h>
  #include <unistd.h>
  #include <string.h>
-@@ -1188,6 +1190,13 @@ mapping_protocol_channel_fill_read_buffe
- 	}
+@@ -86,6 +88,7 @@ static gboolean handle_write            
+ #ifdef DEBUG_ENABLE
+ 
+ static FILE *debug_out = NULL;
++static gboolean at_eof = FALSE;
+ 
+ static void
+ debug_init (void)
+@@ -1189,6 +1192,9 @@ mapping_protocol_channel_fill_read_buffe
  
  	status = g_io_channel_read_chars (channel->iochannel, data, count, &length, NULL);
-+
-+	if (status == G_IO_STATUS_EOF) {
-+		channel->read_watch_id = 0;
-+		if (channel->message_handler != NULL) {
-+			channel->message_handler (channel, NULL, channel->user_data);
-+		}
-+	}
  
++	if (status == G_IO_STATUS_EOF) {
++		at_eof = TRUE;
++	}
  	if (status != G_IO_STATUS_NORMAL) {
  		return;
+ 	}
+@@ -1340,7 +1346,7 @@ handle_read (GIOChannel             *sou
+ 		mapping_protocol_channel_do_iteration (channel);
+ 	}
+ 
+-	if (condition & G_IO_HUP) {
++	if (condition & G_IO_HUP || at_eof) {
+ 		channel->read_watch_id = 0;
+ 		res = FALSE;
+ 		/* send a NULL message to signal error */
