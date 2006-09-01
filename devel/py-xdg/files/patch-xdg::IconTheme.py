18f8492a218851d028498dfa5e01783772398b7b
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2006-09-01 20:58:01 +0000

    - Chase the gnome move to LOCALBASE with updated icon dirs
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@7183 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ devel/py-xdg/files/patch-xdg::IconTheme.py
@@ -0,0 +1,15 @@
+--- xdg/IconTheme.py.orig	Sun Jun  5 14:20:19 2005
++++ xdg/IconTheme.py	Sun Jun  5 14:21:07 2005
+@@ -215,7 +215,11 @@
+ icondirs = []
+ for basedir in xdg_data_dirs:
+ 	icondirs.append(os.path.join(basedir, "icons"))
+-icondirs.append("/usr/share/pixmaps")
++icondirs.append("%%X11BASE%%/share/pixmaps")
++icondirs.append("%%LOCALBASE%%/share/pixmaps")
++icondirs.append("%%LOCALBASE%%/share/gnome/pixmaps")
++icondirs.append("%%X11BASE%%/share/icons")
++icondirs.append("%%LOCALBASE%%/share/icons")
+ icondirs.append(os.path.expanduser("~/.icons"))
+ 
+ # just cache variables, they give a 10x speed improvement
