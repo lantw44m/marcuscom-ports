153f836b9e362f8d141d31eef3e41c1f86a439da
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-10-18 07:44:58 +0000

    Chase the evolutiondataserver shared lib version.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@7814 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -3,7 +3,7 @@
 #
 # $FreeBSD$
 #	$NetBSD: $
-#     $MCom: ports/Mk/bsd.gnome.mk,v 1.402 2006/10/14 10:01:36 marcus Exp $
+#     $MCom: ports/Mk/bsd.gnome.mk,v 1.403 2006/10/15 03:43:19 marcus Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -465,7 +465,7 @@ gnomespeech_LIB_DEPENDS=gnomespeech.7:${PORTSDIR}/accessibility/gnome-speech
 gnomespeech_DETECT=		${LOCALBASE}/libdata/pkgconfig/gnome-speech-1.0.pc
 gnomespeech_USE_GNOME_IMPL=libbonobo
 
-evolutiondataserver_LIB_DEPENDS=edataserver-1.2.7:${PORTSDIR}/databases/evolution-data-server
+evolutiondataserver_LIB_DEPENDS=edataserver-1.2.9:${PORTSDIR}/databases/evolution-data-server
 evolutiondataserver_DETECT=		${LOCALBASE}/libdata/pkgconfig/evolution-data-server-1.2.pc
 evolutiondataserver_USE_GNOME_IMPL=libgnomeui
 
