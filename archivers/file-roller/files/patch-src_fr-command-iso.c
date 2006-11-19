2feb676a88c0dc2a3dad18cfa77ccc062f84adb5
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2006-11-19 01:10:33 +0000

    Fix ISO extraction by making sure the fully-qualified path is passed to isoinfo, and the isoinfo.sh script does not echo any extraneous data to stdout.
    Reported by:	ahze
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@7934 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ archivers/file-roller/files/patch-src_fr-command-iso.c
@@ -0,0 +1,35 @@
+--- src/fr-command-iso.c.orig	Sat Nov 18 20:04:49 2006
++++ src/fr-command-iso.c	Sat Nov 18 20:05:00 2006
+@@ -159,6 +159,7 @@ fr_command_iso_extract (FRCommand  *comm
+ 			gboolean    junk_paths,
+ 			const char *password)
+ {
++	FRCommandIso  *comm_iso = FR_COMMAND_ISO (comm);
+ 	char  *e_dest_dir;
+ 	GList *scan;
+ 
+@@ -181,6 +182,8 @@ fr_command_iso_extract (FRCommand  *comm
+ 		if (e_temp_dest_dir == NULL) 
+ 			continue;
+ 
++		extract_path = g_build_filename (comm_iso->cur_path, path, NULL);
++
+ 		temp_dest_dir = unescape_str (e_temp_dest_dir);
+ 		ensure_dir_exists (temp_dest_dir, 0700);
+ 
+@@ -189,13 +192,14 @@ fr_command_iso_extract (FRCommand  *comm
+ 		fr_process_add_arg (comm->process, "-i");
+ 		fr_process_add_arg (comm->process, comm->e_filename);
+ 		fr_process_add_arg (comm->process, "-x");
+-		fr_process_add_arg (comm->process, path);
++		fr_process_add_arg (comm->process, extract_path);
+ 		fr_process_add_arg (comm->process, ">");
+ 		fr_process_add_arg (comm->process, filename);
+ 		fr_process_end_command (comm->process);
+ 
+ 		g_free (e_temp_dest_dir);
+ 		g_free (temp_dest_dir);
++		g_free (extract_path);
+ 	}
+ 
+ 	g_free (e_dest_dir);
