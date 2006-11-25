0e07efa86324b98a3f975da63a96a66aaaf7f371
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2006-11-25 21:59:20 +0000

    - Don't remove locale dirs with @ in the name if in mtree (ie: sr@Latn)
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@7953 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -5443,7 +5443,7 @@ generate-plist:
 		| ${SED} -e 's|./||; s| missing||' >> ${WRKDIR}/.locale.mtree
 . for a in 1-4 1-3
 	for i in `${GREP} "^share/locale/.*/LC_MESSAGES/.*\.mo" ${TMPPLIST} | ${SED} ${PLIST_SUB:S/$/!g/:S/^/ -e s!%%/:S/=/%%!/} | ${CUT} -d / -f ${a} | ${SORT} -r`; do \
-		if [ "/$${i}/" != "`${GREP} -o "/$${i}/" ${WRKDIR}/.locale.mtree`" ]; then \
+		if [ "$${i}//" != "`${GREP} -o "$${i}//" ${WRKDIR}/.locale.mtree`" ]; then \
 			${ECHO_CMD} "@unexec rmdir %D/$${i} 2>/dev/null || true" >> ${TMPPLIST} ; \
 		fi \
 	done
