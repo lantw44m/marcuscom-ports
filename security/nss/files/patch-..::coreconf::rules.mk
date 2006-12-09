4de35b0f9a63985137952b792f99ba253966fbf1
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2006-12-09 15:17:54 +0000

    - Add bin/nss-config to help phase out patch-sysnss - Install lib/* and bin/* so we don't have to keep installing small   bits of nss for new versions of gecko - move lib/ to lib/nss so we don't conflict with libssl
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@8018 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ security/nss/files/patch-..::coreconf::rules.mk
@@ -0,0 +1,10 @@
+--- ../coreconf/rules.mk.orig	Thu Apr  3 16:48:00 2003
++++ ../coreconf/rules.mk	Fri Apr 11 01:01:50 2003
+@@ -110,6 +110,7 @@
+ endif
+ ifdef SHARED_LIBRARY
+ 	$(INSTALL) -m 775 $(SHARED_LIBRARY) $(SOURCE_LIB_DIR)
++	ln -sf $(notdir $(SHARED_LIBRARY)) $(SOURCE_LIB_DIR)/$(notdir $(SHARED_LIBRARY:.so.1=.so))
+ endif
+ ifdef IMPORT_LIBRARY
+ 	$(INSTALL) -m 775 $(IMPORT_LIBRARY) $(SOURCE_LIB_DIR)
