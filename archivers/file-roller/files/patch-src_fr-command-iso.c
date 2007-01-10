62311a32b24f7ec64466955fb4c552e6676470f6
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-01-10 20:47:41 +0000

    Update to 2.17.5.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@8166 df743ca5-7f9a-e211-a948-0013205c9059

--- archivers/file-roller/files/patch-src_fr-command-iso.c
+++ archivers/file-roller/files/patch-src_fr-command-iso.c
@@ -1,23 +1,23 @@
---- src/fr-command-iso.c.orig	Tue Nov  7 04:03:30 2006
-+++ src/fr-command-iso.c	Sat Nov 18 20:22:18 2006
-@@ -159,6 +159,7 @@ fr_command_iso_extract (FRCommand  *comm
+--- src/fr-command-iso.c.orig	Mon Jan  8 17:06:36 2007
++++ src/fr-command-iso.c	Wed Jan 10 14:43:17 2007
+@@ -164,6 +164,7 @@ fr_command_iso_extract (FRCommand  *comm
  			gboolean    junk_paths,
  			const char *password)
  {
 +	FRCommandIso  *comm_iso = FR_COMMAND_ISO (comm);
  	char  *e_dest_dir;
+ 	char  *e_archive_filename;
  	GList *scan;
- 
-@@ -166,6 +167,7 @@ fr_command_iso_extract (FRCommand  *comm
+@@ -173,6 +174,7 @@ fr_command_iso_extract (FRCommand  *comm
  
  	for (scan = file_list; scan; scan = scan->next) {
  		char       *path = scan->data;
 +		char       *extract_path;
  		const char *filename;
                  char       *file_dir, *e_temp_dest_dir = NULL, *temp_dest_dir = NULL;
- 	
-@@ -180,6 +182,8 @@ fr_command_iso_extract (FRCommand  *comm
- 		if (e_temp_dest_dir == NULL) 
+ 
+@@ -187,6 +189,8 @@ fr_command_iso_extract (FRCommand  *comm
+ 		if (e_temp_dest_dir == NULL)
  			continue;
  
 +		extract_path = g_build_filename (comm_iso->cur_path, path, NULL);
@@ -25,16 +25,16 @@
  		temp_dest_dir = unescape_str (e_temp_dest_dir);
  		ensure_dir_exists (temp_dest_dir, 0700);
  
-@@ -188,13 +192,14 @@ fr_command_iso_extract (FRCommand  *comm
+@@ -195,13 +199,14 @@ fr_command_iso_extract (FRCommand  *comm
  		fr_process_add_arg (comm->process, "-i");
- 		fr_process_add_arg (comm->process, comm->e_filename);
+ 		fr_process_add_arg (comm->process, e_archive_filename);
  		fr_process_add_arg (comm->process, "-x");
 -		fr_process_add_arg (comm->process, path);
 +		fr_process_add_arg (comm->process, extract_path);
  		fr_process_add_arg (comm->process, ">");
  		fr_process_add_arg (comm->process, filename);
  		fr_process_end_command (comm->process);
- 	
+ 
  		g_free (e_temp_dest_dir);
  		g_free (temp_dest_dir);
 +		g_free (extract_path);
