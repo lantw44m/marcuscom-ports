d9c7bd42af64ecd29eadbff4023f1c435819488f
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2007-03-09 20:37:43 +0000

    Fix a hardcore path of /usr/local -> %%LOCALBASE%%. Somehow it has lost in MC and FreeBSD ports tree has it correct.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@8470 df743ca5-7f9a-e211-a948-0013205c9059

--- archivers/file-roller/files/patch-src_fr-command-tar.c
+++ archivers/file-roller/files/patch-src_fr-command-tar.c
@@ -5,7 +5,7 @@
  	if (g_file_test ("/usr/sfw/bin/gtar", G_FILE_TEST_IS_EXECUTABLE))
  		command = g_strdup ("/usr/sfw/bin/gtar");
 +#elif defined(__FreeBSD__)
-+	if (g_file_test ("/usr/local/bin/gtar", G_FILE_TEST_IS_EXECUTABLE))
++	if (g_file_test ("%%LOCALBASE%%/bin/gtar", G_FILE_TEST_IS_EXECUTABLE))
 +		command = g_strdup ("%%LOCALBASE%%/bin/gtar");
  #endif
  	if (command != NULL)
