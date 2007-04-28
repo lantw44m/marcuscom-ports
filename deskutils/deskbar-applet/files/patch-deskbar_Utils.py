da158b304450c2ddcc3d4dc4dfa8630ede3db8ce
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-04-28 05:39:16 +0000

    share/gnome -> share
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@8658 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ deskutils/deskbar-applet/files/patch-deskbar_Utils.py
@@ -0,0 +1,11 @@
+--- deskbar/Utils.py.orig	Tue Nov  1 20:32:51 2005
++++ deskbar/Utils.py	Tue Nov  1 20:33:08 2005
+@@ -10,7 +10,7 @@
+ 	
+ 	sysdirs = os.getenv("XDG_DATA_DIRS")
+ 	if sysdirs == None:
+-		sysdirs = "/usr/local/share:/usr/share"
++	    sysdirs = "/usr/local/share/:%%X11BASE%%/share/"
+ 	
+ 	dirs = "%s:%s" % (dirs, sysdirs)
+ 	return [dir for dir in dirs.split(":") if dir.strip() != "" and exists(dir)]
