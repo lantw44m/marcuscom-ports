51bfa8b0c3d761fd5f8e379bd1e8a58ef70632fe
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-05-19 21:53:14 +0000

    Remove support for a separate X11BASE.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@8886 df743ca5-7f9a-e211-a948-0013205c9059

--- misc/shared-mime-info/files/patch-update-mime-database.c
+++ misc/shared-mime-info/files/patch-update-mime-database.c
@@ -15,7 +15,7 @@
  	env = getenv("XDG_DATA_DIRS");
  	if (!env)
 -		env = "/usr/local/share/:/usr/share/";
-+		env = "/usr/local/share/:%%X11BASE%%/share/";
++		env = "/usr/local/share/";
  	dirs = g_strsplit(env, ":", 0);
  	g_return_if_fail(dirs != NULL);
  	for (n = 0; dirs[n]; n++)
