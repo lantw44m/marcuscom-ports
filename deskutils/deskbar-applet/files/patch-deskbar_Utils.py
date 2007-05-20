57727e912ce3a82fa5f8ba9438237937cffea880
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-05-20 22:03:10 +0000

    Remove support for a separate X11BASE.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@8891 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/deskbar-applet/files/patch-deskbar_Utils.py
+++ deskutils/deskbar-applet/files/patch-deskbar_Utils.py
@@ -5,7 +5,7 @@
  	sysdirs = os.getenv("XDG_DATA_DIRS")
  	if sysdirs == None:
 -		sysdirs = "/usr/local/share:/usr/share"
-+	    sysdirs = "/usr/local/share/:%%X11BASE%%/share/"
++	    sysdirs = "/usr/local/share"
  	
  	dirs = "%s:%s" % (dirs, sysdirs)
  	return [dir for dir in dirs.split(":") if dir.strip() != "" and exists(dir)]
