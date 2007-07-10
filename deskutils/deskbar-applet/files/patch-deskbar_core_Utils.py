d23dc519dd48cdf70d75821dda5d947013ef279e
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2007-07-10 20:50:36 +0000

    Update to 2.19.5. In the NEWS:
    =====================
    deskbar-applet 2.19.5
    =====================
    
    First release of the Google Summer of Code branch maintained by Sebastian
    PÃ¶lsterl. This release features major refactoring including a new GUI and
    Modules API. Therefore, old modules won't work with this version. No new
    big features are included, but this release should contain almost all the
    features that Deskbar had before refactoring.
    
    --
    
    So.. Keep in mind for it might be unstable, missing modules or so.
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9197 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/deskbar-applet/files/patch-deskbar_core_Utils.py
+++ deskutils/deskbar-applet/files/patch-deskbar_core_Utils.py
@@ -1,11 +1,11 @@
---- deskbar/Utils.py.orig	Tue Nov  1 20:32:51 2005
-+++ deskbar/Utils.py	Tue Nov  1 20:33:08 2005
-@@ -10,7 +10,7 @@
+--- deskbar/core/Utils.py.orig	Tue Jul 10 15:24:06 2007
++++ deskbar/core/Utils.py	Tue Jul 10 15:24:23 2007
+@@ -51,7 +51,7 @@
  	
  	sysdirs = os.getenv("XDG_DATA_DIRS")
  	if sysdirs == None:
 -		sysdirs = "/usr/local/share:/usr/share"
-+	    sysdirs = "/usr/local/share"
++		sysdirs = "/usr/local/share"
  	
  	dirs = "%s:%s" % (dirs, sysdirs)
  	return [dir for dir in dirs.split(":") if dir.strip() != "" and exists(dir)]
