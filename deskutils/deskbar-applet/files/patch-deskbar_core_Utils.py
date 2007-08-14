0d012b0b7f09066892e52662f33dc0f751de6b26
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-08-14 18:49:47 +0000

    Update to 2.19.90.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9396 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/deskbar-applet/files/patch-deskbar_core_Utils.py
+++ deskutils/deskbar-applet/files/patch-deskbar_core_Utils.py
@@ -1,11 +1,11 @@
---- deskbar/core/Utils.py.orig	Tue Jul 10 15:24:06 2007
-+++ deskbar/core/Utils.py	Tue Jul 10 15:24:23 2007
-@@ -51,7 +51,7 @@
- 	
- 	sysdirs = os.getenv("XDG_DATA_DIRS")
- 	if sysdirs == None:
--		sysdirs = "/usr/local/share:/usr/share"
-+		sysdirs = "/usr/local/share"
- 	
- 	dirs = "%s:%s" % (dirs, sysdirs)
- 	return [dir for dir in dirs.split(":") if dir.strip() != "" and exists(dir)]
+--- deskbar/core/Utils.py.orig	2007-08-13 13:07:20.000000000 -0400
++++ deskbar/core/Utils.py	2007-08-14 14:35:19.000000000 -0400
+@@ -51,7 +51,7 @@ def get_xdg_data_dirs():
+     
+     sysdirs = os.getenv("XDG_DATA_DIRS")
+     if sysdirs == None:
+-        sysdirs = "/usr/local/share:/usr/share"
++        sysdirs = "/usr/local/share"
+     
+     dirs = "%s:%s" % (dirs, sysdirs)
+     return [dir for dir in dirs.split(":") if dir.strip() != "" and exists(dir)]
