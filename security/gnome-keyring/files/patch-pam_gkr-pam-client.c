81bd189eb0bbc6260ddf2c673c1f5c265de4cc5f
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-08-14 05:21:53 +0000

    Fix the build on FreeBSD < 7-CURRENT.
    Reported by:	mezz
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9381 df743ca5-7f9a-e211-a948-0013205c9059

--- security/gnome-keyring/files/patch-pam_gkr-pam-client.c
+++ security/gnome-keyring/files/patch-pam_gkr-pam-client.c
@@ -1,14 +1,17 @@
---- pam/gkr-pam-client.c.orig	2007-08-12 16:42:30.000000000 -0400
-+++ pam/gkr-pam-client.c	2007-08-12 16:43:46.000000000 -0400
-@@ -29,6 +29,7 @@
+--- pam/gkr-pam-client.c.orig	2007-08-10 15:28:07.000000000 -0400
++++ pam/gkr-pam-client.c	2007-08-14 01:19:08.000000000 -0400
+@@ -29,6 +29,10 @@
  
  #include <sys/types.h>
  #include <sys/socket.h>
++#if defined(LOCAL_PEERCRED)
++#include <sys/param.h>
 +#include <sys/ucred.h>
++#endif
  #include <sys/un.h>
  #include <sys/uio.h>
  #include <sys/wait.h>
-@@ -90,7 +91,7 @@ check_peer_same_uid (int sock)
+@@ -90,7 +94,7 @@ check_peer_same_uid (int sock)
          socklen_t xuc_len = sizeof (xuc);
  
  	if (getsockopt (sock, SOL_SOCKET, LOCAL_PEERCRED, &xuc, &xuc_len) == 0 && 
--- security/gnome-keyring/files/patch-pam_gkr-pam-module.c
+++ security/gnome-keyring/files/patch-pam_gkr-pam-module.c
@@ -1,6 +1,6 @@
---- pam/gkr-pam-module.c.orig	2007-07-29 17:48:01.000000000 -0400
-+++ pam/gkr-pam-module.c	2007-07-29 17:50:09.000000000 -0400
-@@ -40,6 +40,8 @@
+--- pam/gkr-pam-module.c.orig	2007-08-10 15:46:32.000000000 -0400
++++ pam/gkr-pam-module.c	2007-08-14 01:19:49.000000000 -0400
+@@ -41,6 +41,8 @@
  #include <sys/stat.h>
  #include <sys/wait.h>
  
@@ -9,14 +9,16 @@
  #include <assert.h>
  #include <ctype.h>
  #include <errno.h>
-@@ -72,8 +74,8 @@ enum {
+@@ -73,8 +75,10 @@ enum {
  #define  STDERR  2
  
  /* Linux/BSD compatibility */
 -#ifndef PAM_AUTHTOK_RECOVERY_ERR
 -#define PAM_AUTHTOK_RECOVERY_ERR PAM_AUTHTOK_RECOVER_ERR
 +#ifndef PAM_AUTHTOK_RECOVER_ERR
++#ifdef PAM_AUTHTOK_RECOVERY_ERR
 +#define PAM_AUTHTOK_RECOVER_ERR PAM_AUTHTOK_RECOVERY_ERR
++#endif
  #endif
  
  /* -----------------------------------------------------------------------------
