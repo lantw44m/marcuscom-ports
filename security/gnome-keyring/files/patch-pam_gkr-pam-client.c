d390739469f3dc5c52910a153641155533b48c0f
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-08-15 23:40:53 +0000

    Include the headers at the correct location.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9424 df743ca5-7f9a-e211-a948-0013205c9059

--- security/gnome-keyring/files/patch-pam_gkr-pam-client.c
+++ security/gnome-keyring/files/patch-pam_gkr-pam-client.c
@@ -1,17 +1,18 @@
 --- pam/gkr-pam-client.c.orig	2007-08-10 15:28:07.000000000 -0400
-+++ pam/gkr-pam-client.c	2007-08-14 01:19:08.000000000 -0400
-@@ -29,6 +29,10 @@
++++ pam/gkr-pam-client.c	2007-08-15 19:37:52.000000000 -0400
+@@ -47,6 +47,11 @@
+ #include <ucred.h>
+ #endif
  
- #include <sys/types.h>
- #include <sys/socket.h>
 +#if defined(LOCAL_PEERCRED)
 +#include <sys/param.h>
 +#include <sys/ucred.h>
 +#endif
- #include <sys/un.h>
- #include <sys/uio.h>
- #include <sys/wait.h>
-@@ -90,7 +94,7 @@ check_peer_same_uid (int sock)
++
+ #define PAM_APP_NAME      "Auto Login (PAM)"
+ #define PAM_APP_NAME_LEN  (sizeof (PAM_APP_NAME) - 1)
+ 
+@@ -90,7 +95,7 @@ check_peer_same_uid (int sock)
          socklen_t xuc_len = sizeof (xuc);
  
  	if (getsockopt (sock, SOL_SOCKET, LOCAL_PEERCRED, &xuc, &xuc_len) == 0 && 
