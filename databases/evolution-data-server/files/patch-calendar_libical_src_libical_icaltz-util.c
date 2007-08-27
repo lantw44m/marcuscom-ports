e9e73588a03ff56c6cd7a92beddfcfb1f35544cf
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-08-27 20:26:40 +0000

    Fix some style issues with this patch.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9470 df743ca5-7f9a-e211-a948-0013205c9059

--- databases/evolution-data-server/files/patch-calendar_libical_src_libical_icaltz-util.c
+++ databases/evolution-data-server/files/patch-calendar_libical_src_libical_icaltz-util.c
@@ -1,10 +1,10 @@
---- calendar/libical/src/libical/icaltz-util.c.orig	2007-08-27 15:06:14.000000000 -0500
-+++ calendar/libical/src/libical/icaltz-util.c	2007-08-27 15:07:36.000000000 -0500
-@@ -21,12 +21,23 @@
-  */
- 
+--- calendar/libical/src/libical/icaltz-util.c.orig	2007-08-24 02:48:28.000000000 -0400
++++ calendar/libical/src/libical/icaltz-util.c	2007-08-27 16:23:59.000000000 -0400
+@@ -23,6 +23,15 @@
  #include <string.h>
-+#ifdef __FreeBSD__ /* FreeBSD */
+ #if defined(sun) && defined(__SVR4)
+ #include <sys/byteorder.h>
++#elif defined(__FreeBSD__)
 +#include <sys/endian.h>
 +#define __BYTE_ORDER            _BYTE_ORDER
 +#define __LITTLE_ENDIAN         _LITTLE_ENDIAN
@@ -13,14 +13,6 @@
 +#define bswap_16                bswap16
 +#define bswap_32                bswap32
 +#define bswap_64                bswap64
-+#else /* FreeBSD */
- #if defined(sun) && defined(__SVR4)
- #include <sys/byteorder.h>
  #else
  #include <byteswap.h>
  #include <endian.h>
- #endif
-+#endif /* FreeBSD */
- #include <limits.h>
- #include <time.h>
- #include <stdlib.h>
