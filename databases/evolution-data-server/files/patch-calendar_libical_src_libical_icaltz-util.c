a9f730228ab495204a7ba3766df5b4743957e541
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2007-08-27 20:19:13 +0000

    Update to 1.11.91.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9469 df743ca5-7f9a-e211-a948-0013205c9059

--- databases/evolution-data-server/files/patch-calendar_libical_src_libical_icaltz-util.c
+++ databases/evolution-data-server/files/patch-calendar_libical_src_libical_icaltz-util.c
@@ -1,10 +1,10 @@
---- calendar/libical/src/libical/icaltz-util.c.orig	2007-08-01 03:16:33.000000000 -0400
-+++ calendar/libical/src/libical/icaltz-util.c	2007-08-01 03:18:44.000000000 -0400
-@@ -21,8 +21,19 @@
+--- calendar/libical/src/libical/icaltz-util.c.orig	2007-08-27 15:06:14.000000000 -0500
++++ calendar/libical/src/libical/icaltz-util.c	2007-08-27 15:07:36.000000000 -0500
+@@ -21,12 +21,23 @@
   */
  
  #include <string.h>
-+#ifdef __FreeBSD__
++#ifdef __FreeBSD__ /* FreeBSD */
 +#include <sys/endian.h>
 +#define __BYTE_ORDER            _BYTE_ORDER
 +#define __LITTLE_ENDIAN         _LITTLE_ENDIAN
@@ -13,10 +13,14 @@
 +#define bswap_16                bswap16
 +#define bswap_32                bswap32
 +#define bswap_64                bswap64
-+#else
++#else /* FreeBSD */
+ #if defined(sun) && defined(__SVR4)
+ #include <sys/byteorder.h>
+ #else
  #include <byteswap.h>
  #include <endian.h>
-+#endif
+ #endif
++#endif /* FreeBSD */
  #include <limits.h>
  #include <time.h>
  #include <stdlib.h>
