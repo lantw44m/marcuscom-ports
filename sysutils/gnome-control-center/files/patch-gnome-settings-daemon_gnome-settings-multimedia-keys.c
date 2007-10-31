43a8bcc3a9e0cec9d2091087240cfa08089deb6c
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2007-10-31 20:12:43 +0000

    Use nautilus-cd-burner to eject cd's instead of cdcontrol/eject. n-c-b currently uses cdcontrol as its default method of ejecting cd's. However with WITH_GNOMEMOUNT defined it will use gnome-mount (the preferred way since it umounts then ejects the cd instead of just ejecting).
    code based around the way sound-juicer handles ejecting cd's.
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9893 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/gnome-control-center/files/patch-gnome-settings-daemon_gnome-settings-multimedia-keys.c
+++ sysutils/gnome-control-center/files/patch-gnome-settings-daemon_gnome-settings-multimedia-keys.c
@@ -1,19 +1,41 @@
 --- gnome-settings-daemon/gnome-settings-multimedia-keys.c.orig	2007-09-18 05:32:43.000000000 -0400
-+++ gnome-settings-daemon/gnome-settings-multimedia-keys.c	2007-10-28 13:45:28.000000000 -0400
-@@ -610,8 +610,11 @@
++++ gnome-settings-daemon/gnome-settings-multimedia-keys.c	2007-10-31 16:01:14.953632933 -0400
+@@ -31,6 +31,7 @@
+ #include <glib/gi18n.h>
+ #include <gdk/gdkx.h>
+ #include <gtk/gtk.h>
++#include <nautilus-burn.h>
+ 
+ #include "eggaccelerators.h"
+ 
+@@ -609,9 +610,24 @@
+ 					   GCONF_MISC_DIR "/eject_command", NULL);
  	if ((command != NULL) && (strcmp (command, "") != 0))
  		execute (acme, command, FALSE, FALSE);
- 	else
-+#if __FreeBSD__
-+		execute (acme, "cdcontrol eject", FALSE, FALSE);
-+#else
- 		execute (acme, "eject -T", FALSE, FALSE);
--
-+#endif
+-	else
+-		execute (acme, "eject -T", FALSE, FALSE);
++	else {
++		g_type_init ();
++		nautilus_burn_init ();
++		GList *drives, *drivelist;
++		NautilusBurnDriveMonitor *monitor;
++
++		monitor = nautilus_burn_get_drive_monitor ();
++		drives = nautilus_burn_drive_monitor_get_drives (monitor);
++
++		for (drivelist = drives; drivelist != NULL; drivelist = drivelist->next) {
++			NautilusBurnDrive *drive;
++			drive = drivelist->data;
+ 
++			nautilus_burn_drive_eject (drive);
++		}
++        	nautilus_burn_shutdown ();
++	}
++		
  	g_free (command);
  }
  
-@@ -708,7 +711,7 @@
+@@ -708,7 +724,7 @@
  		do_mail_action (acme);
  		break;
  	case SLEEP_KEY:
