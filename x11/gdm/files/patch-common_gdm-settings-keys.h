a15b2f79b4cbb6f2dd6f55a8af4a5d40e2f4e328
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2007-12-13 17:42:49 +0000

    Allow gdm to be started from rc.d.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@9964 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ x11/gdm/files/patch-common_gdm-settings-keys.h
@@ -0,0 +1,10 @@
+--- common/gdm-settings-keys.h.orig	2007-12-13 12:12:17.000000000 -0500
++++ common/gdm-settings-keys.h	2007-12-13 12:12:47.000000000 -0500
+@@ -27,6 +27,7 @@ G_BEGIN_DECLS
+ 
+ #define GDM_KEY_USER "daemon/User"
+ #define GDM_KEY_GROUP "daemon/Group"
++#define GDM_KEY_VT "daemon/VT"
+ 
+ #define GDM_KEY_XDMCP "xdmcp/Enable"
+ #define GDM_KEY_MAX_PENDING "xdmcp/MaxPending"
--- x11/gdm/files/patch-daemon-gdm-server.c
+++ x11/gdm/files/patch-daemon-gdm-server.c
@@ -1,5 +1,5 @@
---- daemon/gdm-server.c.orig	2007-11-01 09:55:49.964708363 -0400
-+++ daemon/gdm-server.c	2007-11-01 09:56:08.940482938 -0400
+--- daemon/gdm-server.c.orig	2007-11-09 10:40:03.000000000 -0500
++++ daemon/gdm-server.c	2007-12-13 12:35:56.000000000 -0500
 @@ -32,6 +32,7 @@
  #include <pwd.h>
  #include <grp.h>
@@ -8,3 +8,64 @@
  
  #include <glib.h>
  #include <glib/gi18n.h>
+@@ -43,6 +44,10 @@
+ #include "gdm-common.h"
+ #include "gdm-signal-handler.h"
+ 
++#include "gdm-settings.h"
++#include "gdm-settings-direct.h"
++#include "gdm-settings-keys.h"
++
+ #include "gdm-server.h"
+ 
+ extern char **environ;
+@@ -76,6 +81,7 @@ struct GdmServerPrivate
+         char    *parent_display_name;
+         char    *parent_auth_file;
+         char    *chosen_hostname;
++	char    *vt;
+ 
+         guint    child_watch_id;
+ };
+@@ -662,7 +668,7 @@ gdm_server_start (GdmServer *server)
+         gboolean res;
+ 
+         /* fork X server process */
+-        res = gdm_server_spawn (server, NULL);
++        res = gdm_server_spawn (server, server->priv->vt);
+ 
+         return res;
+ }
+@@ -889,12 +895,32 @@ static void
+ gdm_server_init (GdmServer *server)
+ {
+ 
++	int vt;
++	gboolean has_vt;
++	GdmSettings *settings = NULL;
+         server->priv = GDM_SERVER_GET_PRIVATE (server);
+ 
++	settings = gdm_settings_new ();
++	if (settings == NULL) {
++		g_assert ("Unable to initialize settings");
++	}
++
++	if (! gdm_settings_direct_init (settings, GDMCONFDIR "/gdm.schemas", "/")) {
++		g_assert ("Unable to initialize settings");
++	}
++
++	has_vt = gdm_settings_direct_get_int (GDM_KEY_VT, &vt);
++
++	g_object_unref (settings);
++
+         server->priv->pid = -1;
+         server->priv->command = g_strdup (X_SERVER " -br -verbose");
+         server->priv->log_dir = g_strdup (LOGDIR);
+ 
++	if (has_vt) {
++		server->priv->vt = g_strdup_printf ("vt %d", vt);
++	}
++
+         add_ready_handler (server);
+ }
+ 
