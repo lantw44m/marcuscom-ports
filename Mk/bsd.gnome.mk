8e857c1500299f65981df44ed44541c475963825
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2008-01-28 22:27:18 +0000

    Chase gconf2 dependency change.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@10253 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -3,7 +3,7 @@
 #
 # $FreeBSD$
 #	$NetBSD: $
-#     $MCom: ports/Mk/bsd.gnome.mk,v 1.436 2008/01/28 04:42:51 mezz Exp $
+#     $MCom: ports/Mk/bsd.gnome.mk,v 1.437 2008/01/28 18:11:02 mezz Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -301,7 +301,7 @@ libbonobo_USE_GNOME_IMPL=libxml2 orbit2
 
 gconf2_LIB_DEPENDS=	gconf-2.4:${PORTSDIR}/devel/gconf2
 gconf2_DETECT=		${LOCALBASE}/libdata/pkgconfig/gconf-2.0.pc
-gconf2_USE_GNOME_IMPL=	orbit2 libxml2 gtk20 linc
+gconf2_USE_GNOME_IMPL=	orbit2 libxml2 gtk20
 
 gnomevfs2_LIB_DEPENDS=	gnomevfs-2.0:${PORTSDIR}/devel/gnome-vfs
 gnomevfs2_DETECT=	${LOCALBASE}/libdata/pkgconfig/gnome-vfs-2.0.pc
