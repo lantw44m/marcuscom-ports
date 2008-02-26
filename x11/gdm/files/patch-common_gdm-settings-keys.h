f77151a9108580624ba710def2e0dc4c9072b23a
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2008-02-26 03:36:26 +0000

    Update to 2.21.8.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@10535 df743ca5-7f9a-e211-a948-0013205c9059

--- x11/gdm/files/patch-common_gdm-settings-keys.h
+++ x11/gdm/files/patch-common_gdm-settings-keys.h
@@ -1,10 +1,10 @@
---- common/gdm-settings-keys.h.orig	2007-12-13 12:12:17.000000000 -0500
-+++ common/gdm-settings-keys.h	2007-12-13 12:12:47.000000000 -0500
-@@ -27,6 +27,7 @@ G_BEGIN_DECLS
- 
- #define GDM_KEY_USER "daemon/User"
- #define GDM_KEY_GROUP "daemon/Group"
+--- common/gdm-settings-keys.h.orig	2008-02-20 16:27:34.000000000 -0500
++++ common/gdm-settings-keys.h	2008-02-25 22:23:35.000000000 -0500
+@@ -30,6 +30,7 @@ G_BEGIN_DECLS
+ #define GDM_KEY_TIMED_LOGIN_ENABLE "daemon/TimedLoginEnable"
+ #define GDM_KEY_TIMED_LOGIN_USER "daemon/TimedLogin"
+ #define GDM_KEY_TIMED_LOGIN_DELAY "daemon/TimedLoginDelay"
 +#define GDM_KEY_VT "daemon/VT"
  
- #define GDM_KEY_XDMCP "xdmcp/Enable"
+ #define GDM_KEY_XDMCP_ENABLE "xdmcp/Enable"
  #define GDM_KEY_MAX_PENDING "xdmcp/MaxPending"
