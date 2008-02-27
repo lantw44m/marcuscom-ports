141d11fe36564c2ecd08c2ce6c1309459b9a143b
mezz <mezz@df743ca5-7f9a-e211-a948-0013205c9059>
2008-02-27 04:07:54 +0000

    Correct it by move a line outside the function and put under the headers area.
    Reviewed by:	marcus
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@10553 df743ca5-7f9a-e211-a948-0013205c9059

--- security/gnome-keyring/files/patch-library_gnome-keyring.c
+++ security/gnome-keyring/files/patch-library_gnome-keyring.c
@@ -1,10 +1,11 @@
---- library/gnome-keyring.c.orig	2008-02-26 20:04:07.000000000 -0600
-+++ library/gnome-keyring.c	2008-02-26 20:04:11.000000000 -0600
-@@ -1786,6 +1786,7 @@
- 	GnomeKeyringResult res;
- 	gchar **daemonenv, **e;
- 	gchar **parts;
-+	extern char **environ;
+--- library/gnome-keyring.c.orig	2008-02-26 22:05:10.000000000 -0600
++++ library/gnome-keyring.c	2008-02-26 22:05:30.000000000 -0600
+@@ -50,6 +50,8 @@
+ #include <dbus/dbus.h>
+ #endif
  
- 	gkr_buffer_init_full (&send, 128, NORMAL_ALLOCATOR);
- 
++extern char **environ;
++
+ typedef enum {
+ 	CALLBACK_DONE,
+ 	CALLBACK_GET_STRING,
