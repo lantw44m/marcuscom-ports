1ccab45efae0d840955554fc0e433725a03ee932
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2008-04-04 15:38:38 +0000

    Update to 31623 Enable svg-fonts (required by --enable-svg)
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@10862 df743ca5-7f9a-e211-a948-0013205c9059

--- www/webkit-gtk2/files/patch-WebCore_plugins_gtk_PluginDatabaseGtk.cpp
+++ www/webkit-gtk2/files/patch-WebCore_plugins_gtk_PluginDatabaseGtk.cpp
@@ -1,11 +1,11 @@
---- WebCore/plugins/gtk/PluginDatabaseGtk.cpp.orig	2008-04-01 11:14:13.073176655 -0400
-+++ WebCore/plugins/gtk/PluginDatabaseGtk.cpp	2008-04-01 11:15:02.191591705 -0400
-@@ -80,7 +80,7 @@
-         g_strfreev(pluginPaths);
-     }
- 
--    path = g_build_filename(G_DIR_SEPARATOR_S "usr", "lib", "browser", "plugins", 0);
-+    path = g_build_filename(G_DIR_SEPARATOR_S %%_BROWSER_PLUGINS_DIR%% 0);
-     paths.append(path);
-     g_free(path);
-     path = g_build_filename(G_DIR_SEPARATOR_S "usr", "local", "lib", "mozilla", "plugins", 0);
+--- WebCore/plugins/gtk/PluginDatabaseGtk.cpp.orig	2008-04-03 19:24:32.000000000 -0400
++++ WebCore/plugins/gtk/PluginDatabaseGtk.cpp	2008-04-04 10:45:21.455604357 -0400
+@@ -78,7 +78,7 @@
+     directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "lib", "browser", "plugins", 0);
+     directories.append(directory);
+     g_free(directory);
+-    directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "local", "lib", "mozilla", "plugins", 0);
++    directory = g_build_filename(G_DIR_SEPARATOR_S %%_BROWSER_PLUGINS_DIR%% 0);
+     directories.append(directory);
+     g_free (directory);
+     directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "lib", "mozilla", "plugins", 0);
