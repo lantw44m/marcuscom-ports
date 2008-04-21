331a7306c1f06d0ee34bbf77a9aa1b6402270b35
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2008-04-21 13:47:47 +0000

    Update to 32284
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@10885 df743ca5-7f9a-e211-a948-0013205c9059

--- www/webkit-gtk2/files/patch-WebCore_plugins_gtk_PluginDatabaseGtk.cpp
+++ www/webkit-gtk2/files/patch-WebCore_plugins_gtk_PluginDatabaseGtk.cpp
@@ -1,11 +1,11 @@
---- WebCore/plugins/gtk/PluginDatabaseGtk.cpp.orig	2008-04-03 19:24:32.000000000 -0400
-+++ WebCore/plugins/gtk/PluginDatabaseGtk.cpp	2008-04-04 10:45:21.455604357 -0400
-@@ -78,7 +78,7 @@
-     directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "lib", "browser", "plugins", 0);
+--- WebCore/plugins/gtk/PluginDatabaseGtk.cpp.orig	2008-04-18 04:00:55.000000000 -0400
++++ WebCore/plugins/gtk/PluginDatabaseGtk.cpp	2008-04-21 08:15:45.665667131 -0400
+@@ -75,7 +75,7 @@
+         g_strfreev(pluginPaths);
+     }
+ 
+-    directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "lib", "browser", "plugins", NULL);
++    directory = g_build_filename(G_DIR_SEPARATOR_S %%_BROWSER_PLUGINS_DIR%% NULL);
      directories.append(directory);
      g_free(directory);
--    directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "local", "lib", "mozilla", "plugins", 0);
-+    directory = g_build_filename(G_DIR_SEPARATOR_S %%_BROWSER_PLUGINS_DIR%% 0);
-     directories.append(directory);
-     g_free (directory);
-     directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "lib", "mozilla", "plugins", 0);
+     directory = g_build_filename(G_DIR_SEPARATOR_S "usr", "local", "lib", "mozilla", "plugins", NULL);
