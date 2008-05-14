2277a33551420aeea303034174efef6fdd1a1286
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2008-05-14 02:24:39 +0000

    Chase the libgnomekbd shared lib version.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@10946 df743ca5-7f9a-e211-a948-0013205c9059

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -3,7 +3,7 @@
 #
 # $FreeBSD$
 #	$NetBSD: $
-#     $MCom: ports/Mk/bsd.gnome.mk,v 1.455 2008/05/07 18:50:20 mezz Exp $
+#     $MCom: ports/Mk/bsd.gnome.mk,v 1.456 2008/05/12 17:34:34 mezz Exp $
 #
 # Please view me with 4 column tabs!
 
@@ -510,7 +510,7 @@ gnomesharp20_RUN_DEPENDS=	${gnomesharp20_DETECT}:${PORTSDIR}/x11-toolkits/gnome-
 gnomesharp20_USE_GNOME_IMPL=	gnomepanel gtkhtml3 gtksharp20 librsvg2 vte
 
 libgnomekbd_DETECT=			${LOCALBASE}/libdata/pkgconfig/libgnomekbd.pc
-libgnomekbd_LIB_DEPENDS=	gnomekbd.2:${PORTSDIR}/x11/libgnomekbd
+libgnomekbd_LIB_DEPENDS=	gnomekbd.3:${PORTSDIR}/x11/libgnomekbd
 libgnomekbd_USE_GNOME_IMPL=	libgnomeui
 
 pygtksourceview_DETECT=		${LOCALBASE}/libdata/pkgconfig/pygtksourceview-2.0.pc
