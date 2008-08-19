f2b273928388495b4f8d15e766692f8ff264ab66
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2008-08-19 00:22:52 +0000

    Update to 2.23.90.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@11316 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/anjuta/files/patch-libanjuta_anjuta-launcher.c
+++ devel/anjuta/files/patch-libanjuta_anjuta-launcher.c
@@ -1,23 +1,5 @@
 --- libanjuta/anjuta-launcher.c.orig	2008-05-29 22:37:02.000000000 +0200
 +++ libanjuta/anjuta-launcher.c	2008-06-05 16:12:17.000000000 +0200
-@@ -67,13 +67,13 @@
- #define FILE_INPUT_BUFFER_SIZE		1048576
- #ifndef __MAX_BAUD
- #  if defined(B460800)
--#    define __MAX_BAUX B460800
-+#    define __MAX_BAUD B460800
- #  elif defined(B307200)
--#    define __MAX_BAUX B307200
-+#    define __MAX_BAUD B307200
- #  elif defined(B256000)
--#    define __MAX_BAUX B256000
-+#    define __MAX_BAUD B256000
- #  else
--#    define __MAX_BAUX B230400
-+#    define __MAX_BAUD B230400
- #  endif
- #endif
- 
 @@ -761,7 +761,8 @@ anjuta_launcher_scan_output (GIOChannel 
  		GError *err = NULL;
  		do
