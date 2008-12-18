a1841e46d4a0c50d5c42812b6bf9f3f7d1e5c5be
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2008-12-18 07:06:39 +0000

    Fix a logic error that prevented playing stereo sound files.
    Reported by:	Alexander Logvinov <freebsd@akavia.ru>
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@11825 df743ca5-7f9a-e211-a948-0013205c9059

--- audio/libcanberra/files/patch-src_oss.c
+++ audio/libcanberra/files/patch-src_oss.c
@@ -1,5 +1,14 @@
---- src/oss.c.orig	2008-09-07 12:54:12.000000000 -0400
-+++ src/oss.c	2008-09-07 12:54:18.000000000 -0400
+--- src/oss.c.orig	2008-08-20 22:11:17.000000000 -0400
++++ src/oss.c	2008-12-18 02:04:42.000000000 -0500
+@@ -235,7 +235,7 @@ static int open_oss(ca_context *c, struc
+     ca_return_val_if_fail(c, CA_ERROR_INVALID);
+     ca_return_val_if_fail(c->private, CA_ERROR_STATE);
+     ca_return_val_if_fail(out, CA_ERROR_INVALID);
+-    ca_return_val_if_fail(ca_sound_file_get_nchannels(out->file) > 2, CA_ERROR_NOTSUPPORTED);
++    ca_return_val_if_fail(ca_sound_file_get_nchannels(out->file) <= 2, CA_ERROR_NOTSUPPORTED);
+ 
+     p = PRIVATE(c);
+ 
 @@ -258,7 +258,7 @@ static int open_oss(ca_context *c, struc
              val = AFMT_S16_NE;
              break;
