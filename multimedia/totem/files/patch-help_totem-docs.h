d62b2c8641bdeb4706f22583d876938c11945f76
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2009-05-09 22:17:22 +0000

    Update to 2.27.1.
    Axe xine support. Not supported anymore.
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@12288 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ multimedia/totem/files/patch-help_totem-docs.h
@@ -0,0 +1,13 @@
+--- help/totem-docs.h.orig	2009-05-09 00:56:01.000000000 +0200
++++ help/totem-docs.h	2009-05-09 00:56:35.000000000 +0200
+@@ -1,7 +1,7 @@
+ const gchar* documentation_credits[] = {
+-t"Baptiste Mille-Mathias <baptiste.millemathias@gmail.org>",
+-t"Chee Bin HOH <cbhoh@gnome.org>",
+-t"Philip Withnall <philip@tecnocode.co.uk>",
++"Baptiste Mille-Mathias <baptiste.millemathias@gmail.org>",
++"Chee Bin HOH <cbhoh@gnome.org>",
++"Philip Withnall <philip@tecnocode.co.uk>",
+ 	NULL
+ };
+ 
