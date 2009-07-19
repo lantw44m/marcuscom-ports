3a7a52252c9d0bb70ad8682d35b84b019995aa61
avl <avl@df743ca5-7f9a-e211-a948-0013205c9059>
2009-07-19 11:56:31 +0000

    Update to 2.27.4.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@12472 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/hamster-applet/files/patch-hamster_hamster-applet.py
+++ deskutils/hamster-applet/files/patch-hamster_hamster-applet.py
@@ -1,11 +1,11 @@
---- hamster/hamster-applet.py.orig	2009-04-25 14:29:45.000000000 +0200
-+++ hamster/hamster-applet.py	2009-05-06 17:47:09.000000000 +0200
-@@ -40,14 +40,14 @@ if _check(name):
- import hamster.defs
+--- hamster/hamster-applet.py.orig	2009-07-19 21:41:20.000000000 +1000
++++ hamster/hamster-applet.py	2009-07-19 21:41:46.000000000 +1000
+@@ -37,14 +37,14 @@ from hamster import defs
+ from hamster.configuration import runtime
  
  # Setup i18n
--locale_dir = os.path.abspath(os.path.join(hamster.defs.DATA_DIR, "locale"))
-+#locale_dir = os.path.abspath(os.path.join(hamster.defs.DATA_DIR, "locale"))
+-locale_dir = os.path.abspath(os.path.join(defs.DATA_DIR, "locale"))
++#locale_dir = os.path.abspath(os.path.join(defs.DATA_DIR, "locale"))
  
 -for module in (gettext, locale):
 -    module.bindtextdomain('hamster-applet', locale_dir)
@@ -20,4 +20,4 @@
 +#        module.bind_textdomain_codeset('hamster-applet','UTF-8')
  
  
- hamster.__init_db()
+ from hamster.applet import HamsterApplet
