f9607860287cf203234a50fe644b574689d165c9
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2009-08-10 21:54:53 +0000

    Update to 1.3.4.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@12595 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gvfs/files/patch-metadata_crc32.h
+++
@@ -1,8 +0,0 @@
---- metadata/crc32.h.orig	2009-07-24 15:24:08.000000000 -0400
-+++ metadata/crc32.h	2009-07-24 15:23:51.000000000 -0400
-@@ -46,4 +46,4 @@
- 
- #include <glib.h>
- 
--guint32 crc32(const void *ptr, size_t len);
-+guint32 metadata_crc32(const void *ptr, size_t len);
