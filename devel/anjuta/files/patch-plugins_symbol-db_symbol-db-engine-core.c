a0e3bc7782b48bc55b53d4946b8673cca250cf4a
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2009-08-27 15:36:24 +0000

    Update to 2.27.91.0.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@12741 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/anjuta/files/patch-plugins_symbol-db_symbol-db-engine-core.c
+++ devel/anjuta/files/patch-plugins_symbol-db_symbol-db-engine-core.c
@@ -1,5 +1,5 @@
---- plugins/symbol-db/symbol-db-engine-core.c.orig	2009-07-27 16:14:31.000000000 +0200
-+++ plugins/symbol-db/symbol-db-engine-core.c	2009-07-28 00:06:02.000000000 +0200
+--- plugins/symbol-db/symbol-db-engine-core.c.orig	2009-08-27 09:17:03.000000000 +0200
++++ plugins/symbol-db/symbol-db-engine-core.c	2009-08-27 17:32:35.000000000 +0200
 @@ -1504,7 +1504,7 @@ sdb_engine_ctags_output_thread (gpointer
  
  			/* found out a new marker */ 
@@ -9,18 +9,18 @@
  	}
  	else 
  	{
-@@ -1830,8 +1830,8 @@ sdb_engine_scan_files_1 (SymbolDBEngine 
- 		gchar *temp_file;
+@@ -1831,8 +1831,8 @@ sdb_engine_scan_files_1 (SymbolDBEngine 
+ 		gint i = 0;
  		while (TRUE)
  		{
--			temp_file = g_strdup_printf ("/anjuta-%d_%ld.tags", getpid (),
--								 time (NULL));
-+			temp_file = g_strdup_printf ("%s/anjuta-%d_%ld.tags", SHARED_MEMORY_PREFIX, getpid (),
-+								 (long) time (NULL));
+-			temp_file = g_strdup_printf ("/anjuta-%d_%ld%d.tags", getpid (),
+-								 time (NULL), i++);
++			temp_file = g_strdup_printf ("%s/anjuta-%d_%ld%d.tags", SHARED_MEMORY_PREFIX, getpid (),
++								 (long) time (NULL), i++);
  			gchar *test;
  			test = g_strconcat (SHARED_MEMORY_PREFIX, temp_file, NULL);
  			if (g_file_test (test, G_FILE_TEST_EXISTS) == TRUE)
-@@ -1851,12 +1851,12 @@ sdb_engine_scan_files_1 (SymbolDBEngine 
+@@ -1852,12 +1852,12 @@ sdb_engine_scan_files_1 (SymbolDBEngine 
  		priv->shared_mem_str = temp_file;
  		
  		if ((priv->shared_mem_fd = 
@@ -35,7 +35,7 @@
  		priv->shared_mem_file = fdopen (priv->shared_mem_fd, "a+b");
  		/*DEBUG_PRINT ("temp_file %s", temp_file);*/
  
-@@ -2441,7 +2441,7 @@ sdb_engine_init (SymbolDBEngine * object
+@@ -2446,7 +2446,7 @@ sdb_engine_init (SymbolDBEngine * object
  static void
  sdb_engine_unlink_shared_files (gpointer key, gpointer value, gpointer user_data)
  {
@@ -44,7 +44,7 @@
  }
  
  static void 
-@@ -2530,7 +2530,7 @@ sdb_engine_finalize (GObject * object)
+@@ -2535,7 +2535,7 @@ sdb_engine_finalize (GObject * object)
  	
  	if (priv->shared_mem_str)
  	{
@@ -53,7 +53,7 @@
  		g_free (priv->shared_mem_str);
  		priv->shared_mem_str = NULL;
  	}
-@@ -6054,12 +6054,12 @@ symbol_db_engine_update_buffer_symbols (
+@@ -6059,12 +6059,12 @@ symbol_db_engine_update_buffer_symbols (
  		 * target buffer one */
  		base_filename = g_filename_display_basename (relative_path);
  		
