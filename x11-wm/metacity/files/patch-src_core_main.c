32a7c26f6733bdfce538ac0613fcf463ef00b4d4
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2009-10-03 03:38:01 +0000

    Fix the build on 6.X.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13084 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-wm/metacity/files/patch-src_core_main.c
+++ x11-wm/metacity/files/patch-src_core_main.c
@@ -1,5 +1,5 @@
---- src/core/main.c.orig	2009-05-03 11:54:25.000000000 +0200
-+++ src/core/main.c	2009-05-03 11:55:24.000000000 +0200
+--- src/core/main.c.orig	2009-09-08 16:55:35.000000000 -0400
++++ src/core/main.c	2009-10-02 23:36:53.000000000 -0400
 @@ -58,7 +58,11 @@
  
  #include <stdlib.h>
@@ -12,3 +12,13 @@
  #include <stdio.h>
  #include <string.h>
  #include <signal.h>
+@@ -378,6 +382,9 @@ sigterm_handler (int signum)
+ }
+ 
+ static guint sigchld_signal_id = 0;
++#ifndef CLD_EXITED
++#define CLD_EXITED 1
++#endif
+ 
+ static void
+ sigchld_handler (int signum, siginfo_t *info, void *context)
