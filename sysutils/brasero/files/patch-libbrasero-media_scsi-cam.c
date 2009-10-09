52242e92ef3aaf1f3360af05cff39cce73e076a8
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2009-10-09 04:09:11 +0000

    Save the errno before calling close().
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13103 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/brasero/files/patch-libbrasero-media_scsi-cam.c
+++ sysutils/brasero/files/patch-libbrasero-media_scsi-cam.c
@@ -1,11 +1,24 @@
---- libbrasero-media/scsi-cam.c.orig	2009-10-08 22:34:11.000000000 -0400
-+++ libbrasero-media/scsi-cam.c	2009-10-08 22:35:09.000000000 -0400
-@@ -193,6 +193,9 @@ brasero_device_handle_open (const gchar 
+--- libbrasero-media/scsi-cam.c.orig	2009-09-25 10:39:03.000000000 -0400
++++ libbrasero-media/scsi-cam.c	2009-10-09 00:06:22.000000000 -0400
+@@ -184,6 +184,8 @@ brasero_device_handle_open (const gchar 
+ 		handle->fd = fd;
+ 	}
+ 	else {
++		int serrno;
++
+ 		if (code) {
+ 			if (errno == EAGAIN
+ 			||  errno == EWOULDBLOCK
+@@ -193,6 +195,13 @@ brasero_device_handle_open (const gchar 
  				*code = BRASERO_SCSI_ERRNO;
  		}
  
++		serrno = errno;
++
 +		if (fd > -1)
 +			close (fd);
++
++		errno = serrno;
 +
  		return NULL;
  	}
