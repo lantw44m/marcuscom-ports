6b7c9a8270b623b1006ae87d7596d499f3978023
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2010-01-07 21:14:06 +0000

    Just restore the 2.28.1 patch to fix the libxul problem.  This will fix a crash on startup.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13381 df743ca5-7f9a-e211-a948-0013205c9059

--- x11/yelp/files/patch-src_yelp-gecko-utils.cpp
+++ x11/yelp/files/patch-src_yelp-gecko-utils.cpp
@@ -1,12 +1,13 @@
---- src/yelp-gecko-utils.cpp.orig	2010-01-07 15:44:16.000000000 -0500
-+++ src/yelp-gecko-utils.cpp	2010-01-07 15:46:48.000000000 -0500
-@@ -28,7 +28,9 @@
+--- src/yelp-gecko-utils.cpp.orig	2009-03-29 23:21:59.000000000 -0400
++++ src/yelp-gecko-utils.cpp	2009-03-29 23:25:48.000000000 -0400
+@@ -28,8 +28,10 @@
  #include <nsStringAPI.h>
  
  #ifdef HAVE_GECKO_1_9
-+#define XPCOM_GLUE 1
++#ifdef XPCOM_GLUE
  #include <gtkmozembed_glue.cpp>
-+#undef XPCOM_GLUE
  #endif
++#endif
  
  #include <gtkmozembed.h>
+ #include <gtkmozembed_internal.h>
