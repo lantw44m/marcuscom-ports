68d13c1a288b344db106c87226cc06b31aaf8898
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2010-01-17 12:31:16 +0000

    Update to 2.29.5.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13479 df743ca5-7f9a-e211-a948-0013205c9059

--- x11/gdm/files/patch-gui_simple-chooser_gdm-host-chooser-widget.c
+++ x11/gdm/files/patch-gui_simple-chooser_gdm-host-chooser-widget.c
@@ -1,20 +1,11 @@
---- gui/simple-chooser/gdm-host-chooser-widget.c.orig	2009-12-21 17:36:30.000000000 +0100
-+++ gui/simple-chooser/gdm-host-chooser-widget.c	2009-12-26 23:31:52.000000000 +0100
-@@ -443,7 +443,7 @@ find_broadcast_addresses (GdmHostChooser
-                         /* paranoia */
-                         ifreq.ifr_name[sizeof (ifreq.ifr_name) - 1] = '\0';
- 
--                        if (ioctl (sock, SIOCGIFFLAGS, &ifreq) < 0) {
-+                        if ((ioctl (sock, SIOCGIFFLAGS, &ifreq) < 0) && (errno != ENXIO)) {
-                                 g_warning ("Could not get SIOCGIFFLAGS for %s", ifr[i].ifr_name);
-                         }
- 
+--- gui/simple-chooser/gdm-host-chooser-widget.c.orig	2010-01-13 14:29:00.000000000 +0000
++++ gui/simple-chooser/gdm-host-chooser-widget.c	2010-01-17 13:04:14.000000000 +0000
 @@ -543,6 +543,8 @@ xdmcp_init (GdmHostChooserWidget *widget
  #ifdef ENABLE_IPV6
          widget->priv->socket_fd = socket (AF_INET6, SOCK_DGRAM, 0);
          if (widget->priv->socket_fd != -1) {
-+                int off = 0;
-+                setsockopt(widget->priv->socket_fd, IPPROTO_IPV6, IPV6_V6ONLY, (void *)&off, sizeof (off));
++		int off = 0;
++		setsockopt(widget->priv->socket_fd, IPPROTO_IPV6, IPV6_V6ONLY, (void *)&off, sizeof (off));
                  widget->priv->have_ipv6 = TRUE;
          }
  #endif
