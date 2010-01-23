c7d5a53531cec0d22de37378f5f1aac6da0e6c95
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2010-01-23 19:56:24 +0000

    Sync with FreeBSD ports.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13494 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/hal/files/patch-hald_freebsd_hf-usb2.c
+++ sysutils/hal/files/patch-hald_freebsd_hf-usb2.c
@@ -1,6 +1,40 @@
 --- hald/freebsd/hf-usb2.c.orig	2009-08-24 08:42:29.000000000 -0400
-+++ hald/freebsd/hf-usb2.c	2009-12-23 14:16:03.000000000 -0500
-@@ -98,10 +98,10 @@ hf_usb2_probe_interfaces(HalDevice *pare
++++ hald/freebsd/hf-usb2.c	2010-01-23 14:54:14.000000000 -0500
+@@ -42,22 +42,6 @@
+ static struct libusb20_backend *hf_usb2_be = NULL;
+ 
+ static void
+-hf_usb2_copy_parent (HalDevice *parent,
+-		     const char *key,
+-		     gpointer user_data)
+-{
+-  HalDevice *device;
+-
+-  g_return_if_fail(HAL_IS_DEVICE(parent));
+-  g_return_if_fail(HAL_IS_DEVICE(user_data));
+-
+-  device = HAL_DEVICE(user_data);
+-
+-  if (! strncmp(key, "usb_device.", strlen("usb_device.")))
+-    hal_device_copy_property(parent, key, device, key);
+-}
+-
+-static void
+ hf_usb2_probe_interfaces(HalDevice *parent)
+ {
+   int num_interfaces;
+@@ -79,9 +63,9 @@ hf_usb2_probe_interfaces(HalDevice *pare
+ 
+       hal_device_property_set_string(device, "info.subsystem", "usb");
+       hal_device_property_set_int(device, "usb.interface.number", i);
+-      hal_device_property_foreach(parent, hf_usb2_copy_parent, device);
+       hal_device_copy_property(parent, "info.product", device, "info.product");
+       hal_device_copy_property(parent, "info.vendor", device, "info.vendor");
++      hal_device_merge_with_rewrite(device, parent, "usb.", "usb_device.");
+ 
+       if (hf_device_preprobe(device))
+         {
+@@ -98,10 +82,10 @@ hf_usb2_probe_interfaces(HalDevice *pare
  	  if (driver)
              {
  	      if (! strcmp(driver, "ukbd"))
@@ -14,7 +48,7 @@
  	          hf_runner_run_sync(device, 0, "hald-probe-mouse", NULL);
  	        }
  	      else if (! strcmp(driver, "uhid"))
-@@ -192,11 +192,12 @@ hf_usb2_probe (void)
+@@ -192,11 +176,12 @@ hf_usb2_probe (void)
        addr = libusb20_dev_get_address(pdev);
  
        if (addr == 1)
@@ -29,7 +63,7 @@
        if (! parent || hal_device_property_get_bool(parent, "info.ignore"))
          continue;
  
-@@ -216,7 +217,13 @@ hf_usb2_devd_add (const char *name,
+@@ -216,7 +201,13 @@ hf_usb2_devd_add (const char *name,
    HalDevice *parent_device;
    int bus, addr, pbus, paddr;
  
@@ -44,7 +78,7 @@
      return FALSE;
    else if (strncmp(parent, "ugen", strlen("ugen")))
      return TRUE;
-@@ -232,7 +239,8 @@ hf_usb2_devd_add (const char *name,
+@@ -232,7 +223,8 @@ hf_usb2_devd_add (const char *name,
  
    parent_device = hf_device_store_match(hald_get_gdl(),
      "usb_device.bus_number", HAL_PROPERTY_TYPE_INT32, pbus,
@@ -54,7 +88,7 @@
  
    if (parent_device && ! hal_device_property_get_bool(parent_device,
        "info.ignore"))
-@@ -255,8 +263,6 @@ hf_usb2_devd_remove (const char *name,
+@@ -255,8 +247,6 @@ hf_usb2_devd_remove (const char *name,
  
    if (strncmp(name, "ugen", strlen("ugen")))
      return FALSE;
@@ -63,7 +97,7 @@
  
    if (sscanf(name, "ugen%i.%i", &bus, &addr) != 2)
      return FALSE;
-@@ -265,7 +271,8 @@ hf_usb2_devd_remove (const char *name,
+@@ -265,7 +255,8 @@ hf_usb2_devd_remove (const char *name,
  
    device = hf_device_store_match(hald_get_gdl(), "usb_device.bus_number",
      HAL_PROPERTY_TYPE_INT32, bus, "usb_device.port_number",
@@ -73,7 +107,7 @@
  
    if (device)
      {
-@@ -276,6 +283,23 @@ hf_usb2_devd_remove (const char *name,
+@@ -276,6 +267,23 @@ hf_usb2_devd_remove (const char *name,
    return FALSE;
  }
  
@@ -97,7 +131,7 @@
  HFHandler hf_usb2_handler = {
    .privileged_init	= hf_usb2_privileged_init,
    .probe		= hf_usb2_probe
-@@ -283,5 +307,6 @@ HFHandler hf_usb2_handler = {
+@@ -283,5 +291,6 @@ HFHandler hf_usb2_handler = {
  
  HFDevdHandler hf_usb2_devd_handler = {
    .add =	hf_usb2_devd_add,
