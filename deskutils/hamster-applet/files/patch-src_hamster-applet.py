37b6b56cb40c661cc4a1f796ec09b91fa049c97e
avl <avl@df743ca5-7f9a-e211-a948-0013205c9059>
2010-01-29 11:01:54 +0000

    Update to 2.29.6.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13528 df743ca5-7f9a-e211-a948-0013205c9059

--- deskutils/hamster-applet/files/patch-src_hamster-applet.py
+++ deskutils/hamster-applet/files/patch-src_hamster-applet.py
@@ -1,22 +1,22 @@
---- hamster/hamster-applet.py.orig	2009-12-18 20:42:05.000000000 +0100
-+++ hamster/hamster-applet.py	2009-12-25 16:38:19.000000000 +0100
-@@ -102,13 +102,13 @@ if __name__ == "__main__":
-         from hamster.configuration import runtime
-         
+--- src/hamster-applet.py.orig	2010-01-29 17:37:34.000000000 +0800
++++ src/hamster-applet.py	2010-01-29 17:38:21.000000000 +0800
+@@ -95,13 +95,13 @@ if __name__ == "__main__":
+         from hamster.configuration import runtime, dialogs
+ 
          # Setup i18n
--        locale_dir = os.path.abspath(os.path.join(defs.DATA_DIR, "locale"))        
+-        locale_dir = os.path.abspath(os.path.join(defs.DATA_DIR, "locale"))
 -        for module in (gettext, locale):
 -            module.bindtextdomain('hamster-applet', locale_dir)
 -            module.textdomain('hamster-applet')
-+#        locale_dir = os.path.abspath(os.path.join(defs.DATA_DIR, "locale"))        
++#        locale_dir = os.path.abspath(os.path.join(defs.DATA_DIR, "locale"))
 +#        for module in (gettext, locale):
 +#            module.bindtextdomain('hamster-applet', locale_dir)
 +#            module.textdomain('hamster-applet')
-         
+ 
 -            if hasattr(module, 'bind_textdomain_codeset'):
 -                module.bind_textdomain_codeset('hamster-applet','UTF-8')
 +#            if hasattr(module, 'bind_textdomain_codeset'):
 +#                module.bind_textdomain_codeset('hamster-applet','UTF-8')
-         
+ 
          gtk.window_set_default_icon_name("hamster-applet")
-     
+ 
