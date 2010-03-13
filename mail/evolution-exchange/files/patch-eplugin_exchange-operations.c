35f34ada149c137e5af7fd9c1fedc5a433e6cbd4
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2010-03-13 08:57:09 +0000

    Update to 2.29.92, and add stub functions to plugin.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13804 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ mail/evolution-exchange/files/patch-eplugin_exchange-operations.c
@@ -0,0 +1,18 @@
+--- eplugin/exchange-operations.c.orig	2010-03-13 03:42:59.000000000 -0500
++++ eplugin/exchange-operations.c	2010-03-13 03:43:52.000000000 -0500
+@@ -304,3 +304,15 @@ is_exchange_personal_folder (ExchangeAcc
+ 	}
+ 	return FALSE;
+ }
++
++const char * g_module_check_init (gpointer module);
++const char *
++g_module_check_init (gpointer module) {
++	return NULL;
++}
++
++void g_module_unload (gpointer module);
++void
++g_module_unload (gpointer module) {
++	return NULL;
++}
