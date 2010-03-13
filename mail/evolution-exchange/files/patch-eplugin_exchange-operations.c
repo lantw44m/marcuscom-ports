c4b4f3bdc96a34c506bd40a8581a64f948b7a99e
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2010-03-13 22:50:40 +0000

    Add a stub symbol for e_plugin_lib_get_configure_widget.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13820 df743ca5-7f9a-e211-a948-0013205c9059

--- mail/evolution-exchange/files/patch-eplugin_exchange-operations.c
+++ mail/evolution-exchange/files/patch-eplugin_exchange-operations.c
@@ -1,6 +1,6 @@
---- eplugin/exchange-operations.c.orig	2010-03-13 03:42:59.000000000 -0500
-+++ eplugin/exchange-operations.c	2010-03-13 03:43:52.000000000 -0500
-@@ -304,3 +304,15 @@ is_exchange_personal_folder (ExchangeAcc
+--- eplugin/exchange-operations.c.orig	2010-03-07 20:00:49.000000000 -0500
++++ eplugin/exchange-operations.c	2010-03-13 17:47:23.000000000 -0500
+@@ -304,3 +304,21 @@ is_exchange_personal_folder (ExchangeAcc
  	}
  	return FALSE;
  }
@@ -15,4 +15,10 @@
 +void
 +g_module_unload (gpointer module) {
 +	return;
++}
++
++GtkWidget *e_plugin_lib_get_configure_widget (EPlugin *plugin);
++GtkWidget *
++e_plugin_lib_get_configure_widget (Eplugin *plugin) {
++	return NULL;
 +}
--- mail/evolution-mapi/files/patch-src_account-setup-eplugin_exchange-mapi-account-setup.c
+++ mail/evolution-mapi/files/patch-src_account-setup-eplugin_exchange-mapi-account-setup.c
@@ -1,6 +1,6 @@
 --- src/account-setup-eplugin/exchange-mapi-account-setup.c.orig	2010-03-08 04:03:24.000000000 -0500
-+++ src/account-setup-eplugin/exchange-mapi-account-setup.c	2010-03-13 04:06:21.000000000 -0500
-@@ -696,3 +696,15 @@ exchange_mapi_cal_commit (EPlugin *epl, 
++++ src/account-setup-eplugin/exchange-mapi-account-setup.c	2010-03-13 17:49:17.000000000 -0500
+@@ -696,3 +696,21 @@ exchange_mapi_cal_commit (EPlugin *epl, 
  	// Update the folder list in the plugin and ExchangeMAPIFolder
  	return;
  }
@@ -15,4 +15,10 @@
 +void
 +g_module_unload (gpointer module) {
 +	return;
++}
++
++GtkWidget *e_plugin_lib_get_configure_widget (EPlugin *plugin);
++GtkWidget *
++e_plugin_lib_get_configure_widget (EPlugin *plugin) {
++	return NULL;
 +}
