95dc486e7b3b5810bf97c8bc2e9a6add5d995110
avl <avl@df743ca5-7f9a-e211-a948-0013205c9059>
2010-03-29 12:36:09 +0000

    Fix patch.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@13894 df743ca5-7f9a-e211-a948-0013205c9059

--- multimedia/totem-pl-parser/files/patch-plparse_totem-pl-parser-podcast.c
+++ multimedia/totem-pl-parser/files/patch-plparse_totem-pl-parser-podcast.c
@@ -1,10 +1,10 @@
 --- plparse/totem-pl-parser-podcast.c	2010-03-27 04:45:52.000000000 -0400
 +++ plparse/totem-pl-parser-podcast.c.orig	2010-03-27 04:45:30.000000000 -0400
-@@ -25,7 +25,6 @@
+@@ -25,6 +25,7 @@
  #include <glib.h>
  
  #ifndef TOTEM_PL_PARSER_MINI
--#undef _LARGEFILE64_SOURCE
++#undef _LARGEFILE64_SOURCE
  #include <zlib.h>
  
  #include "xmlparser.h"
