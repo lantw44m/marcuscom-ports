cdda2fd464bfb59b35bb53d6d206212db604033f
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2010-07-26 16:25:14 +0000

    gdm 2.30.4. gdm3 2.31.2
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@14374 df743ca5-7f9a-e211-a948-0013205c9059

--- x11/gdm/files/patch-gui_simple-greeter_gdm-user-manager.c
+++ x11/gdm/files/patch-gui_simple-greeter_gdm-user-manager.c
@@ -1,15 +1,15 @@
---- gui/simple-greeter/gdm-user-manager.c.orig	2010-06-26 00:09:31.000000000 +0200
-+++ gui/simple-greeter/gdm-user-manager.c	2010-07-20 23:23:07.000000000 +0200
-@@ -62,6 +62,8 @@
+--- gui/simple-greeter/gdm-user-manager.c.orig	2010-07-12 23:17:16.000000000 +0200
++++ gui/simple-greeter/gdm-user-manager.c	2010-07-26 18:19:57.000000000 +0200
+@@ -60,6 +60,8 @@
  
  #ifdef __sun
- #define DEFAULT_MINIMAL_UID     100
+ #define FALLBACK_MINIMAL_UID     100
 +#elif defined(__FreeBSD__)
-+#define DEFAULT_MINIMAL_UID     1000
++#define FALLBACK_MINIMAL_UID     1000
  #else
- #define DEFAULT_MINIMAL_UID     500
+ #define FALLBACK_MINIMAL_UID     500
  #endif
-@@ -1678,12 +1680,17 @@ reload_passwd_file (GHashTable *valid_sh
+@@ -1548,12 +1550,17 @@ reload_passwd_file (GHashTable *valid_sh
                                                     NULL,
                                                     g_object_unref);
  
@@ -27,7 +27,7 @@
  
          /* Make sure we keep users who are logged in no matter what. */
          g_hash_table_iter_init (&iter, current_users_by_name);
-@@ -1733,9 +1740,15 @@ reload_passwd_file (GHashTable *valid_sh
+@@ -1603,9 +1610,15 @@ reload_passwd_file (GHashTable *valid_sh
  
                  g_debug ("GdmUserManager: include_all is TRUE");
  
@@ -42,8 +42,8 @@
 +#endif
  
                          /* Skip users below MinimalUID... */
-                         if (pwent->pw_uid < DEFAULT_MINIMAL_UID) {
-@@ -1810,7 +1823,11 @@ reload_passwd_file (GHashTable *valid_sh
+                         if (pwent->pw_uid < FALLBACK_MINIMAL_UID) {
+@@ -1680,7 +1693,11 @@ reload_passwd_file (GHashTable *valid_sh
   out:
          /* Cleanup */
  
