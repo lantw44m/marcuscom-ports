15910f40da85937321a853266d88dd39c66acce3
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2010-08-29 21:07:06 +0000

    Whoops, forgot to commit some bug fixes.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@14480 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/glib20/files/patch-gio_gcredentials.c
+++ devel/glib20/files/patch-gio_gcredentials.c
@@ -1,17 +1,18 @@
---- gio/gcredentials.c.orig	2010-08-29 12:29:02.000000000 -0400
-+++ gio/gcredentials.c	2010-08-29 13:46:08.000000000 -0400
-@@ -22,6 +22,10 @@
+--- gio/gcredentials.c.orig	2010-07-30 15:36:31.000000000 -0400
++++ gio/gcredentials.c	2010-08-29 17:05:48.000000000 -0400
+@@ -22,6 +22,11 @@
  
  #include "config.h"
  
 +#ifdef __FreeBSD__
 +#include <sys/types.h>
 +#include <sys/socket.h>
++#include <string.h>
 +#endif
  #include <stdlib.h>
  
  #include <gobject/gvaluecollector.h>
-@@ -55,6 +59,9 @@
+@@ -55,6 +60,9 @@
   * <citerefentry><refentrytitle>unix</refentrytitle><manvolnum>7</manvolnum></citerefentry>
   * man page for details. This corresponds to
   * %G_CREDENTIALS_TYPE_LINUX_UCRED.
@@ -21,7 +22,7 @@
   */
  
  /**
-@@ -72,6 +79,8 @@ struct _GCredentials
+@@ -72,6 +80,8 @@ struct _GCredentials
  
  #ifdef __linux__
    struct ucred native;
@@ -30,7 +31,7 @@
  #else
  #ifdef __GNUC__
  #warning Please add GCredentials support for your OS
-@@ -120,6 +129,11 @@ g_credentials_init (GCredentials *creden
+@@ -120,6 +130,11 @@ g_credentials_init (GCredentials *creden
    credentials->native.pid = getpid ();
    credentials->native.uid = geteuid ();
    credentials->native.gid = getegid ();
@@ -42,7 +43,7 @@
  #endif
  }
  
-@@ -173,6 +187,14 @@ g_credentials_to_string (GCredentials *c
+@@ -173,6 +188,14 @@ g_credentials_to_string (GCredentials *c
      g_string_append_printf (ret, "gid=%" G_GINT64_FORMAT ",", (gint64) credentials->native.gid);
    if (ret->str[ret->len - 1] == ',')
      ret->str[ret->len - 1] = '\0';
@@ -57,7 +58,7 @@
  #else
    g_string_append (ret, "unknown");
  #endif
-@@ -213,6 +235,9 @@ g_credentials_is_same_user (GCredentials
+@@ -213,6 +236,9 @@ g_credentials_is_same_user (GCredentials
  #ifdef __linux__
    if (credentials->native.uid == other_credentials->native.uid)
      ret = TRUE;
@@ -67,14 +68,14 @@
  #else
    g_set_error_literal (error,
                         G_IO_ERROR,
-@@ -263,6 +288,17 @@ g_credentials_get_native (GCredentials  
+@@ -263,6 +289,17 @@ g_credentials_get_native (GCredentials  
      {
        ret = &credentials->native;
      }
 +#elif defined(__FreeBSD__)
 +  if (native_type != G_CREDENTIALS_TYPE_FREEBSD_CMSGCRED)
 +    {
-+      g_waring ("g_credentials_get_native: Trying to get credentials of type %d but only "
++      g_warning ("g_credentials_get_native: Trying to get credentials of type %d but only "
 +		 "G_CREDENTIALS_TYPE_FREEBSD_CMSGCRED is supported.",
 +		 native_type);
 +    }
@@ -85,7 +86,7 @@
  #else
    g_warning ("g_credentials_get_native: Trying to get credentials but GLib has no support "
               "for the native credentials type. Please add support.");
-@@ -302,6 +338,17 @@ g_credentials_set_native (GCredentials  
+@@ -302,6 +339,17 @@ g_credentials_set_native (GCredentials  
      {
        memcpy (&credentials->native, native, sizeof (struct ucred));
      }
@@ -103,7 +104,7 @@
  #else
    g_warning ("g_credentials_set_native: Trying to set credentials but GLib has no support "
               "for the native credentials type. Please add support.");
-@@ -338,6 +385,8 @@ g_credentials_get_unix_user (GCredential
+@@ -338,6 +386,8 @@ g_credentials_get_unix_user (GCredential
  
  #ifdef __linux__
    ret = credentials->native.uid;
@@ -112,7 +113,7 @@
  #else
    ret = -1;
    g_set_error_literal (error,
-@@ -381,6 +430,9 @@ g_credentials_set_unix_user (GCredential
+@@ -381,6 +431,9 @@ g_credentials_set_unix_user (GCredential
  #ifdef __linux__
    credentials->native.uid = uid;
    ret = TRUE;
