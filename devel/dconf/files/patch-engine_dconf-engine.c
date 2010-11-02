0d26c0b50b5136c1ee5fecb50ff4ead579e2fcbe
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2010-11-02 23:28:59 +0000

    Use ftuncate() instead of commented out code block with posix_fallocate(). Fix some memleaks while here.
    Obtained from:	dconf git
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@14856 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/dconf/files/patch-engine_dconf-engine.c
+++ devel/dconf/files/patch-engine_dconf-engine.c
@@ -1,19 +1,11 @@
---- engine/dconf-engine.c.orig	2010-10-21 13:49:19.000000000 +0200
-+++ engine/dconf-engine.c	2010-10-21 14:16:31.000000000 +0200
-@@ -144,6 +144,7 @@ dconf_engine_setup_user (DConfEngine *en
+--- engine/dconf-engine.c.orig	2010-08-04 17:40:53.000000000 +0200
++++ engine/dconf-engine.c	2010-11-03 00:23:07.000000000 +0100
+@@ -144,7 +144,7 @@ dconf_engine_setup_user (DConfEngine *en
  
            if (fd >= 0)
              {
-+/*
-               if (posix_fallocate (fd, 0, 1) == 0)
+-              if (posix_fallocate (fd, 0, 1) == 0)
++              if (ftruncate (fd, 1) == 0)
                  {
                    engine->shm = mmap (NULL, 1, PROT_READ, MAP_SHARED, fd, 0);
-@@ -151,7 +152,7 @@ dconf_engine_setup_user (DConfEngine *en
-                   if (engine->shm == MAP_FAILED)
-                     engine->shm = NULL;
-                 }
--
-+*/
-               close (fd);
-             }
-         }
+ 
