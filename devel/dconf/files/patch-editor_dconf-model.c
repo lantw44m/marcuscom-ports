19d840249880c4d85c6ee72f00bed8bb3a51f31e
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2011-03-23 13:56:00 +0000

    Update to 0.7.3.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@15575 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/dconf/files/patch-editor_dconf-model.c
+++ devel/dconf/files/patch-editor_dconf-model.c
@@ -1,9 +1,9 @@
---- editor/dconf-model.c.orig	2011-03-19 23:04:18.000000000 +0100
-+++ editor/dconf-model.c	2011-03-19 23:05:20.000000000 +0100
-@@ -2170,7 +2170,7 @@ SettingsModel* settings_model_construct 
- 	_tmp5_ = _tmp4_;
+--- editor/dconf-model.c.orig	2011-03-22 16:59:25.000000000 +0100
++++ editor/dconf-model.c	2011-03-23 14:53:41.000000000 +0100
+@@ -2303,7 +2303,7 @@
+ 	_tmp2_ = schema_list_new ();
  	_schema_list_unref0 (self->schemas);
- 	self->schemas = _tmp5_;
+ 	self->schemas = _tmp2_;
 -	schema_list_load_directory (self->schemas, "/usr/share/glib-2.0/schemas", &_inner_error_);
 +	schema_list_load_directory (self->schemas, "/usr/local/share/glib-2.0/schemas", &_inner_error_);
  	if (_inner_error_ != NULL) {
