750eb479b96c5d4bbccf04f1cac2a9be852400fd
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2011-04-27 15:47:48 +0000

    Update to 3.0.1.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@15903 df743ca5-7f9a-e211-a948-0013205c9059

--- www/epiphany3/files/extra-patch-embed_ephy-embed-prefs.c
+++ www/epiphany3/files/extra-patch-embed_ephy-embed-prefs.c
@@ -1,10 +1,12 @@
---- embed/ephy-embed-prefs.c.orig	2010-12-09 09:22:56.000000000 +0000
-+++ embed/ephy-embed-prefs.c	2011-01-16 14:51:37.000000000 +0000
-@@ -483,6 +483,7 @@ ephy_embed_prefs_init (void)
+--- embed/ephy-embed-prefs.c.orig	2011-04-27 17:46:35.000000000 +0200
++++ embed/ephy-embed-prefs.c	2011-04-27 17:47:14.000000000 +0200
+@@ -481,8 +483,8 @@ ephy_embed_prefs_init (void)
+ 
    /* Hardcoded settings */
    g_object_set (webkit_settings,
-                 "auto-shrink-images", FALSE,
-+                "enable-spell-checking", TRUE,
-                 "enable-default-context-menu", FALSE,
+-                "enable-default-context-menu", FALSE,
+                 "enable-spell-checking", TRUE,
++                "enable-default-context-menu", FALSE,
                  "enable-site-specific-quirks", TRUE,
                  "enable-page-cache", TRUE,
+                 "enable-developer-extras", TRUE,
