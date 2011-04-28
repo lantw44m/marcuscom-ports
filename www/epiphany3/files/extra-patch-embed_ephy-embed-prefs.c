9febd039f3dd57d8ff7de66aea0a3f49ce444f33
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2011-04-28 07:00:06 +0000

    Fix patching.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@15928 df743ca5-7f9a-e211-a948-0013205c9059

--- www/epiphany3/files/extra-patch-embed_ephy-embed-prefs.c
+++ www/epiphany3/files/extra-patch-embed_ephy-embed-prefs.c
@@ -1,12 +1,10 @@
---- embed/ephy-embed-prefs.c.orig	2011-04-27 17:46:35.000000000 +0200
-+++ embed/ephy-embed-prefs.c	2011-04-27 17:47:14.000000000 +0200
-@@ -481,8 +483,8 @@ ephy_embed_prefs_init (void)
- 
+--- embed/ephy-embed-prefs.c.orig	2011-04-26 00:08:02.000000000 +0200
++++ embed/ephy-embed-prefs.c	2011-04-28 08:59:55.000000000 +0200
+@@ -482,6 +482,7 @@ ephy_embed_prefs_init (void)
    /* Hardcoded settings */
    g_object_set (webkit_settings,
--                "enable-default-context-menu", FALSE,
-                 "enable-spell-checking", TRUE,
-+                "enable-default-context-menu", FALSE,
+                 "enable-default-context-menu", FALSE,
++                "enable-spell-checking", TRUE,
                  "enable-site-specific-quirks", TRUE,
                  "enable-page-cache", TRUE,
                  "enable-developer-extras", TRUE,
