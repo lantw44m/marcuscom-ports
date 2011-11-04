f9f6d128a96d148924b3e9196f775e0e578773fd
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2011-11-04 12:45:11 +0000

    Use upstream patch to see if CC is set. This removes the temp hardcode I put in place at build time.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@16554 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gobject-introspection/files/patch-giscanner_sourcescanner.py
+++ devel/gobject-introspection/files/patch-giscanner_sourcescanner.py
@@ -1,11 +1,12 @@
 --- giscanner/sourcescanner.py.orig	2011-09-03 20:42:14.000000000 +0200
-+++ giscanner/sourcescanner.py	2011-10-04 13:48:48.000000000 +0200
-@@ -274,7 +274,7 @@
++++ giscanner/sourcescanner.py	2011-11-04 13:36:04.000000000 +0100
+@@ -274,7 +274,8 @@ class SourceScanner(object):
  
          defines = ['__GI_SCANNER__']
          undefs = []
 -        cpp_args = [os.environ.get('CC', 'cc'), '-E', '-C', '-I.', '-']
-+        cpp_args = ['%%CC%%', '-E', '-C', '-I.', '-']
++        cpp_args = os.environ.get('CC', 'cc').split()
++        cpp_args += ['-E', '-C', '-I.', '-']
  
          cpp_args += self._cpp_options
          proc = subprocess.Popen(cpp_args,
