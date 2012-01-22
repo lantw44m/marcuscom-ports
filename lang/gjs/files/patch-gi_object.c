714f0736d0f61557d0e74e6d9b147d087aead8ea
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2012-01-22 10:58:32 +0000

    Update to 1.30.1.
    While here get rid of a 6.x check.
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@16660 df743ca5-7f9a-e211-a948-0013205c9059

--- lang/gjs/files/patch-gi_object.c
+++ lang/gjs/files/patch-gi_object.c
@@ -2,19 +2,19 @@ libxul 1.9.2.x doesn't support this yet.
 
 http://git.gnome.org/browse/gjs/commit/?id=72bc4307860a315f9d2173d700582add938d79a0
 
---- gi/object.c.orig	2011-09-22 17:15:11.000000000 +0200
-+++ gi/object.c	2011-09-22 17:17:46.000000000 +0200
-@@ -674,7 +674,6 @@
+--- gi/object.c.orig	2012-01-22 11:53:37.000000000 +0100
++++ gi/object.c	2012-01-22 11:54:37.000000000 +0100
+@@ -674,7 +674,6 @@ GJS_NATIVE_CONSTRUCTOR_DECLARE(object_in
          if (unthreadsafe_template_for_constructor.gobj == NULL) {
              GParameter *params;
              int n_params;
 -            GTypeQuery query;
+             JSObject *old_jsobj;
  
              gtype = g_registered_type_info_get_g_type( (GIRegisteredTypeInfo*) priv->info);
-             if (gtype == G_TYPE_NONE) {
-@@ -693,9 +692,6 @@
-             priv->gobj = g_object_newv(gtype, n_params, params);
-             free_g_params(params, n_params);
+@@ -711,9 +710,6 @@ GJS_NATIVE_CONSTRUCTOR_DECLARE(object_in
+                 goto out;
+             }
  
 -            g_type_query(gtype, &query);
 -            JS_updateMallocCounter(context, query.instance_size);
