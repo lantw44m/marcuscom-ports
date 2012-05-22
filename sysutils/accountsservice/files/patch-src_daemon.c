f6c062ba4bfc59c8c294973d0f245108c5209f40
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2012-05-22 08:56:42 +0000

    Update to 0.6.21.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@16774 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/accountsservice/files/patch-src_daemon.c
+++ sysutils/accountsservice/files/patch-src_daemon.c
@@ -1,12 +1,18 @@
---- src/daemon.c.orig	2011-10-17 21:30:22.000000000 +0200
-+++ src/daemon.c	2011-10-18 16:28:09.000000000 +0200
-@@ -54,6 +54,9 @@
- #define PATH_LOGIN_DEFS "/etc/login.defs"
- #define PATH_GDM_CUSTOM "/etc/gdm/custom.conf"
+--- src/daemon.c.orig	2012-05-08 17:14:59.000000000 +0200
++++ src/daemon.c	2012-05-22 10:45:14.000000000 +0200
+@@ -70,6 +70,7 @@
+         "nobody4",
+         "noaccess",
+         "postgres",
++        "pgsql",
+         "pvm",
+         "rpm",
+         "nfsnobody",
+@@ -79,6 +80,7 @@
+         "games",
+         "man",
+         "at",
++        "saned",
+         NULL
+ };
  
-+#ifdef __FreeBSD__
-+#define FALLBACK_MINIMAL_UID 1000
-+#endif
- #ifndef FALLBACK_MINIMAL_UID
- #define FALLBACK_MINIMAL_UID 500
- #endif
