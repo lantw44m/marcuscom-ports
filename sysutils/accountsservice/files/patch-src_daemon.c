ef5444c75c4786599129040631b8768915c22ffc
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2012-06-29 08:46:28 +0000

    Update to 0.6.22.
    This fixes CVE-2012-2737
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@16973 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/accountsservice/files/patch-src_daemon.c
+++ sysutils/accountsservice/files/patch-src_daemon.c
@@ -1,6 +1,15 @@
---- src/daemon.c.orig	2012-05-08 17:14:59.000000000 +0200
-+++ src/daemon.c	2012-05-22 10:45:14.000000000 +0200
-@@ -70,6 +70,7 @@
+--- src/daemon.c.orig	2012-06-28 17:24:55.000000000 +0200
++++ src/daemon.c	2012-06-29 10:44:09.000000000 +0200
+@@ -49,7 +49,7 @@
+ #define PATH_SHADOW "/etc/shadow"
+ #define PATH_NOLOGIN "/sbin/nologin"
+ #define PATH_FALSE "/bin/false"
+-#define PATH_GDM_CUSTOM "/etc/gdm/custom.conf"
++#define PATH_GDM_CUSTOM "/usr/local/etc/gdm/custom.conf"
+ 
+ static const char *default_excludes[] = {
+         "bin",
+@@ -68,6 +68,7 @@
          "nobody4",
          "noaccess",
          "postgres",
@@ -8,7 +17,7 @@
          "pvm",
          "rpm",
          "nfsnobody",
-@@ -79,6 +80,7 @@
+@@ -77,6 +78,7 @@
          "games",
          "man",
          "at",
@@ -16,3 +25,17 @@
          NULL
  };
  
+@@ -315,7 +317,13 @@
+         g_hash_table_foreach (daemon->priv->users, listify_hash_values_hfunc, &old_users);
+         g_slist_foreach (old_users, (GFunc) g_object_ref, NULL);
+ 
++#ifdef HAVE_FGETPWENT
+         while ((pwent = fgetpwent (fp)) != NULL) {
++#else
++        setpwent();
++
++        while ((pwent = getpwent ()) != NULL) {
++#endif
+                 /* Skip system users... */
+                 if (daemon_local_user_is_excluded (daemon, pwent->pw_name, pwent->pw_shell)) {
+                         g_debug ("skipping user: %s", pwent->pw_name);
