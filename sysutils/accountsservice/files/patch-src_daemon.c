63ad05984348698cf0b229dc3c0a648ec61aa4fb
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2012-09-27 15:44:13 +0000

    Tweak the patch a bit and remove a part which is already handled by a sed line.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@17079 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/accountsservice/files/patch-src_daemon.c
+++ sysutils/accountsservice/files/patch-src_daemon.c
@@ -1,14 +1,5 @@
 --- src/daemon.c.orig	2012-08-16 19:03:51.000000000 +0000
-+++ src/daemon.c	2012-09-19 21:48:15.000000000 +0000
-@@ -49,7 +49,7 @@
- #define PATH_SHADOW "/etc/shadow"
- #define PATH_NOLOGIN "/sbin/nologin"
- #define PATH_FALSE "/bin/false"
--#define PATH_GDM_CUSTOM "/etc/gdm/custom.conf"
-+#define PATH_GDM_CUSTOM "/usr/local/etc/gdm/custom.conf"
- 
- static const char *default_excludes[] = {
-         "bin",
++++ src/daemon.c	2012-09-19 22:31:43.000000000 +0000
 @@ -68,6 +68,7 @@
          "nobody4",
          "noaccess",
@@ -25,13 +16,14 @@
          NULL
  };
  
-@@ -304,7 +306,11 @@
+@@ -304,7 +306,12 @@
  
          /* Every iteration */
          fp = *state;
 +#ifdef HAVE_FGETPWENT
          pwent = fgetpwent (fp);
 +#else
++	setpwent();
 +	pwent = getpwent ();
 +#endif
          if (pwent != NULL) {
