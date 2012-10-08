ca0901871495faaf97136f0e16297af0226246f3
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2012-10-08 09:31:20 +0000

    Fix gdm login
    Submitted by:	Berislav Purgar <bpurgar@gmail.com>
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@17109 df743ca5-7f9a-e211-a948-0013205c9059

--- sysutils/accountsservice/files/patch-src_daemon.c
+++ sysutils/accountsservice/files/patch-src_daemon.c
@@ -1,5 +1,5 @@
 --- src/daemon.c.orig	2012-08-16 19:03:51.000000000 +0000
-+++ src/daemon.c	2012-09-19 22:31:43.000000000 +0000
++++ src/daemon.c	2012-10-08 09:18:14.000000000 +0000
 @@ -68,6 +68,7 @@
          "nobody4",
          "noaccess",
@@ -16,14 +16,13 @@
          NULL
  };
  
-@@ -304,7 +306,12 @@
+@@ -304,7 +306,11 @@
  
          /* Every iteration */
          fp = *state;
 +#ifdef HAVE_FGETPWENT
          pwent = fgetpwent (fp);
 +#else
-+	setpwent();
 +	pwent = getpwent ();
 +#endif
          if (pwent != NULL) {
