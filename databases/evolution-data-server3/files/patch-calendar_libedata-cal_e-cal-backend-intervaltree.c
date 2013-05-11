f25e897e80197fa45df0faf03cd6f9d749a21a74
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2013-05-11 22:19:59 +0000

    Readd some more lost patches
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@18451 df743ca5-7f9a-e211-a948-0013205c9059

--- databases/evolution-data-server3/files/patch-calendar_libedata-cal_e-cal-backend-intervaltree.c
+++ databases/evolution-data-server3/files/patch-calendar_libedata-cal_e-cal-backend-intervaltree.c
@@ -1,5 +1,5 @@
---- calendar/libedata-cal/e-cal-backend-intervaltree.c.orig	2013-05-06 19:20:48.000000000 +0000
-+++ calendar/libedata-cal/e-cal-backend-intervaltree.c	2013-05-06 19:20:57.000000000 +0000
+--- ./calendar/libedata-cal/e-cal-backend-intervaltree.c.orig	2011-01-18 17:09:50.000000000 +0000
++++ ./calendar/libedata-cal/e-cal-backend-intervaltree.c	2011-01-18 17:09:59.000000000 +0000
 @@ -26,7 +26,7 @@
  
  #include <stdio.h>
