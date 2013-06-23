acb2581203f3eb0b031070cabac4360217a05869
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2013-06-23 10:52:15 +0000

    Update to 3.6.2.
    Submitted by:	Gustau Perez
    
    
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@18582 df743ca5-7f9a-e211-a948-0013205c9059

--- x11/gnome-shell/files/patch-js_ui_panel.js
+++ x11/gnome-shell/files/patch-js_ui_panel.js
@@ -1,23 +1,16 @@
---- js/ui/panel.js.orig	2011-12-15 15:57:12.000000000 +0100
-+++ js/ui/panel.js	2011-12-15 15:57:42.000000000 +0100
-@@ -27,7 +27,7 @@ const BUTTON_DND_ACTIVATION_TIMEOUT = 25
- const ANIMATED_ICON_UPDATE_TIMEOUT = 100;
- const SPINNER_ANIMATION_TIME = 0.2;
- 
--const STANDARD_STATUS_AREA_ORDER = ['a11y', 'keyboard', 'volume', 'bluetooth', 'network', 'battery', 'userMenu'];
-+const STANDARD_STATUS_AREA_ORDER = ['a11y', 'keyboard', 'volume', 'bluetooth', 'battery', 'userMenu'];
- const STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION = {
-     'a11y': imports.ui.status.accessibility.ATIndicator,
-     'volume': imports.ui.status.volume.Indicator,
-@@ -39,11 +39,6 @@ const STANDARD_STATUS_AREA_SHELL_IMPLEME
- if (Config.HAVE_BLUETOOTH)
-     STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION['bluetooth'] = imports.ui.status.bluetooth.Indicator;
+--- js/ui/panel.js.old	2013-04-28 19:22:35.753485501 +0000
++++ js/ui/panel.js	2013-04-28 19:23:28.059021301 +0000
+@@ -910,13 +910,6 @@
+     PANEL_ITEM_IMPLEMENTATIONS['bluetooth'] =
+         imports.ui.status.bluetooth.Indicator;
  
 -try {
--    STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION['network'] = imports.ui.status.network.NMApplet;
+-    PANEL_ITEM_IMPLEMENTATIONS['network'] =
+-        imports.ui.status.network.NMApplet;
 -} catch(e) {
 -    log('NMApplet is not supported. It is possible that your NetworkManager version is too old');
 -}
+-
+ const Panel = new Lang.Class({
+     Name: 'Panel',
  
- const GDM_STATUS_AREA_ORDER = ['a11y', 'display', 'keyboard', 'volume', 'battery', 'powerMenu'];
- const GDM_STATUS_AREA_SHELL_IMPLEMENTATION = {
