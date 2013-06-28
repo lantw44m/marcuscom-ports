f570426127d46e4e6deece811d33db15d29eff38
kwm <kwm@df743ca5-7f9a-e211-a948-0013205c9059>
2013-06-28 20:03:59 +0000

    Fix patch to actualy compile , grrr.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports/trunk@18606 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/gobject-introspection/files/patch-giscanner_sourcescanner.py
+++ devel/gobject-introspection/files/patch-giscanner_sourcescanner.py
@@ -1,6 +1,6 @@
---- giscanner/sourcescanner.py.orig	2013-06-28 21:08:22.000000000 +0200
-+++ giscanner/sourcescanner.py	2013-06-28 21:08:54.000000000 +0200
-@@ -281,8 +281,9 @@
+--- giscanner/sourcescanner.py.orig	2013-03-06 23:53:19.000000000 +0100
++++ giscanner/sourcescanner.py	2013-06-28 22:01:04.000000000 +0200
+@@ -281,8 +281,11 @@
          # some help from gcc from MinGW/Cygwin or so.
          # Note that the generated dumper program is
          # still built and linked by Visual C++.
@@ -9,6 +9,8 @@
 +        if 'clang' not in cpp_exec:
 +            if 'cl' in cpp_exec:
 +                cpp_args = 'gcc'.split()
++            else:
++	        cpp_args = cpp_exec
          else:
              cpp_args = cpp_exec
          cpp_args += ['-E', '-C', '-I.', '-']
